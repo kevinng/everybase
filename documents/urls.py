2c6dd6c8fca954683704fe77d8493ba00e9f4d98
Kevin Ng <kevinngsw@gmail.com>
2020-01-13 23:22:22 +0800

    Reverse Right-Side Nav-Items Direction

--- documents/urls.py
+++ documents/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('inbox/', views.inbox, name='inbox'),
+  path('documents/', views.documents, name='documents'),
+  path('sent/', views.sent, name='sent'),
+  path('materials/', views.materials, name='materials'),
+  path('colleagues/', views.colleagues, name='colleagues'),
   path('r/<str:file_to_render>', views.r),
 ]
--- documents/views.py
+++ documents/views.py
@@ -1,5 +1,25 @@
 from django.template.response import TemplateResponse
 
+def inbox(request):
+    template_name = 'documents/inbox.html'
+    return TemplateResponse(request, template_name, {'location': 'inbox'})
+
+def documents(request):
+    template_name = 'documents/inbox.html'
+    return TemplateResponse(request, template_name, {'location': 'documents'})
+
+def sent(request):
+    template_name = 'documents/inbox.html'
+    return TemplateResponse(request, template_name, {'location': 'sent'})
+
+def materials(request):
+    template_name = 'documents/inbox.html'
+    return TemplateResponse(request, template_name, {'location': 'materials'})
+
+def colleagues(request):
+    template_name = 'documents/inbox.html'
+    return TemplateResponse(request, template_name, {'location': 'colleagues'})
+
 def r(request, file_to_render):
     template_name = 'documents/%s' % file_to_render
     return TemplateResponse(request, template_name, {})
