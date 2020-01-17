73af0d4d33a87e539e72152e9b26051a9e7a7f9f
Kevin Ng <kevinngsw@gmail.com>
2020-01-17 23:08:11 +0800

    Refactor File Model Fields and Add File Field
    - Add file_uploaded Field field for quick access to file
    
    - Tidy/comment code so it's clear which fields we're updating on which
        pass - i.e. issue pre-signed URL, upload file, create document
    
    - Comment static and public media S3 integration code
    
    - Make migrations and migrate

--- documents/models.py
+++ documents/models.py
@@ -4,6 +4,8 @@ import uuid
 from accounts.models import Account, Organization
 from materials.models import Material, Batch
 
+from everybase.storage_backends import PrivateMediaStorage
+
 class DocumentType(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
@@ -39,24 +41,36 @@ class Document(models.Model):
 
 class File(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # Data set when pre-signed URL is issued - 1st pass
     presigned_url_issued = models.DateTimeField(auto_now_add=True)
-    file_uploaded = models.DateTimeField(blank=True, null=True, default=None)
-    deleted = models.DateTimeField(blank=True, null=True, default=None)
     presigned_url = models.URLField()
     expires_in = models.IntegerField()
     bucket_name = models.CharField(max_length=63)
     object_key = models.CharField(max_length=1024)
+
+    # Data set when file is uploaded using pre-signed URL - 2nd pass
+    file_uploaded = models.FileField(
+        blank=True, null=True, default=None,
+        storage=PrivateMediaStorage())
+    uploaded = models.DateTimeField(blank=True, null=True, default=None)
     object_content_length = models.IntegerField(blank=True, null=True, default=None)
     object_e_tag = models.CharField(blank=True, null=True, default=None, max_length=1024)
     object_content_type = models.CharField(blank=True, null=True, default=None, max_length=30)
     object_last_modified = models.DateTimeField(blank=True, null=True, default=None)
-
     uploader = models.ForeignKey(Document,
+        models.CASCADE,
         blank=True, null=True, default=None,
-        models.CASCADE, related_name='files_uploaded')
+        related_name='files_uploaded')
+
+    # Data set when the document is created - 3rd pass
     document = models.ForeignKey(Document,
+        models.CASCADE,
         blank=True, null=True, default=None,
-        models.CASCADE, related_name='files')
+        related_name='files')
+
+    # Other fields
+    deleted = models.DateTimeField(blank=True, null=True, default=None)
 
 class DocumentMaterial(models.Model):
     document = models.ForeignKey(Document, models.CASCADE)
