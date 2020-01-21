1137515767be443de9491b2070dd1bd8f5dc7b4b
Kevin Ng <kevinngsw@gmail.com>
2020-01-21 10:50:08 +0800

    Show Presigned URL Models in Admin

--- documents/admin.py
+++ documents/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
 from .models import (
-    Document, DocumentType, File, DocumentMaterial, DocumentBatch)
+    Document, DocumentType, File, DocumentMaterial, DocumentBatch,
+    CreateFilePresignedURL, ReadFilePresignedURL)
 
 admin.site.register(Document)
 admin.site.register(DocumentType)
 admin.site.register(File)
 admin.site.register(DocumentMaterial)
 admin.site.register(DocumentBatch)
+admin.site.register(CreateFilePresignedURL)
+admin.site.register(ReadFilePresignedURL)
