7f5cea704efff1940e2cb4f989bd2a583bda5915
Kevin Ng <kevinngsw@gmail.com>
2020-05-09 18:14:30 +0800

    Show Cloaked Link Description in Admin Listing

--- cloaker/models.py
+++ cloaker/models.py
@@ -6,4 +6,4 @@ class CloakedLink(models.Model):
     url = models.URLField()
 
     def __str__(self):
-        return str(self.id) + ' - ' + self.page_title
+        return str(self.id) + '; ' + self.page_title + '; ' + self.description
