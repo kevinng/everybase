148db8ac25895db407234fdcb0b0154552bfba41
Kevin Ng <kevinngsw@gmail.com>
2020-09-22 12:47:38 +0800

    Implement common models

--- common/models.py
+++ common/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+"""
+Primary key ID is already present in standard models. Parent-Children Choice
+fields need to be implemented at model-level.
+"""
+
+class Standard():
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    deleted = models.DateTimeField(null=True, default=None)
+
+class Choice():
+    name = models.CharField(max_length=100)
+    details_md = models.TextField()
+
+    programmatic_key = models.CharField(max_length=100)
+    programmatic_details_md = models.TextField()
