2a8cfeb07fc421c09fbb8a878b031e077522fbf3
Kevin Ng <kevinngsw@gmail.com>
2020-09-22 13:59:49 +0800

    Define base models

--- common/models.py
+++ common/models.py
@@ -1,18 +1,3 @@
 from django.db import models
 
-"""
-Primary key ID is already present in standard models. Parent-Children Choice
-fields need to be implemented at model-level.
-"""
-
-class Standard():
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    deleted = models.DateTimeField(null=True, default=None)
-
-class Choice():
-    name = models.CharField(max_length=100)
-    details_md = models.TextField()
-
-    programmatic_key = models.CharField(max_length=100)
-    programmatic_details_md = models.TextField()
+# Create your models here.
