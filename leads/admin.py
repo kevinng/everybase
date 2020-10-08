073cb224cede959233827dbb72bec99ec5c700f9
Kevin Ng <kevinngsw@gmail.com>
2020-10-08 14:07:23 +0800

    Register MatchMethod admin

--- leads/admin.py
+++ leads/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import (Incoterm, Currency, PaymentMode, ContactType, LeadCategory)
+from .models import (Incoterm, Currency, PaymentMode, ContactType, LeadCategory,
+    MatchMethod)
 from common.admin import ChoiceAdmin, ParentChildrenChoice
 
 admin.site.register(Incoterm, ChoiceAdmin)
@@ -7,3 +8,4 @@ admin.site.register(Currency, ChoiceAdmin)
 admin.site.register(PaymentMode, ChoiceAdmin)
 admin.site.register(ContactType, ChoiceAdmin)
 admin.site.register(LeadCategory, ParentChildrenChoice)
+admin.site.register(MatchMethod, ChoiceAdmin)
