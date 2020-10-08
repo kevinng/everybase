a10d4e66aab53c0f9f65ca13475587a6f9fdb8dd
Kevin Ng <kevinngsw@gmail.com>
2020-10-08 14:09:56 +0800

    Register MatchStatus admin

--- leads/admin.py
+++ leads/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import (Incoterm, Currency, PaymentMode, ContactType, LeadCategory,
-    MatchMethod)
+    MatchMethod, MatchStatus)
 from common.admin import ChoiceAdmin, ParentChildrenChoice
 
 admin.site.register(Incoterm, ChoiceAdmin)
@@ -9,3 +9,4 @@ admin.site.register(PaymentMode, ChoiceAdmin)
 admin.site.register(ContactType, ChoiceAdmin)
 admin.site.register(LeadCategory, ParentChildrenChoice)
 admin.site.register(MatchMethod, ChoiceAdmin)
+admin.site.register(MatchStatus, ChoiceAdmin)
