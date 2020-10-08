5e8369a41851f0b6e766942bdd2335da83d852ae
Kevin Ng <kevinngsw@gmail.com>
2020-10-08 12:14:05 +0800

    Create PaymentMode admin

--- leads/admin.py
+++ leads/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import (Incoterm, Currency)
+from .models import (Incoterm, Currency, PaymentMode)
 from common.admin import ChoiceAdmin
 
 admin.site.register(Incoterm, ChoiceAdmin)
 admin.site.register(Currency, ChoiceAdmin)
+admin.site.register(PaymentMode, ChoiceAdmin)
