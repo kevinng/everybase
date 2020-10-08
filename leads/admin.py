d72e9271ea1afa44d0aee39acbea188a37282369
Kevin Ng <kevinngsw@gmail.com>
2020-10-08 12:23:09 +0800

    Add ContactType admin

--- leads/admin.py
+++ leads/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import (Incoterm, Currency, PaymentMode)
+from .models import (Incoterm, Currency, PaymentMode, ContactType)
 from common.admin import ChoiceAdmin
 
 admin.site.register(Incoterm, ChoiceAdmin)
 admin.site.register(Currency, ChoiceAdmin)
 admin.site.register(PaymentMode, ChoiceAdmin)
+admin.site.register(ContactType, ChoiceAdmin)
