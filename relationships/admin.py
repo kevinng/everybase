17014774a66a1c0599a7cb0ac9e9a1682518c736
Kevin Ng <kevinngsw@gmail.com>
2020-10-13 14:22:06 +0800

    Register PersonAddressType admin

--- relationships/admin.py
+++ relationships/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import (PersonLinkType, PersonLink, PersonCompanyType)
+from .models import (PersonLinkType, PersonLink, PersonCompanyType,
+    PersonAddressType)
 
 admin.site.register(PersonLinkType)
 admin.site.register(PersonLink)
 admin.site.register(PersonCompanyType)
+admin.site.register(PersonAddressType)
