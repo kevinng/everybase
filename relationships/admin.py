b49f4a6a6a52097d74ac1e1ad1b7718d2e8e924f
Kevin Ng <kevinngsw@gmail.com>
2020-10-13 13:34:08 +0800

    Register PersonLink admin

--- relationships/admin.py
+++ relationships/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import (PersonLinkType)
+from .models import (PersonLinkType, PersonLink)
 
 admin.site.register(PersonLinkType)
+admin.site.register(PersonLink)
