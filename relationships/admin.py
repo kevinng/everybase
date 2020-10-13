f260b2b11aa42e948c280dad0fb146886c729110
Kevin Ng <kevinngsw@gmail.com>
2020-10-13 13:34:47 +0800

    Register PersonCompanyLink admin

--- relationships/admin.py
+++ relationships/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import (PersonLinkType, PersonLink)
+from .models import (PersonLinkType, PersonLink, PersonCompanyType)
 
 admin.site.register(PersonLinkType)
 admin.site.register(PersonLink)
+admin.site.register(PersonCompanyType)
