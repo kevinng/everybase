c52e114d17b4116f6c8c139702baeced03f1a296
Kevin Ng <kevinngsw@gmail.com>
2020-10-14 13:05:16 +0800

    Register communication models

--- communication/admin.py
+++ communication/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import (Issue, IssueTag, IssueStatus, Conversation,
+    ConversationChannel, ConversationEmail, ConversationEmailStatus,
+    ConversationChat, ConversationChatStatus, ConversationVoice,
+    ConversationVoiceStatus, ConversationVideo, ConversationVideoStatus)
 
-# Register your models here.
+admin.site.register(Issue)
+admin.site.register(IssueTag)
+admin.site.register(IssueStatus)
+admin.site.register(Conversation)
+admin.site.register(ConversationChannel)
+admin.site.register(ConversationEmail)
+admin.site.register(ConversationEmailStatus)
+admin.site.register(ConversationChat)
+admin.site.register(ConversationChatStatus)
+admin.site.register(ConversationVoice)
+admin.site.register(ConversationVoiceStatus)
+admin.site.register(ConversationVideo)
+admin.site.register(ConversationVideoStatus)
