48bc6b9d7a4931cd19542065f90f436ab4747329
Kevin Ng <kevinngsw@gmail.com>
2020-10-14 16:03:54 +0800

    Refactor growth models to use imported common helper lambda

--- growth/models.py
+++ growth/models.py
@@ -1,53 +1,6 @@
 from django.db import models
+from common.models import fk, m2m, m2mt, tf, cf, ff, dtf
 from common.models import Standard, Choice
-
-# --- Start: Helper lambda for model field declarations ---
-
-# Foreign key
-fk = lambda klass, name=None, verbose_name=None, null=False: models.ForeignKey(
-    klass,
-    on_delete=models.PROTECT,
-    related_name=name,
-    related_query_name=name,
-    verbose_name=verbose_name,
-    null=null,
-    blank=null
-)
-
-# Many-to-many
-m2m = lambda klass, name, blank=False: models.ManyToManyField(
-    klass,
-    related_name=name,
-    related_query_name=name,
-    blank=blank
-)
-
-# Many-to-many through
-m2mt = lambda klass, thru, f1, f2, name: models.ManyToManyField(
-    klass,
-    through=thru,
-    through_fields=(f1, f2),
-    related_name=name,
-    related_query_name=name
-)
-
-# Text
-tf = lambda verbose_name=None, null=False: models.TextField(
-    verbose_name=verbose_name, null=null, blank=null)
-
-# Char
-cf = lambda verbose_name=None, null=False: models.CharField(
-    verbose_name=verbose_name, max_length=100, null=null, blank=null)
-
-# Float
-ff = lambda verbose_name=None, null=False: models.FloatField(
-    verbose_name=verbose_name, null=null, blank=null)
-
-# Datetime
-dtf = lambda verbose_name=None, null=False, default=None: models.DateTimeField(
-    null=null, blank=null, default=default)
-
-# --- End: Helper lambda for model field declarations ---
 
 class GmassCampaignResult(Standard):
     first_name = cf(null=True)
