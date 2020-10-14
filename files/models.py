0cbdfb3a2e7d6754ea6492289e2062132af124f6
Kevin Ng <kevinngsw@gmail.com>
2020-10-14 12:31:03 +0800

    Refactor file models
    - Use helper lambdas
    - Set revelant fields nullable
    - Changed S3 URL field to use URLField instead of CharField

--- files/models.py
+++ files/models.py
@@ -1,28 +1,64 @@
 from django.db import models
 from common.models import Standard, ParentChildrenChoice
 
-# --- Start: Abstract ---
+# --- Start: Helper lambda for model field declarations ---
 
-# Helper function to declare foreign key relationships.
-fk = lambda klass, name: models.ForeignKey(
-        klass,
-        on_delete=models.PROTECT,
-        related_name=name,
-        related_query_name=name
-    )
+# Foreign key
+fk = lambda klass, name=None, verbose_name=None, null=False: models.ForeignKey(
+    klass,
+    on_delete=models.PROTECT,
+    related_name=name,
+    related_query_name=name,
+    verbose_name=verbose_name,
+    null=null,
+    blank=null
+)
 
-# --- End: Abstract ---
+# Many-to-many
+m2m = lambda klass, name, blank=False: models.ManyToManyField(
+    klass,
+    related_name=name,
+    related_query_name=name,
+    blank=blank
+)
+
+# Many-to-many through
+m2mt = lambda klass, thru, f1, f2, name: models.ManyToManyField(
+    klass,
+    through=thru,
+    through_fields=(f1, f2),
+    related_name=name,
+    related_query_name=name
+)
+
+# Text
+tf = lambda verbose_name=None, null=False: models.TextField(
+    verbose_name=verbose_name, null=null, blank=null)
+
+# Char
+cf = lambda verbose_name=None, null=False: models.CharField(
+    verbose_name=verbose_name, max_length=100, null=null, blank=null)
+
+# Float
+ff = lambda verbose_name=None, null=False: models.FloatField(
+    verbose_name=verbose_name, null=null, blank=null)
+
+# Datetime
+dtf = lambda verbose_name=None, null=False, default=None: models.DateTimeField(
+    null=null, blank=null, default=default)
+
+# --- End: Helper lambda for model field declarations ---
 
 class FileTag(ParentChildrenChoice):
     pass
 
 class File(Standard):
-    source_person = fk('relationships.Person', 'files')
-    source_issue = fk('communication.Issue', 'files')
+    source_person = fk('relationships.Person', 'files', null=True)
+    source_issue = fk('communication.Issue', 'files', null=True)
 
-    s3_url = models.CharField(max_length=100)
-    details_md = models.TextField()
+    s3_url = models.URLField()
+    details_md = tf('Details in Markdown', True)
 
-    anonymization_source = fk('self', 'anonymization_copies')
+    anonymization_source = fk('self', 'anonymization_copies', null=True)
     
-    tags = fk('FileTag', 'files')
+    tags = fk('FileTag', 'files', null=True)
