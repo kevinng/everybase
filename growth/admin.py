5da3a3843bb7cc43be83581bb1b9d42d8be0d4fe
Kevin Ng <kevinngsw@gmail.com>
2020-10-14 10:53:11 +0800

    Register growth admins

--- growth/admin.py
+++ growth/admin.py
@@ -3,3 +3,10 @@ from .models import (GmassCampaignResult, GmassCampaign,
     ChemicalClusterOfSingaporeResult, Fibre2FashionResult, ZeroBounceResult,
     DataSource, SourcedEmail)
 
+admin.site.register(GmassCampaignResult)
+admin.site.register(GmassCampaign)
+admin.site.register(ChemicalClusterOfSingaporeResult)
+admin.site.register(Fibre2FashionResult)
+admin.site.register(ZeroBounceResult)
+admin.site.register(DataSource)
+admin.site.register(SourcedEmail)
