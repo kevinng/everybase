c1a49cdf8167b4f76ad50e194d5b16dc3eb3a987
Kevin Ng <kevinngsw@gmail.com>
2020-10-16 23:16:06 +0800

    Rearrange code

--- growth/admin.py
+++ growth/admin.py
@@ -11,36 +11,6 @@ admin.site.register(ZeroBounceResult)
 admin.site.register(DataSource)
 admin.site.register(SourcedEmail)
 
-@admin.register(ChemicalClusterOfSingaporeResult)
-class ChemicalClusterOfSingaporeAdmin(admin.ModelAdmin):
-    # List page settings
-    list_display = standard_list_display + ['sourced', 'company_name',
-        'telephone', 'fax', 'website', 'source_link']
-    list_editable = standard_list_editable + ['sourced', 'company_name',
-        'telephone', 'fax', 'website', 'source_link']
-    list_per_page = 1000
-    list_filter = standard_list_filter + ['sourced']
-    search_fields = ['id', 'company_name', 'telephone', 'fax', 'website',
-        'source_link']
-    ordering = ['sourced'] + standard_ordering
-    show_full_result_count = True
-    
-    # Details page settings
-    save_on_top = True
-    readonly_fields = standard_readonly_fields
-    fieldsets = standard_fieldsets + [
-        ('Source', {'fields': ['sourced', 'source_link']}),
-        ('Result details', {
-            'fields': ['company_name', 'telephone', 'fax', 'email_str', 'website',
-                'address_str']
-        }),
-        ('Model references', {
-            'fields': ['company', 'email', 'phone_numbers', 'link', 'address']
-        })
-    ]
-    autocomplete_fields = ['company', 'email', 'phone_numbers', 'link',
-        'address']
-
 @admin.register(GmassCampaignResult)
 class GmassCampaignResultAdmin(admin.ModelAdmin):
     # List page settings
@@ -96,3 +66,33 @@ class GmassCampaign(admin.ModelAdmin):
             'spreadsheet']
         })
     ]
+
+@admin.register(ChemicalClusterOfSingaporeResult)
+class ChemicalClusterOfSingaporeAdmin(admin.ModelAdmin):
+    # List page settings
+    list_display = standard_list_display + ['sourced', 'company_name',
+        'telephone', 'fax', 'website', 'source_link']
+    list_editable = standard_list_editable + ['sourced', 'company_name',
+        'telephone', 'fax', 'website', 'source_link']
+    list_per_page = 1000
+    list_filter = standard_list_filter + ['sourced']
+    search_fields = ['id', 'company_name', 'telephone', 'fax', 'website',
+        'source_link']
+    ordering = ['sourced'] + standard_ordering
+    show_full_result_count = True
+    
+    # Details page settings
+    save_on_top = True
+    readonly_fields = standard_readonly_fields
+    fieldsets = standard_fieldsets + [
+        ('Source', {'fields': ['sourced', 'source_link']}),
+        ('Result details', {
+            'fields': ['company_name', 'telephone', 'fax', 'email_str', 'website',
+                'address_str']
+        }),
+        ('Model references', {
+            'fields': ['company', 'email', 'phone_numbers', 'link', 'address']
+        })
+    ]
+    autocomplete_fields = ['company', 'email', 'phone_numbers', 'link',
+        'address']
