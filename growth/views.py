79c007a1f57018b643f88d137e60386be0e54069
Kevin Ng <kevinngsw@gmail.com>
2020-10-22 17:24:47 +0800

    Remove GmassCampaignResultList bulk create

--- growth/views.py
+++ growth/views.py
@@ -2,18 +2,7 @@ from .models import GmassCampaignResult
 from .serializers import GmassCampaignResultSerializer
 from rest_framework import generics, permissions
 
-class BulkListCreateAPIView(generics.ListCreateAPIView):
-    def get_serializer(self, *args, **kwargs):
-        """
-        Override: if input 'data' is a list, set kwargs['many']=True, which
-        returns a list serializer instead of a normal serializer.
-        """
-        if isinstance(kwargs.get('data', {}), list):
-            kwargs['many'] = True
-
-        return super(__class__, self).get_serializer(*args, **kwargs)
-
-class GmassCampaignResultList(BulkListCreateAPIView):
+class GmassCampaignResultList(generics.ListCreateAPIView):
     queryset = GmassCampaignResult.objects.all()
     serializer_class = GmassCampaignResultSerializer
     permission_classes = [permissions.IsAuthenticated]
