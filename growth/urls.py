7782ed058fc414be672cb8e431808d999fb30cbc
Kevin Ng <kevinngsw@gmail.com>
2020-11-01 00:13:36 +0800

    Refactor growth.urls imports

--- growth/urls.py
+++ growth/urls.py
@@ -1,40 +1,57 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import (GmassCampaignResultList, GmassCampaignResultDetail,
-    GmassCampaignList, GmassCampaignDetail,
-    ChemicalClusterOfSingaporeResultList,
-    ChemicalClusterOfSingaporeResultDetail, Fibre2FashionResultList,
-    Fibre2FashionResultDetail, ZeroBounceResultList, ZeroBounceResultDetail,
-    DataSourceList, DataSourceDetail, SourcedEmailList, SourcedEmailDetail,
-    ChemicalBookResultList, ChemicalBookResultDetail, LookChemResultList,
-    LookChemResultDetail, WorldOfChemicalResultList,
-    WorldOfChemicalResultDetail)
+from . import views
 
 urlpatterns = [
-    path('gmass_campaign_result/', GmassCampaignResultList.as_view()),
+
+    path('gmass_campaign_result/',
+        views.GmassCampaignResultList.as_view()),
     path('gmass_campaign_result/<int:pk>/',
-        GmassCampaignResultDetail.as_view()),
-    path('gmass_campaign/', GmassCampaignList.as_view()),
-    path('gmass_campaign/<int:pk>/', GmassCampaignDetail.as_view()),
+        views.GmassCampaignResultDetail.as_view()),
+
+    path('gmass_campaign/',
+        views.GmassCampaignList.as_view()),
+    path('gmass_campaign/<int:pk>/',
+        views.GmassCampaignDetail.as_view()),
+
     path('chemical_cluster_of_singapore_result/',
-        ChemicalClusterOfSingaporeResultList.as_view()),
+        views.ChemicalClusterOfSingaporeResultList.as_view()),
     path('chemical_cluster_of_singapore_result/<int:pk>/',
-        ChemicalClusterOfSingaporeResultDetail.as_view()),
-    path('fibre_2_fashion_result/', Fibre2FashionResultList.as_view()),
+        views.ChemicalClusterOfSingaporeResultDetail.as_view()),
+
+    path('fibre_2_fashion_result/',
+        views.Fibre2FashionResultList.as_view()),
     path('fibre_2_fashion_result/<int:pk>/',
-        Fibre2FashionResultDetail.as_view()),
-    path('zero_bounce_result/', ZeroBounceResultList.as_view()),
-    path('zero_bounce_result/<int:pk>/', ZeroBounceResultDetail.as_view()),
-    path('data_source/', DataSourceList.as_view()),
-    path('data_source/<int:pk>/', DataSourceDetail.as_view()),
-    path('sourced_email/', SourcedEmailList.as_view()),
-    path('sourced_email/<int:pk>/', SourcedEmailDetail.as_view()),
-    path('chemical_book_result/', ChemicalBookResultList.as_view()),
-    path('chemical_book_result/<int:pk>/', ChemicalBookResultDetail.as_view()),
-    path('look_chem_result/', LookChemResultList.as_view()),
-    path('look_chem_result/<int:pk>/', LookChemResultDetail.as_view()),
-    path('world_of_chemicals_result/', WorldOfChemicalResultList.as_view()),
+        views.Fibre2FashionResultDetail.as_view()),
+
+    path('zero_bounce_result/',
+        views.ZeroBounceResultList.as_view()),
+    path('zero_bounce_result/<int:pk>/',
+        views.ZeroBounceResultDetail.as_view()),
+
+    path('data_source/',
+        views.DataSourceList.as_view()),
+    path('data_source/<int:pk>/',
+        views.DataSourceDetail.as_view()),
+
+    path('sourced_email/',
+        views.SourcedEmailList.as_view()),
+    path('sourced_email/<int:pk>/',
+        views.SourcedEmailDetail.as_view()),
+
+    path('chemical_book_result/',
+        views.ChemicalBookResultList.as_view()),
+    path('chemical_book_result/<int:pk>/',
+        views.ChemicalBookResultDetail.as_view()),
+
+    path('look_chem_result/',
+        views.LookChemResultList.as_view()),
+    path('look_chem_result/<int:pk>/',
+        views.LookChemResultDetail.as_view()),
+
+    path('world_of_chemicals_result/',
+        views.WorldOfChemicalResultList.as_view()),
     path('world_of_chemicals_result/<int:pk>/',
-        WorldOfChemicalResultDetail.as_view())
+        views.WorldOfChemicalResultDetail.as_view())
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
