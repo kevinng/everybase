48afa44c9fa5a4e1c7b863dc38012987672af860
Kevin Ng <kevinngsw@gmail.com>
2020-11-01 00:04:21 +0800

    Refactor leads.serializers

--- leads/serializers.py
+++ leads/serializers.py
@@ -2,95 +2,91 @@ from rest_framework import serializers
 from common.models import standard_fieldnames, choice_fieldnames
 from .models import (expirable_invalidable_fieldnames, lead_fieldnames,
     commission_fieldnames, quote_fieldnames)
-from .models import (Incoterm, Currency, PaymentMode, ContactType, LeadCategory,
-    MatchMethod, MatchStatus, SupplyQuoteStatus, DemandQuoteStatus,
-    UnitOfMeasure, UOMRelationship, Supply, Demand, SupplyQuote,
-    ProductionCapability, DemandQuote, Trench, Match, SupplyCommission,
-    DemandCommission)
+from . import models
 
 class IncotermSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Incoterm
+        model = models.Incoterm
         fields = ['id'] + choice_fieldnames
 
 class CurrencySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Currency
+        model = models.Currency
         fields = ['id'] + choice_fieldnames
 
 class PaymentModeSerializer(serializers.ModelSerializer):
     class Meta:
-        model = PaymentMode
+        model = models.PaymentMode
         fields = ['id'] + choice_fieldnames
 
 class ContactTypeSerializer(serializers.ModelSerializer):
     class Meta:
-        model = ContactType
+        model = models.ContactType
         fields = ['id'] + choice_fieldnames
 
 class LeadCategorySerializer(serializers.ModelSerializer):
     class Meta:
-        model = LeadCategory
+        model = models.LeadCategory
         fields = ['id'] + choice_fieldnames + ['parent']
 
 class MatchMethodSerializer(serializers.ModelSerializer):
     class Meta:
-        model = MatchMethod
+        model = models.MatchMethod
         fields = ['id'] + choice_fieldnames
 
 class MatchStatusSerializer(serializers.ModelSerializer):
     class Meta:
-        model = MatchStatus
+        model = models.MatchStatus
         fields = ['id'] + choice_fieldnames
 
 class SupplyQuoteStatusSerializer(serializers.ModelSerializer):
     class Meta:
-        model = SupplyQuoteStatus
+        model = models.SupplyQuoteStatus
         fields = ['id'] + choice_fieldnames
 
 class DemandQuoteStatusSerializer(serializers.ModelSerializer):
     class Meta:
-        model = DemandQuoteStatus
+        model = models.DemandQuoteStatus
         fields = ['id'] + choice_fieldnames
 
 class UnitOfMeasureSerializer(serializers.ModelSerializer):
     class Meta:
-        model = UnitOfMeasure
+        model = models.UnitOfMeasure
         fields = ['id'] + choice_fieldnames + ['category', 'parents']
 
 class UOMRelationshipSerializer(serializers.ModelSerializer):
     class Meta:
-        model = UOMRelationship
+        model = models.UOMRelationship
         fields = ['id', 'child', 'parent', 'multiple', 'details_md']
 
 class SupplySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Supply
+        model = models.Supply
         fields = ['id'] + standard_fieldnames + lead_fieldnames + \
             expirable_invalidable_fieldnames
 
 class DemandSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Demand
+        model = models.Demand
         fields = ['id'] + standard_fieldnames + lead_fieldnames + \
             expirable_invalidable_fieldnames
 
 class SupplyQuoteSerializer(serializers.ModelSerializer):
     class Meta:
-        model = SupplyQuote
+        model = models.SupplyQuote
         fields = ['id'] + standard_fieldnames + quote_fieldnames + \
             expirable_invalidable_fieldnames + ['supply', 'status',
             'packing_details_md', 'downstreams', 'demand_quotes']
 
 class ProductionCapabilitySerializer(serializers.ModelSerializer):
     class Meta:
-        model = ProductionCapability
+        model = models.ProductionCapability
         fields = ['id'] + standard_fieldnames + ['supply_quote', 'start',
             'end', 'capacity_quantity', 'capacity_seconds', 'details_md']
 
 class DemandQuoteSerializer(serializers.ModelSerializer):
     class Meta:
-        model = DemandQuote
+        model = models.DemandQuote
         fields = ['id'] + standard_fieldnames + quote_fieldnames + \
             expirable_invalidable_fieldnames + ['demand', 'status',
             'details_as_received_md', 'positive_origin_countries',
@@ -98,27 +94,27 @@ class DemandQuoteSerializer(serializers.ModelSerializer):
 
 class TrenchSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Trench
+        model = models.Trench
         fields = ['id', 'quantity', 'after_deposit_seconds', 'paymode',
             'payment_before_release', 'details_md', 'supply_quote',
             'demand_quote']
 
 class MatchSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Match
+        model = models.Match
         fields = ['id'] + standard_fieldnames + ['demand_quote', 'supply_quote',
             'status', 'method', 'details_md']
 
 class SupplyCommissionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = SupplyCommission
+        model = models.SupplyCommission
         fields = ['id'] + standard_fieldnames + commission_fieldnames + \
             expirable_invalidable_fieldnames + ['quotes', 'supply', 'person',
             'company']
 
 class DemandCommissionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = DemandCommission
+        model = models.DemandCommission
         fields = ['id'] + standard_fieldnames + commission_fieldnames + \
             expirable_invalidable_fieldnames + ['quotes', 'demand', 'person',
             'company']
