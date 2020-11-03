9c6f4bbd75ef5b7fb76131f564566bedc66796fe
Kevin Ng <kevinngsw@gmail.com>
2020-11-03 18:59:10 +0800

    Refactor leads.admin

--- leads/admin.py
+++ leads/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-from common.admin import (ChoiceAdmin, choice_fieldsets, choice_list_display,
-    choice_list_editable, standard_list_display, standard_list_editable,
-    standard_list_filter, standard_ordering, standard_fieldsets,
-    standard_readonly_fields)
-
 from . import models as mod
+from common import admin as commod
 
 # --- Start: Abstract configurations ---
 
@@ -39,14 +35,14 @@ _lead_search_fields = ['category', 'display_name', 'details_md', 'contact',
     mod.MatchStatus,
     mod.PaymentMode,
     mod.SupplyQuoteStatus)
-class ChoiceAdmin(ChoiceAdmin):
+class ChoiceAdmin(commod.ChoiceAdmin):
     pass
 
 @admin.register(mod.LeadCategory)
-class LeadCategoryAdmin(ChoiceAdmin):
-    list_display = choice_list_display + ['parent']
-    list_editable = choice_list_editable + ['parent']
-    fieldsets = choice_fieldsets + \
+class LeadCategoryAdmin(commod.ChoiceAdmin):
+    list_display = commod.choice_list_display + ['parent']
+    list_editable = commod.choice_list_editable + ['parent']
+    fieldsets = commod.choice_fieldsets + \
         [('Model references', {'fields': ['parent']})]
     autocomplete_fields = ['parent']
 
@@ -63,27 +59,26 @@ class UOMRelationshipParentInline(admin.StackedInline):
     verbose_name_plurals = 'Child UOM Relationships'
 
 @admin.register(mod.UnitOfMeasure)
-class UnitOfMeasureAdmin(ChoiceAdmin):
+class UnitOfMeasureAdmin(commod.ChoiceAdmin):
     # List page settings
-    list_display = choice_list_display + ['category']
-    list_editable = choice_list_editable + ['category']
+    list_display = commod.choice_list_display + ['category']
+    list_editable = commod.choice_list_editable + ['category']
     list_filter = ['category']
     search_fields = ['id', 'category', 'parents']
 
     # Details page settings
-    fieldsets = choice_fieldsets + [(None, {'fields': ['category']})]
+    fieldsets = commod.choice_fieldsets + [(None, {'fields': ['category']})]
     autocomplete_fields = ['category']
     inlines = [
         UOMRelationshipChildInline,
         UOMRelationshipParentInline
     ]
 
-_uom_relationship_fields = ['child', 'parent', 'multiple', 'details_md']
 @admin.register(mod.UOMRelationship)
 class UOMRelationshipAdmin(admin.ModelAdmin):
     # List page settings
-    list_display = ['id'] + _uom_relationship_fields
-    list_editable = _uom_relationship_fields
+    list_display = ['id', 'child', 'parent', 'multiple', 'details_md']
+    list_editable = ['child', 'parent', 'multiple', 'details_md']
     list_per_page = 1000
     list_filter = ['child', 'parent']
     search_fields = ['id', 'child', 'parent', 'details_md']
@@ -95,21 +90,21 @@ class UOMRelationshipAdmin(admin.ModelAdmin):
     readonly_fields = ['id']
     fieldsets = [
         (None, {'fields': ['id']}),
-        (None, {'fields': _uom_relationship_fields})
+        (None, {'fields': ['child', 'parent', 'multiple', 'details_md']})
     ]
     autocomplete_fields = ['child', 'parent']
 
 @admin.register(mod.Supply)
 class SupplyAdmin(admin.ModelAdmin):
     search_fields = ['id']
-    list_display = standard_list_display + \
+    list_display = commod.standard_list_display + \
         _expirable_invalidable_fields + \
         _lead_fields
-    list_editable = standard_list_editable + \
+    list_editable = commod.standard_list_editable + \
         _expirable_invalidable_fields + \
         _lead_fields
     list_per_page = 1000
-    list_filter = standard_list_filter + \
+    list_filter = commod.standard_list_filter + \
         _expirable_invalidable_filter + \
         _lead_filter
     search_fields = ['id'] + \
@@ -118,9 +113,7 @@ class SupplyAdmin(admin.ModelAdmin):
     show_full_result_count = True
 
     # Details page settings
-    save_on_top = True
-    readonly_fields = standard_readonly_fields
-    fieldsets = standard_fieldsets + \
+    fieldsets = commod.standard_fieldsets + \
         _expirable_invalidable_fieldsets + \
         _lead_fieldsets
     autocomplete_fields = _lead_autocomplete_fields
@@ -128,14 +121,14 @@ class SupplyAdmin(admin.ModelAdmin):
 @admin.register(mod.Demand)
 class DemandAdmin(admin.ModelAdmin):
     search_fields = ['id']
-    list_display = standard_list_display + \
+    list_display = commod.standard_list_display + \
         _expirable_invalidable_fields + \
         _lead_fields
-    list_editable = standard_list_editable + \
+    list_editable = commod.standard_list_editable + \
         _expirable_invalidable_fields + \
         _lead_fields
     list_per_page = 1000
-    list_filter = standard_list_filter + \
+    list_filter = commod.standard_list_filter + \
         _expirable_invalidable_filter + \
         _lead_filter
     search_fields = ['id'] + \
@@ -144,141 +137,138 @@ class DemandAdmin(admin.ModelAdmin):
     show_full_result_count = True
 
     # Details page settings
-    save_on_top = True
-    readonly_fields = standard_readonly_fields
-    fieldsets = standard_fieldsets + \
+    fieldsets = commod.standard_fieldsets + \
         _expirable_invalidable_fieldsets + \
         _lead_fieldsets
     autocomplete_fields = _lead_autocomplete_fields
 
-_demand_commission_fields = ['demand', 'person', 'company']
 @admin.register(mod.DemandCommission)
 class DemandCommissionAdmin(admin.ModelAdmin):
     # List page settings
-    list_display = standard_list_display + _demand_commission_fields
-    list_editable = standard_list_editable + _demand_commission_fields
+    list_display = commod.standard_list_display + \
+        ['demand', 'person', 'company']
+    list_editable = commod.standard_list_editable + \
+        ['demand', 'person', 'company']
     list_per_page = 1000
-    list_filter = standard_list_filter + _demand_commission_fields
-    search_fields = ['id'] + _demand_commission_fields
-    ordering = standard_ordering
+    list_filter = commod.standard_list_filter + ['demand', 'person', 'company']
+    search_fields = ['id', 'demand', 'person', 'company']
+    ordering = commod.standard_ordering
     show_full_result_count = True
 
     # Details page settings
-    save_on_top = True
-    readonly_fields = standard_readonly_fields
-    fieldsets = standard_fieldsets + [
-        (None, {'fields': ['quotes'] + _demand_commission_fields})]
-    autocomplete_fields = ['quotes'] + _demand_commission_fields
+    fieldsets = commod.standard_fieldsets + [
+        (None, {'fields': ['quotes', 'demand', 'person', 'company']})]
+    autocomplete_fields = ['quotes', 'demand', 'person', 'company']
 
-_demand_quote_fields = ['demand', 'status', 'details_as_received_md',
-    'negative_details_md']
 @admin.register(mod.DemandQuote)
 class DemandQuoteAdmin(admin.ModelAdmin):
     # List page settings
-    list_display = standard_list_display + _demand_quote_fields
-    list_editable = standard_list_editable + _demand_quote_fields
+    list_display = commod.standard_list_display + ['demand', 'status',
+        'details_as_received_md', 'negative_details_md']
+    list_editable = commod.standard_list_editable + ['demand', 'status',
+        'details_as_received_md', 'negative_details_md']
     list_per_page = 1000
-    list_filter = standard_list_filter + ['demand', 'status']
+    list_filter = commod.standard_list_filter + ['demand', 'status']
     search_fields = ['id', 'positive_origin_countries',
-        'negative_origin_countries'] + _demand_quote_fields
-    ordering = standard_ordering
+        'negative_origin_countries'] + ['demand', 'status',
+        'details_as_received_md', 'negative_details_md']
+    ordering = commod.standard_ordering
     show_full_result_count = True
 
     # Details page settings
-    save_on_top = True
-    readonly_fields = standard_readonly_fields
-    fieldsets = standard_fieldsets + [
+    fieldsets = commod.standard_fieldsets + [
         (None, {'fields': ['positive_origin_countries',
-            'negative_origin_countries'] + _demand_quote_fields})
+            'negative_origin_countries', 'demand', 'status',
+            'details_as_received_md', 'negative_details_md']})
     ]
     autocomplete_fields = ['demand', 'status', 'positive_origin_countries',
         'negative_origin_countries']
 
-_supply_commission_fields = ['supply', 'person', 'company']
 @admin.register(mod.SupplyCommission)
 class SupplyCommissionAdmin(admin.ModelAdmin):
     # List page settings
-    list_display = standard_list_display + _supply_commission_fields
-    list_editable = standard_list_editable + _supply_commission_fields
+    list_display = commod.standard_list_display + \
+        ['supply', 'person', 'company']
+    list_editable = commod.standard_list_editable + \
+        ['supply', 'person', 'company']
     list_per_page = 1000
-    list_filter = standard_list_filter + _supply_commission_fields
-    search_fields = ['id'] + _supply_commission_fields
-    ordering = standard_ordering
+    list_filter = commod.standard_list_filter + ['supply', 'person', 'company']
+    search_fields = ['id', 'supply', 'person', 'company']
+    ordering = commod.standard_ordering
     show_full_result_count = True
 
     # Details page settings
-    save_on_top = True
-    readonly_fields = standard_readonly_fields
-    fieldsets = standard_fieldsets + \
-        [(None, {'fields': ['quotes'] + _supply_commission_fields})]
-    autocomplete_fields = ['quotes'] + _supply_commission_fields
+    fieldsets = commod.standard_fieldsets + \
+        [(None, {'fields': ['quotes', 'supply', 'person', 'company']})]
+    autocomplete_fields = ['quotes', 'supply', 'person', 'company']
 
-_supply_quote_fields = ['supply', 'status', 'packing_details_md']
 @admin.register(mod.SupplyQuote)
 class SupplyQuoteAdmin(admin.ModelAdmin):
     # List page settings
-    list_display = standard_list_display + _supply_quote_fields
-    list_editable = standard_list_editable + _supply_quote_fields
+    list_display = commod.standard_list_display + ['supply', 'status',
+        'packing_details_md']
+    list_editable = commod.standard_list_editable + ['supply', 'status',
+        'packing_details_md']
     list_per_page = 1000
-    list_filter = standard_list_filter + ['status']
-    search_fields = ['id'] + _supply_quote_fields
-    ordering = standard_ordering
+    list_filter = commod.standard_list_filter + ['status']
+    search_fields = ['id'] + ['supply', 'status', 'packing_details_md']
+    ordering = commod.standard_ordering
     show_full_result_count = True
 
     # Details page settings
-    save_on_top = True
-    readonly_fields = standard_readonly_fields
-    fieldsets = standard_fieldsets + \
-        [('Details', {'fields': _supply_quote_fields + ['downstreams']})]
+    fieldsets = commod.standard_fieldsets + \
+        [('Details', {'fields': ['supply', 'status', 'packing_details_md',
+            'downstreams']})]
     autocomplete_fields = ['supply', 'status', 'downstreams']
 
-_match_details = ['demand_quote', 'supply_quote', 'status', 'method',
-    'details_md']
 @admin.register(mod.Match)
 class MatchAdmin(admin.ModelAdmin):
     # List page settings
-    list_display = standard_list_display + _match_details
-    list_editable = standard_list_editable + _match_details
+    list_display = commod.standard_list_display + \
+        ['demand_quote', 'supply_quote', 'status', 'method', 'details_md']
+    list_editable = commod.standard_list_editable + \
+        ['demand_quote', 'supply_quote', 'status', 'method', 'details_md']
     list_per_page = 1000
-    list_filter = standard_list_filter + ['status', 'method']
-    search_fields = ['id'] + _match_details
-    ordering = standard_ordering
+    list_filter = commod.standard_list_filter + ['status', 'method']
+    search_fields = ['id', 'demand_quote', 'supply_quote', 'status', 'method',
+        'details_md']
+    ordering = commod.standard_ordering
     show_full_result_count = True
 
     # Details page settings
-    save_on_top = True
-    readonly_fields = standard_readonly_fields
-    fieldsets = standard_fieldsets + \
-        [('Match details', {'fields': _match_details})]
+    fieldsets = commod.standard_fieldsets + \
+        [('Match details', {'fields': ['demand_quote', 'supply_quote', 'status',
+            'method', 'details_md']})]
     autocomplete_fields = ['demand_quote', 'supply_quote', 'status', 'method']
 
-_production_capability_fields = ['supply_quote', 'details_md', 'start', 'end',
-    'capacity_quantity', 'capacity_seconds']
 @admin.register(mod.ProductionCapability)
 class ProductionCapabilityAdmin(admin.ModelAdmin):
     # List page settings
-    list_display = standard_list_display + _production_capability_fields
-    list_editable = standard_list_editable + _production_capability_fields
+    list_display = commod.standard_list_display + ['supply_quote', \
+        'details_md', 'start', 'end', 'capacity_quantity', 'capacity_seconds']
+    list_editable = commod.standard_list_editable + ['supply_quote', \
+        'details_md', 'start', 'end', 'capacity_quantity', 'capacity_seconds']
     list_per_page = 1000
-    list_filter = standard_list_filter + ['start', 'end']
+    list_filter = commod.standard_list_filter + ['start', 'end']
     search_fields = ['id', 'supply_quote', 'details_md']
-    ordering = standard_ordering
+    ordering = commod.standard_ordering
     show_full_result_count = True
 
     # Details page settings
-    save_on_top = True
-    readonly_fields = standard_readonly_fields
-    fieldsets = standard_fieldsets + \
-        [(None, {'fields': _production_capability_fields})]
+    fieldsets = commod.standard_fieldsets + \
+        [(None, {'fields': ['supply_quote', 'details_md', 'start', 'end',
+            'capacity_quantity', 'capacity_seconds']})]
     autocomplete_fields = ['supply_quote']
 
-_trench_fields = ['supply_quote', 'demand_quote', 'quantity',
-    'after_deposit_seconds', 'paymode', 'payment_before_release', 'details_md']
 @admin.register(mod.Trench)
 class TrenchAdmin(admin.ModelAdmin):
     # List page settings
-    list_display = ['id'] + _trench_fields
-    list_editable = _trench_fields
+    list_display = ['id', 'supply_quote', 'demand_quote', 'quantity',
+        'after_deposit_seconds', 'paymode', 'payment_before_release',
+        'details_md']
+    list_editable = ['supply_quote', 'demand_quote', 'quantity',
+        'after_deposit_seconds', 'paymode', 'payment_before_release',
+        'details_md']
     list_per_page = 1000
     list_filter = ['paymode', 'payment_before_release']
     search_fields = ['id', 'supply_quote', 'demand_quote', 'details_md']
@@ -287,5 +277,7 @@ class TrenchAdmin(admin.ModelAdmin):
 
     # Details page settings
     save_on_top = True
-    fieldsets = [(None, {'fields': _trench_fields})]
+    fieldsets = [(None, {'fields': ['supply_quote', 'demand_quote', 'quantity',
+        'after_deposit_seconds', 'paymode', 'payment_before_release',
+        'details_md']})]
     autocomplete_fields = ['supply_quote', 'demand_quote', 'paymode']
