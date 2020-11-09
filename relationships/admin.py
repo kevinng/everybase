9379ae096541346063e6c5c50378c292c826f871
Kevin Ng <kevinngsw@gmail.com>
2020-11-09 13:12:59 +0800

    Configure InvalidEmail admin

--- relationships/admin.py
+++ relationships/admin.py
@@ -92,6 +92,25 @@ class EmailAdmin(admin.ModelAdmin):
     ]
     autocomplete_fields = ['import_job']
 
+@admin.register(mod.InvalidEmail)
+class InvalidEmailAdmin(admin.ModelAdmin):
+    # List page settings
+    list_display = comadm.standard_list_display + ['email', 'import_job']
+    list_editable = comadm.standard_list_editable + ['email', 'import_job']
+    list_per_page = 1000
+    list_filter = comadm.standard_list_filter
+    search_fields = ['id', 'email', 'import_job']
+    ordering = comadm.standard_ordering
+    show_full_result_count = True
+
+    # Details page settings
+    save_on_top = True
+    readonly_fields = comadm.standard_readonly_fields
+    fieldsets = comadm.standard_fieldsets + [
+        (None, {'fields': ['email', 'import_job']})
+    ]
+    autocomplete_fields = ['import_job']
+
 _person_fields = ['given_name', 'family_name', 'country', 'state', 'notes_md']
 @admin.register(mod.Person)
 class PersonAdmin(admin.ModelAdmin):
