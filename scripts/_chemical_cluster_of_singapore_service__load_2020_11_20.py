1e59f1cb2d0ef6102b9858fde56f8d02d4e441ab
Kevin Ng <kevinngsw@gmail.com>
2020-11-20 23:08:21 +0800

    Script to load ChemicalClusterOfSingaporeService

---
+++ scripts/_chemical_cluster_of_singapore_service__load_2020_11_20.py
@@ -0,0 +1,21 @@
+import pytz
+from datetime import datetime
+from .shared import helpers
+from growth.models import ChemicalClusterOfSingaporeService
+
+_NAMESPACE = 'chemical_cluster_of_singapore_service'
+
+def parse_row(row, import_job):
+
+    service = ChemicalClusterOfSingaporeService(
+        import_job=import_job,
+        harvested=datetime.now(pytz.timezone('Asia/Singapore')),
+        source_url=helpers.clean_string(row.get('url', None)),
+        company_name=helpers.clean_string(row.get('name', None)),
+        service=helpers.clean_string(row.get('service', None))
+    )
+    service.full_clean()
+    service.save()
+    
+def run():
+    helpers.load(parse_row, _NAMESPACE)
