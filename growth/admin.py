bcbaf66527d9dc17ec97eba5fe7f6bb65890ba1c
Kevin Ng <kevinngsw@gmail.com>
2020-11-20 23:44:38 +0800

    Finetune Fibre2FashionBuyingOffer admin

--- growth/admin.py
+++ growth/admin.py
@@ -131,14 +131,10 @@ class ChemicalClusterOfSingaporeServiceAdmin(admin.ModelAdmin):
 @admin.register(mod.Fibre2FashionBuyingOffer)
 class Fibre2FashionBuyingOfferAdmin(admin.ModelAdmin):
     # List page settings
-    list_display = comadm.standard_list_display + ['import_job', 'harvested',
-        'source_url', 'category', 'sub_category', 'title', 'reference_no',
-        'description', 'email_str', 'product_info_html', 'email',
-        'invalid_email']
-    list_editable = comadm.standard_list_editable + ['harvested',
-        'source_url', 'category', 'sub_category', 'title', 'reference_no',
-        'description', 'email_str', 'product_info_html', 'email',
-        'invalid_email']
+    list_display = comadm.standard_list_display + ['email',
+        'invalid_email', 'import_job', 'harvested', 'source_url', 'category',
+        'sub_category', 'title', 'reference_no', 'description', 'email_str',
+        'product_info_html']
     list_per_page = 50
     list_filter = comadm.standard_list_filter + ['import_job', 'harvested',
         'category', 'sub_category']
