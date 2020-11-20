a653154796c92a6d5657fed47e5afd99dd3e7e06
Kevin Ng <kevinngsw@gmail.com>
2020-11-21 01:33:26 +0800

    Finetune ZeroBounceResult admin

--- growth/admin.py
+++ growth/admin.py
@@ -185,16 +185,10 @@ class Fibre2FashionSellingOfferAdmin(admin.ModelAdmin):
 @admin.register(mod.ZeroBounceResult)
 class ZeroBounceResultAdmin(admin.ModelAdmin):
     # List page settings
-    list_display = comadm.standard_list_display + ['import_job', 'generated',
-        'email_str', 'status', 'sub_status', 'account', 'domain',
-        'first_name', 'last_name', 'gender', 'free_email', 'mx_found',
-        'mx_record', 'smtp_provider', 'did_you_mean', 'email', 'invalid_email',
-        'did_you_mean_email']
-    list_editable = comadm.standard_list_editable + ['generated',
-        'email_str', 'status', 'sub_status', 'account', 'domain',
-        'first_name', 'last_name', 'gender', 'free_email', 'mx_found',
-        'mx_record', 'smtp_provider', 'did_you_mean', 'email', 'invalid_email',
-        'did_you_mean_email']
+    list_display = comadm.standard_list_display + ['email', 'invalid_email',
+        'did_you_mean_email', 'import_job', 'generated', 'email_str', 'status',
+        'sub_status', 'account', 'domain', 'first_name', 'last_name', 'gender',
+        'free_email', 'mx_found', 'mx_record', 'smtp_provider', 'did_you_mean']
     list_per_page = 50
     list_filter = comadm.standard_list_filter + ['import_job', 'generated',
         'status', 'sub_status', 'gender', 'free_email', 'mx_found']
