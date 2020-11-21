c2c62e7bdd6afed1d526e6a6436f3ca89e276ba1
Kevin Ng <kevinngsw@gmail.com>
2020-11-21 13:55:40 +0800

    Script to fix timestamps on all growth models

--- scripts/2020_11_21_fix_harvested_timestamps.py
+++ scripts/2020_11_21_fix_harvested_timestamps.py
@@ -1,48 +1,36 @@
 import pytz
 from datetime import datetime
-from .shared import helpers
-from growth.models import ZeroBounceResult
+from growth import models
 
-_NAMESPACE = 'zero_bounce_result'
-
-def parse_row(row, import_job):
-
-    email_str = helpers.clean_string(row.get('Email Address', None))
-    (email, invalid_email) = helpers.record_email(email_str, import_job)
-
-    did_you_mean = helpers.clean_string(row.get('ZB Did You Mean', None))
-    (did_you_mean_email, _) = helpers.record_email(did_you_mean, import_job)
-
-    free_email = True if helpers.clean_string(row.get('ZB Free Email', None)) \
-        == 'true' else False
-
-    mx_found = True if helpers.clean_string(row.get('ZB MX Found', None)) \
-        == 'true' else False
-
+def run():
     sgtz = pytz.timezone('Asia/Singapore')
 
-    result = ZeroBounceResult(
-        import_job=import_job,
-        generated=datetime(2020, 3, 9, tzinfo=sgtz),
-        email_str=email_str,
-        status=helpers.clean_string(row.get('ZB Status', None)),
-        sub_status=helpers.clean_string(row.get('ZB Sub Status', None)),
-        account=helpers.clean_string(row.get('ZB Account', None)),
-        domain=helpers.clean_string(row.get('ZB Domain', None)),
-        first_name=helpers.clean_string(row.get('ZB First Name', None)),
-        last_name=helpers.clean_string(row.get('ZB Last Name', None)),
-        gender=helpers.clean_string(row.get('ZB Gender', None)),
-        free_email=free_email,
-        mx_found=mx_found,
-        mx_record=helpers.clean_string(row.get('ZB MX Record', None)),
-        smtp_provider=helpers.clean_string(row.get('ZB SMTP Provider', None)),
-        did_you_mean=did_you_mean,
-        email=email,
-        invalid_email=invalid_email,
-        did_you_mean_email=did_you_mean_email
-    )
-    result.full_clean()
-    result.save()
-    
-def run():
-    helpers.load(parse_row, _NAMESPACE)
+    models.ChemicalBookSupplier.objects.all().update(
+        harvested=datetime(2020, 3, 8, tzinfo=sgtz))
+
+    models.ChemicalClusterOfSingaporeCompany.objects.all().update(
+        harvested=datetime(2020, 2, 28, tzinfo=sgtz))
+
+    models.ChemicalClusterOfSingaporeProduct.objects.all().update(
+        harvested=datetime(2020, 2, 28, tzinfo=sgtz))
+
+    models.ChemicalClusterOfSingaporeService.objects.all().update(
+        harvested=datetime(2020, 2, 28, tzinfo=sgtz))
+
+    models.Fibre2FashionBuyingOffer.objects.all().update(
+        harvested=datetime(2020, 3, 3, tzinfo=sgtz))
+
+    models.Fibre2FashionSellingOffer.objects.all().update(
+        harvested=datetime(2020, 2, 29, tzinfo=sgtz))
+
+    models.LookChemSupplier.objects.all().update(
+        harvested=datetime(2020, 3, 9, tzinfo=sgtz))
+
+    models.OKChemBuyingRequest.objects.all().update(
+        harvested=datetime(2020, 2, 24, tzinfo=sgtz))
+
+    models.WorldOfChemicalsSupplier.objects.all().update(
+        harvested=datetime(2020, 3, 8, tzinfo=sgtz))
+
+    models.ZeroBounceResult.objects.all().update(
+        generated=datetime(2020, 3, 9, tzinfo=sgtz))
