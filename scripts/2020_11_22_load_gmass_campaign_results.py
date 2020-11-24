105a421a1fd76d1fa458fbb40d6b125d7fedb776
Kevin Ng <kevinngsw@gmail.com>
2020-11-24 15:59:01 +0800

    Script to update Gmass data

--- scripts/2020_11_22_load_gmass_campaign_results.py
+++ scripts/2020_11_22_load_gmass_campaign_results.py
@@ -1,29 +1,4 @@
-import requests
-from growth.models import GmassCampaign
 from growth import tasks
 
 def run():
-    # id = '63d7d845-a384-4f24-8cdd-aad77f1e360f'
-    # load_gmass_campaign_results.delay(id)
-    # load_gmass_campaign_results(id)
-
-    campaign = GmassCampaign.objects.get(pk=3)
-    # tasks.load_gmass_campaign_main_report(campaign)
-
-    # tasks.load_gmass_account_bounces(campaign)
-
-    tasks.load_gmass_account_unsubscribes(campaign)
-
-
-
-    
-
-    
-
-    
-
-    # with open('./scripts/out.csv', 'r') as file:
-    #     reader = csv.DictReader(file)
-    #     print(reader.fieldnames)
-
-    # print(data)
+    tasks.update_gmass_data()
