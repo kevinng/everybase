9bdcb125405c75b6f9b3ad3a43f9f45563dcb63a
Kevin Ng <kevinngsw@gmail.com>
2020-11-26 23:01:56 +0800

    Finetune relationships admin

--- relationships/admin.py
+++ relationships/admin.py
@@ -79,7 +79,7 @@ class EmailAdmin(admin.ModelAdmin):
     list_display = comadm.standard_list_display + ['email', 'import_job']
     list_editable = comadm.standard_list_editable + ['email']
     list_per_page = 50
-    list_filter = comadm.standard_list_filter + ['import_job']
+    list_filter = ['import_job'] + comadm.standard_list_filter
     search_fields = ['id', 'email']
     ordering = comadm.standard_ordering
     show_full_result_count = True
@@ -98,7 +98,7 @@ class InvalidEmailAdmin(admin.ModelAdmin):
     list_display = comadm.standard_list_display + ['email', 'import_job']
     list_editable = comadm.standard_list_editable + ['email']
     list_per_page = 50
-    list_filter = comadm.standard_list_filter + ['import_job']
+    list_filter = ['import_job'] + comadm.standard_list_filter
     search_fields = ['id', 'email']
     ordering = comadm.standard_ordering
     show_full_result_count = True
