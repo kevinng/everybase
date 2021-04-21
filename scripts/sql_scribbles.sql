57c9717ad62c2b9aae4086070a5fd8fd2a492096
Kevin Ng <kevinngsw@gmail.com>
2021-04-21 23:46:18 +0800

    Update SQL scribbles

--- scripts/sql_scribbles.sql
+++ scripts/sql_scribbles.sql
@@ -14,10 +14,15 @@ WHERE
 relationships_accessedurl.user_id = relationships_user.id AND
 relationships_user.email_id > 1000;
 
-
 DELETE FROM relationships_useripdevice_accessed_urls
 USING relationships_accessedurl, relationships_user
 WHERE
 relationships_useripdevice_accessed_urls.accessedurl_id = relationships_accessedurl.id AND
 relationships_accessedurl.user_id = relationships_user.id AND
+relationships_user.email_id > 1000;
+
+DELETE FROM relationships_supply
+USING relationships_user
+WHERE
+relationships_supply.user_id = relationships_user.id AND
 relationships_user.email_id > 1000;
