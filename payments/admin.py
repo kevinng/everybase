70dd0d126060b6f119bf7a5220d8bf4773dee671
Kevin Ng <kevinngsw@gmail.com>
2021-04-25 18:03:21 +0800

    Create payments app, models, migrations, admins

--- payments/admin.py
+++ payments/admin.py
@@ -3,6 +3,52 @@ from django.contrib import admin
 from . import models as mod
 from common import admin as comadm
 
+_stripe_session_fields = ['started', 'succeeded', 'failed', 'session_id',
+    'currency', 'unit_amount']
+@admin.register(mod.StripeSession)
+class StripeSession(admin.ModelAdmin):
+    # List page settings
+    list_display = comadm.standard_list_display + _stripe_session_fields
+    list_editable = comadm.standard_list_editable + _stripe_session_fields
+    list_per_page = 50
+    list_filter = comadm.standard_list_filter + ['started', 'succeeded',
+        'failed', 'currency']
+    search_fields = comadm.standard_search_fields + ['session_id']
+    ordering = comadm.standard_ordering
+    show_full_result_count = True
+
+    # Details page settings
+    save_on_top = True
+    readonly_fields = comadm.standard_readonly_fields
+    fieldsets = comadm.standard_fieldsets + [
+        ('Details', {'fields': _stripe_session_fields})
+    ]
+
+_payment_event_fields = ['event_type', 'currency', 'amount', 'user', 'match',
+    'session']
+@admin.register(mod.PaymentEvent)
+class PaymentEventAdmin(admin.ModelAdmin):
+    # List page settings
+    list_display = comadm.standard_list_display + _payment_event_fields
+    list_editable = comadm.standard_list_editable + _payment_event_fields
+    list_per_page = 50
+    list_filter = comadm.standard_list_filter + ['event_type', 'currency']
+    search_fields = comadm.standard_search_fields + ['user__name']
+    ordering = comadm.standard_ordering
+    show_full_result_count = True
+
+    # Details page settings
+    save_on_top = True
+    readonly_fields = comadm.standard_readonly_fields
+    fieldsets = comadm.standard_fieldsets + [
+        ('Details', {'fields': _payment_event_fields})
+    ]
+    autocomplete_fields = ['event_type', 'currency', 'user', 'match', 'session']
+
+@admin.register(mod.PaymentEventType)
+class PaymentEventTypeAdmin(comadm.ChoiceAdmin):
+    pass 
+
 @admin.register(mod.Currency)
 class CurrencyAdmin(comadm.ChoiceAdmin):
     pass
--- payments/models.py
+++ payments/models.py
@@ -1,7 +1,103 @@
 from django.db import models
 from common.models import Standard, Choice
 
+class StripeSession(Standard):
+    """Stripe session.
+
+    Last updated: 25 April 2021, 4:12 PM
+    """
+    started = models.DateTimeField(
+        null=True,
+        blank=True,
+        db_index=True
+    )
+    succeeded = models.DateTimeField(
+        null=True,
+        blank=True,
+        db_index=True
+    )
+    failed = models.DateTimeField(
+        null=True,
+        blank=True,
+        db_index=True
+    )
+    session_id = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True,
+        db_index=True        
+    )
+    currency = models.ForeignKey(
+        'Currency',
+        null=True,
+        blank=True,
+        related_name='stripe_sessions',
+        related_query_name='stripe_sessions',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+    unit_amount = models.FloatField(
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+class PaymentEvent(Standard):
+    """Payment event.
+
+    Last updated: 25 April 2021, 3:45 PM
+    """
+    event_type = models.ForeignKey(
+        'PaymentEventType',
+        related_name='payment_events',
+        related_query_name='payment_events',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+    currency = models.ForeignKey(
+        'Currency',
+        related_name='payment_events',
+        related_query_name='payment_events',
+        on_delete=models.PROTECT,
+        db_index=True
+    )	
+    amount = models.FloatField(db_index=True)
+	
+    user = models.ForeignKey(
+        'relationships.User',
+        related_name='payment_events',
+        related_query_name='payment_events',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+    match = models.ForeignKey(
+        'relationships.Match',
+        related_name='payment_events',
+        related_query_name='payment_events',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+    session = models.ForeignKey(
+        'StripeSession',
+        related_name='payment_events',
+        related_query_name='payment_events',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+
+class PaymentEventType(Choice):
+    """Payment event type.
+
+    Last updated: 25 April 2021, 4:11 PM
+    """
+    pass
+
 class Currency(Choice):
+    """Currency. E.g., USD.
+
+    Last updated: 23 April 2021, 6:52 PM
+    """
+
     class Meta:
         verbose_name = 'Currency'
         verbose_name_plural = 'Currencies'
