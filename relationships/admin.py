d4fa6ba071f9a740151dc43dfad92bfc6cb9dc9f
Kevin Ng <kevinngsw@gmail.com>
2021-04-27 15:22:41 +0800

    Refactor CompanyProductTypeAdmin
    Extend class from StandardAdmin

--- relationships/admin.py
+++ relationships/admin.py
@@ -110,22 +110,17 @@ class ProductTypeAdmin(comadm.StandardChoiceAdmin):
 
 _company_product_type_fields = ['popularity', 'company', 'product_type']
 @admin.register(mod.CompanyProductType)
-class CompanyProductTypeAdmin(admin.ModelAdmin):
+class CompanyProductTypeAdmin(comadm.StandardAdmin):
     # List page settings
     list_display = comadm.standard_list_display + \
         _company_product_type_fields
     list_editable = comadm.standard_list_editable + \
         _company_product_type_fields
-    list_per_page = 50
     list_filter = comadm.standard_list_filter + ['product_type']
     search_fields = comadm.standard_search_fields + ['company__display_name',
         'company__notes', 'product_type__name', 'product_type__description']
-    ordering = comadm.standard_ordering
-    show_full_result_count = True
 
     # Details page settings
-    save_on_top = True
-    readonly_fields = comadm.standard_readonly_fields
     fieldsets = comadm.standard_fieldsets + [
         ('Details', {'fields': _company_product_type_fields})
     ]
