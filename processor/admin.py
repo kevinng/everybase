a17e8c213a498dde4b23e7ed6516f34b3cf330be
Kevin Ng <kevinngsw@gmail.com>
2021-04-27 21:07:16 +0800

    Extend TestMessageGroupAdmin from StandardAdmin

--- processor/admin.py
+++ processor/admin.py
@@ -5,19 +5,13 @@ from common import admin as comadm
 
 _message_fields = ['body']
 @admin.register(mod.TestMessageGroup)
-class TestMessageGroupAdmin(admin.ModelAdmin):
+class TestMessageGroupAdmin(comadm.StandardAdmin):
     # List page settings
     list_display = comadm.standard_list_display + _message_fields
     list_editable = comadm.standard_list_editable + _message_fields
-    list_per_page = 50
-    list_filter = comadm.standard_list_filter
     search_fields = comadm.standard_search_fields + _message_fields
-    ordering = comadm.standard_ordering
-    show_full_result_count = True
 
     # Details page settings
-    save_on_top = True
-    readonly_fields = comadm.standard_readonly_fields
     fieldsets = comadm.standard_fieldsets + [
         ('Details', {'fields': _message_fields})
     ]
