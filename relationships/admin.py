fc30057a8681eda90b1d34f6ee13701ab29b3afa
Kevin Ng <kevinngsw@gmail.com>
2021-04-27 14:55:09 +0800

    Refactor PaymentTermAdmin
    Extend StandardChoiceAdmin and override relevant fields

--- relationships/admin.py
+++ relationships/admin.py
@@ -281,6 +281,19 @@ class IncotermAvailabilityAdmin(comadm.StandardChoiceAdmin):
 class LocationAdmin(comadm.StandardChoiceAdmin):
     pass
 
+_payment_term_fields = ['supply_quote', 'demand_quote']
+@admin.register(mod.PaymentTerm)
+class PaymentTermAdmin(comadm.StandardChoiceAdmin):
+    # List page settings
+    list_display = comadm.standard_choice_list_display + _payment_term_fields
+    list_editable = comadm.standard_choice_list_editable + _payment_term_fields
+
+    # Details page settings
+    fieldsets = comadm.standard_choice_fieldsets + [
+        ('Quotes', {'fields': _payment_term_fields})
+    ]
+    autocomplete_fields = _payment_term_fields
+
 _lead_fields = ['product_type', 'company', 'product', 'user']
 _lead_search_fields = ['product_type__name', 'product_type__description',
     'company__display_name', 'company__notes', 'product__display_name',
@@ -324,29 +337,6 @@ class DemandAdmin(admin.ModelAdmin):
     ]
     autocomplete_fields = ['product_type', 'company', 'product', 'user']
 
-<<<<<<< HEAD
-=======
-_payment_term_fields = ['supply_quote', 'demand_quote']
-@admin.register(mod.PaymentTerm)
-class PaymentTermAdmin(admin.ModelAdmin):
-    # List page settings
-    list_display = comadm.standard_choice_list_display + _payment_term_fields
-    list_editable = comadm.standard_choice_list_editable + _payment_term_fields
-    list_per_page = 50
-    list_filter = comadm.standard_choice_list_filter
-    search_fields = comadm.standard_choice_search_fields
-    ordering = comadm.standard_ordering
-    show_full_result_count = True
-
-    # Details page settings
-    save_on_top = True
-    readonly_fields = comadm.standard_choice_readonly_fields
-    fieldsets = comadm.standard_choice_fieldsets + [
-        ('Details', {'fields': _payment_term_fields})
-    ]
-    autocomplete_fields = _payment_term_fields
-
->>>>>>> eb545df94d182a999de546f87693d1e80547a004
 _unit_of_measure_fields = ['plural_name', 'product_type']
 @admin.register(mod.UnitOfMeasure)
 class UnitOfMeasureAdmin(admin.ModelAdmin):
