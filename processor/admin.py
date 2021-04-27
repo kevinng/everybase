333ab5d5cd0b9e0c3d01fe4efc3aeeb363525c47
Kevin Ng <kevinngsw@gmail.com>
2021-04-27 21:07:48 +0800

    Extend BaseTruthAdmin from StandardAdmin

--- processor/admin.py
+++ processor/admin.py
@@ -19,21 +19,16 @@ class TestMessageGroupAdmin(comadm.StandardAdmin):
 _base_truth_fields = ['message_group', 'method', 'text_output',
     'integer_output', 'float_output']
 @admin.register(mod.BaseTruth)
-class BaseTruthAdmin(admin.ModelAdmin):
+class BaseTruthAdmin(comadm.StandardAdmin):
     # List page settings
     list_display = comadm.standard_list_display + _base_truth_fields
     list_editable = comadm.standard_list_editable + _base_truth_fields
-    list_per_page = 50
     list_filter = comadm.standard_list_filter + ['method']
     search_fields = comadm.standard_search_fields + ['message_group__body',
         'method__name', 'method__description', 'text_output',
         'integer_output', 'float_output']
-    ordering = comadm.standard_ordering
-    show_full_result_count = True
 
     # Details page settings
-    save_on_top = True
-    readonly_fields = comadm.standard_readonly_fields
     fieldsets = comadm.standard_fieldsets + [
         ('Details', {'fields': _base_truth_fields})
     ]
