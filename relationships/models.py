df638ee88c28a2a3bf9ff77a9152e2c9c63107b8
Kevin Ng <kevinngsw@gmail.com>
2021-05-12 19:19:21 +0800

    Add all-new relationships models

--- relationships/models.py
+++ relationships/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 from common.models import (Standard, Choice, LowerCaseCharField,
     LowerCaseEmailField)
 from django.core.exceptions import ValidationError
@@ -328,3 +329,458 @@ class UserIPDevice(Standard):
     class Meta:
         verbose_name = 'User IP-Device'
         verbose_name_plural = 'User IP-Devices'
+
+class UnitOfMeasure(Standard, Choice):
+    """Unit of measure. Description is displayed to user.
+
+    Last updated: 12 May 2021, 1:26 PM
+    """
+    plural_name = models.CharField(
+        max_length=200,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+    product_type = models.ForeignKey(
+        'ProductType',
+        related_name='unit_of_measures',
+        related_query_name='unit_of_measures',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+
+class Availability(Choice):
+    """Availability. E.g., ready stock, OTG.
+
+    Last updated: 12 May 2021, 1:27 PM
+    """
+    pass
+
+class Country(Choice):
+    """Country.
+
+    Last updated: 12 May 2021, 1:28 PM
+    """
+    pass
+
+class State(Choice):
+    """State.
+
+    Last updated: 12 May 2021, 1:38 PM
+    """
+    country = models.ForeignKey(
+        'Country',
+        related_name='states',
+        related_query_name='states',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+
+class ProductType(Choice):
+    """Product type.
+
+    Last updated: 12 May 2021, 1:39 PM
+    """
+    pass
+
+class Connection(Choice):
+    """Connection.
+
+    Last updated: 12 May 2021, 2:35 PM
+    """
+    user_1 = models.ForeignKey(
+        'User',
+        related_name='connection_user_1s',
+        related_query_name='connection_user_1s',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+    user_2 = models.ForeignKey(
+        'User',
+        related_name='connection_user_2s',
+        related_query_name='connection_user_2s',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+
+class TimeFrame(Standard):
+    """Time frame.
+
+    Last updated: 12 May 2021, 4:03 PM
+    """
+    duration_uom = models.CharField(
+        max_length=2,
+        choices=[
+            ('d', 'Day'),
+            ('w', 'Week'),
+            ('m', 'Month')
+        ],
+        db_index=True
+    )
+    duration = models.FloatField(
+        null=True,
+        blank=True,
+        db_index=True
+    )
+    deadline = models.DateTimeField(
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    def clean(self):
+        super(TimeFrame, self).clean()
+
+        # Either supply_quote or demand_quote must be set.
+        if (self.duration_uom is None and self.duration is None) and \
+            self.deadline:
+            raise ValidationError('Either duration/uom or deadline must be \
+                set.')
+
+class Match(Choice):
+    """Match between supply and demand.
+
+    Last updated: 12 May 2021, 2:40 PM
+    """
+    supply = models.ForeignKey(
+        'Supply',
+        related_name='matches',
+        related_query_name='matches',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+    demand = models.ForeignKey(
+        'Demand',
+        related_name='matches',
+        related_query_name='matches',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+
+class Supply(Standard):
+    """Supply.
+
+    Last updated: 12 May 2021, 6:14 PM
+    """
+    user = models.ForeignKey(
+        'User',
+        related_name='supplies',
+        related_query_name='supplies',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+
+    product_type_captured = models.TextField(
+        null=True,
+        blank=True
+    )
+    product_type = models.ForeignKey(
+        'ProductType',
+        related_name='supplies',
+        related_query_name='supplies',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    country_state_captured = models.TextField(
+        null=True,
+        blank=True        
+    )
+    country = models.ForeignKey(
+        'Country',
+        related_name='supplies',
+        related_query_name='supplies',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+    state = models.ForeignKey(
+        'State',
+        related_name='supplies',
+        related_query_name='supplies',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    availability_captured = models.TextField(
+        null=True,
+        blank=True        
+    )
+    availability = models.ForeignKey(
+        'Availability',
+        related_name='supplies',
+        related_query_name='supplies',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    packing_captured = models.TextField(
+        null=True,
+        blank=True        
+    )
+    packing = models.ForeignKey(
+        'UnitOfMeasure',
+        related_name='supplies',
+        related_query_name='supplies',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    quantity_captured = models.TextField(
+        null=True,
+        blank=True
+    )
+    quantity = models.FloatField(
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    preorder_timeframe_captured = models.TextField(
+        null=True,
+        blank=True
+    )
+    preorder_timeframe = models.ForeignKey(
+        'TimeFrame',
+        related_name='supplies',
+        related_query_name='supplies',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    price_captured = models.TextField(
+        null=True,
+        blank=True
+    )
+    price = models.FloatField(
+        null=True,
+        blank=True
+    )
+    currency = models.ForeignKey(
+        'payments.Currency',
+        related_name='supplies',
+        related_query_name='supplies',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    deposit_percentage_captured = models.TextField(
+        null=True,
+        blank=True
+    )
+    deposit_percentage = models.FloatField(
+        null=True,
+        blank=True
+    )
+
+    accept_lc_captured = models.TextField(
+        null=True,
+        blank=True
+    )
+    accept_lc = models.BooleanField(
+        null=True,
+        blank=True
+    )
+
+    previous_version = models.ForeignKey(
+        'Supply',
+        related_name='supply_previous_versions',
+        related_query_name='supply_previous_versions',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+    next_version = models.ForeignKey(
+        'Supply',
+        related_name='supply_next_versions',
+        related_query_name='supply_next_versions',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+class Demand(Standard):
+    """Demand.
+
+    Last updated: 12 May 2021, 4:12 PM
+    """
+    user = models.ForeignKey(
+        'User',
+        related_name='demands',
+        related_query_name='demands',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+
+    product_type_captured = models.TextField(
+        null=True,
+        blank=True
+    )
+    product_type = models.ForeignKey(
+        'ProductType',
+        related_name='demands',
+        related_query_name='demands',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    country_state_captured = models.TextField(
+        null=True,
+        blank=True        
+    )
+    country = models.ForeignKey(
+        'Country',
+        related_name='demands',
+        related_query_name='demands',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+    state = models.ForeignKey(
+        'State',
+        related_name='demands',
+        related_query_name='demands',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    packing_captured = models.TextField(
+        null=True,
+        blank=True        
+    )
+    packing = models.ForeignKey(
+        'UnitOfMeasure',
+        related_name='demands',
+        related_query_name='demands',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    quantity_captured = models.TextField(
+        null=True,
+        blank=True
+    )
+    quantity = models.FloatField(
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    price_captured = models.TextField(
+        null=True,
+        blank=True
+    )
+    price = models.FloatField(
+        null=True,
+        blank=True
+    )
+    currency = models.ForeignKey(
+        'payments.Currency',
+        related_name='demands',
+        related_query_name='demands',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    previous_version = models.ForeignKey(
+        'Demand',
+        related_name='demand_previous_versions',
+        related_query_name='demand_previous_versions',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+    next_version = models.ForeignKey(
+        'Demand',
+        related_name='demand_next_versions',
+        related_query_name='demand_next_versions',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+class QuestionAnswerPair(Standard):
+    """Question-answer pair.
+
+    Last updated: 12 May 2021, 5:27 PM
+    """
+    asker = models.ForeignKey(
+        'User',
+        related_name='question_answer_pair_askers',
+        related_query_name='question_answer_pair_askers',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+    answerer = models.ForeignKey(
+        'User',
+        related_name='question_answer_pair_answerers',
+        related_query_name='question_answer_pair_answerers',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
+
+    asked = models.DateTimeField(
+        null=True,
+        blank=True,
+        db_index=True
+    )
+    sent_answerer = models.DateTimeField(
+        null=True,
+        blank=True,
+        db_index=True
+    )
+    answered = models.DateTimeField(
+        null=True,
+        blank=True,
+        db_index=True
+    )
+
+    question_captured = models.TextField()
+    question_rewrote = models.TextField(
+        null=True,
+        blank=True
+    )
+
+    answer_captured = models.TextField(
+        null=True,
+        blank=True,
+        db_index=True
+    )
+    answer_rewrote = models.TextField(
+        null=True,
+        blank=True
+    )
+
+    match = models.ForeignKey(
+        'Match',
+        related_name='question_answer_pairs',
+        related_query_name='question_answer_pairs',
+        on_delete=models.PROTECT,
+        db_index=True
+    )
