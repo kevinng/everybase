12640918b08ee450e5ccb8cf42cf9750e64c85ce
Kevin Ng <kevinngsw@gmail.com>
2021-05-14 14:28:45 +0800

    Refactor chat webhook url root

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
+webhook_root = 'webhooks/'
 urlpatterns = [
-    path('webhooks/', views.TwilioIncomingMessageView.as_view()),
+    path(f'{webhook_root}messages/', views.TwilioIncomingMessageView.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
