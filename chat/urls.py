17ea3976be248202f0cfa6b69fe4d2cbd83cac28
Kevin Ng <kevinngsw@gmail.com>
2021-05-14 14:30:16 +0800

    Rename Twilio incoming message url

--- chat/urls.py
+++ chat/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-webhook_root = 'webhooks/'
+webhook_root = 'webhooks'
 urlpatterns = [
-    path(f'{webhook_root}messages/', views.TwilioIncomingMessageView.as_view()),
+    path(f'{webhook_root}/message_v1_a/',
+        views.TwilioIncomingMessageView.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
