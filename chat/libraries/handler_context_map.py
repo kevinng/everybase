1543689813b4b70cfd1efa7973d220acec13c1b7
Kevin Ng <kevinngsw@gmail.com>
2021-05-25 15:13:39 +0800

    Handler-Context mapping and handlers outline
    - Define context-to-handler map in handler_context_map.py
    - Define MessageHandler class and subclass outlines in handlers.py
    - Delete deprecated contexts.py

---
+++ chat/libraries/handler_context_map.py
@@ -0,0 +1,84 @@
+"""Mapping of context to handler"""
+
+from chat.libraries import intents, messages, handlers
+
+handler_context_map = {
+    intents.SPEAK_HUMAN: {
+        messages.CONFIRM_HUMAN: handlers.SPEAK_HUMAN__CONFIRM_HUMAN
+    },
+    intents.EXPLAIN_SERVICE: {
+        messages.EXPLAIN_SERVICE: handlers.EXPLAIN_SERVICE__EXPLAIN_SERVICE
+    },
+    intents.REGISTER: {
+        messages.REGISTER__GET_NAME: handlers.REGISTER__REGISTER__GET_NAME,
+        messages.MENU: handlers.REGISTER__MENU
+    },
+    intents.NEW_SUPPLY: {
+        messages.SUPPLY__GET_PRODUCT: handlers.NEW_SUPPLY__SUPPLY__GET_PRODUCT,
+        messages.SUPPLY__GET_AVAILABILITY: handlers.NEW_SUPPLY__SUPPLY__GET_AVAILABILITY,
+        messages.SUPPLY__GET_COUNTRY_STATE: handlers.NEW_SUPPLY__SUPPLY__GET_COUNTRY_STATE,
+        messages.SUPPLY__CONFIRM_PACKING: handlers.NEW_SUPPLY__SUPPLY__CONFIRM_PACKING,
+        messages.SUPPLY__GET_PACKING: handlers.NEW_SUPPLY__SUPPLY__GET_PACKING,
+        messages.SUPPLY__GET_QUANTITY: handlers.NEW_SUPPLY__SUPPLY__GET_QUANTITY,
+        messages.SUPPLY__GET_TIMEFRAME: handlers.NEW_SUPPLY__SUPPLY__GET_TIMEFRAME,
+        messages.SUPPLY__GET_PRICE: handlers.NEW_SUPPLY__SUPPLY__GET_PRICE,
+        messages.SUPPLY__GET_DEPOSIT: handlers.NEW_SUPPLY__SUPPLY__GET_DEPOSIT,
+        messages.SUPPLY__GET_ACCEPT_LC: handlers.NEW_SUPPLY__SUPPLY__GET_ACCEPT_LC,
+        messages.SUPPLY__THANK_YOU: handlers.NEW_SUPPLY__SUPPLY__THANK_YOU
+    },
+    intents.NEW_DEMAND: {
+        messages.DEMAND__GET_PRODUCT: handlers.NEW_DEMAND__DEMAND__GET_PRODUCT,
+        messages.DEMAND__GET_COUNTRY_STATE: handlers.NEW_DEMAND__DEMAND__GET_COUNTRY_STATE,
+        messages.DEMAND__GET_QUANTITY: handlers.NEW_DEMAND__DEMAND__GET_QUANTITY,
+        messages.DEMAND__GET_PRICE: handlers.NEW_DEMAND__DEMAND__GET_PRICE,
+        messages.DEMAND__THANK_YOU: handlers.NEW_DEMAND__DEMAND__THANK_YOU
+    },
+    intents.DISCUSS_W_BUYER: {
+        messages.SUPPLY__GET_PRODUCT: handlers.DISCUSS_W_BUYER__SUPPLY__GET_PRODUCT,
+        messages.SUPPLY__GET_AVAILABILITY: handlers.DISCUSS_W_BUYER__SUPPLY__GET_AVAILABILITY,
+        messages.SUPPLY__GET_COUNTRY_STATE: handlers.DISCUSS_W_BUYER__SUPPLY__GET_COUNTRY_STATE,
+        messages.SUPPLY__CONFIRM_PACKING: handlers.DISCUSS_W_BUYER__SUPPLY__CONFIRM_PACKING,
+        messages.SUPPLY__GET_PACKING: handlers.DISCUSS_W_BUYER__SUPPLY__GET_PACKING,
+        messages.SUPPLY__GET_QUANTITY: handlers.DISCUSS_W_BUYER__SUPPLY__GET_QUANTITY,
+        messages.SUPPLY__GET_TIMEFRAME: handlers.DISCUSS_W_BUYER__SUPPLY__GET_TIMEFRAME,
+        messages.SUPPLY__GET_PRICE: handlers.DISCUSS_W_BUYER__SUPPLY__GET_PRICE,
+        messages.SUPPLY__GET_DEPOSIT: handlers.DISCUSS_W_BUYER__SUPPLY__GET_DEPOSIT,
+        messages.SUPPLY__GET_ACCEPT_LC: handlers.DISCUSS_W_BUYER__SUPPLY__GET_ACCEPT_LC,
+        messages.DISCUSS__CONFIRM_INTERESTED: handlers.DISCUSS_W_BUYER__DISCUSS__CONFIRM_INTERESTED,
+        messages.STILL_INTERESTED__CONFIRM: handlers.DISCUSS_W_BUYER__STILL_INTERESTED__CONFIRM,
+        messages.STILL_INTERESTED__THANK_YOU: handlers.DISCUSS_W_BUYER__STILL_INTERESTED__THANK_YOU,
+        messages.DISCUSS__CONFIRM_UPDATED: handlers.DISCUSS_W_BUYER__DISCUSS__CONFIRM_UPDATED,
+        messages.DISCUSS__ASK: handlers.DISCUSS_W_BUYER__DISCUSS__ASK,
+        messages.DISCUSS__THANK_YOU: handlers.DISCUSS_W_BUYER__DISCUSS__THANK_YOU,
+        messages.ALREADY_CONNECTED: handlers.DISCUSS_W_BUYER__DISCUSS__ALREADY_CONNECTED
+    },
+    intents.DISCUSS_W_SELLER: {
+        messages.DEMAND__GET_PRODUCT: handlers.DISCUSS_W_SELLER__DEMAND__GET_PRODUCT,
+        messages.DEMAND__GET_COUNTRY_STATE: handlers.DISCUSS_W_SELLER__DEMAND__GET_COUNTRY_STATE,
+        messages.DEMAND__GET_QUANTITY: handlers.DISCUSS_W_SELLER__DEMAND__GET_QUANTITY,
+        messages.DEMAND__GET_PRICE: handlers.DISCUSS_W_SELLER__DEMAND__GET_PRICE,
+        messages.DISCUSS__CONFIRM_INTERESTED: handlers.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTERESTED,
+        messages.STILL_INTERESTED__CONFIRM: handlers.DISCUSS_W_SELLER__STILL_INTERESTED__CONFIRM,
+        messages.STILL_INTERESTED__THANK_YOU: handlers.DISCUSS_W_SELLER__STILL_INTERESTED__THANK_YOU,
+        messages.DISCUSS__CONFIRM_UPDATED: handlers.DISCUSS_W_SELLER__DISCUSS__CONFIRM_UPDATED,
+        messages.DISCUSS__ASK: handlers.DISCUSS_W_SELLER__DISCUSS__ASK,
+        messages.DISCUSS__THANK_YOU: handlers.DISCUSS_W_SELLER__DISCUSS__THANK_YOU,
+        messages.ALREADY_CONNECTED: handlers.DISCUSS_W_SELLER__DISCUSS__ALREADY_CONNECTED
+    },
+    intents.STOP_DISCUSSION: {
+        messages.STOP_DISCUSSION__REASON: handlers.STOP_DISCUSSION__STOP_DISCUSSION__REASON,
+        messages.STOP_DISCUSSION__THANK_YOU: handlers.STOP_DISCUSSION__STOP_DISCUSSION__THANK_YOU
+    },
+    intents.CONNECT: {
+        messages.PLEASE_PAY: handlers.CONNECT__PLEASE_PAY,
+    },
+    intents.NO_INTENT: {
+        messages.DO_NOT_UNDERSTAND: handlers.NO_INTENT__DO_NOT_UNDERSTAND,
+        messages.MENU: handlers.NO_INTENT__MENU,
+        messages.YOUR_QUESTION: handlers.NO_INTENT__YOUR_QUESTION,
+        messages.YOUR_ANSWER: handlers.NO_INTENT__YOUR_ANSWER,
+        messages.PAYEE_CONNECTED: handlers.NO_INTENT__PAYEE_CONNECTED,
+        messages.NON_PAYEE_CONNECTED: handlers.NO_INTENT__NON_PAYEE_CONNECTED,
+        messages.NO_MESSAGE: handlers.NO_INTENT__NO_MESSAGE
+    }
+}
--- chat/libraries/handlers.py
+++ chat/libraries/handlers.py
@@ -1,169 +1,312 @@
+"""This file defines all ContextHandler sub-classes - i.e., actual
+implementation of what to do in each context.
 
+Classes are named in the format:
 
-def handle__unregistered(message):
-    """
-Actually, this can be 'in' for no context, not registered
-    """
-    pass
-
-def handle__whats_your_name(message):
-    """
-A CONTEXT IS TIED TO THIS IN METHOD
-    """
-    pass
-
-def handle__menu(message):
-    """
-    Can fan out to either choices
-
-the fanning out logic can be done here
-context switching codes can also be moved here
-
-
-
-
-
-
-
-
-
-
-A CONTEXT IS TIED TO THIS IN METHOD - in a dictionary, so we know there's just 1 context per method
-
-
-OUT METHODS ARE TIED TO CHOICES, OR if there are no choices, then we have only
-one out method
-
-
-    what if the user abandons? the context will expire after some time
-
-    what if the user has more than 1 active context? the protocol is to have
-    the user confirm which context is he referring to
-    """
-    pass
-
-# ----- Start: Flow following menu option 1 (find buyer) -----
-
-# depending on the choice of menu, I want to send the right messages
-
-def OUT__what_sell(message):
-    pass
-
-def OUT__what_sell_availability(message):
-    pass
-
-def OUT__what_sell_location(message):
-    pass
-
-def OUT__confirm_sell_packing(message):
-    pass
-
-def OUT__what_sell_packing(message):
-    pass
-
-
-# ----- End: Flow following menu option 1 (find buyer) -----
-
-
-
-# Can I have data structures
-# graph structure
-
-# a points to b
+<intent key>__<message key>
 """
 
+from chat import models
+
+class MessageHandler:
+    """A context is a unique pair of the user's intent, and the last message
+    sent by the system. A user's context may be ascertained by reading the
+    intent_key and message_key of the last outbound Twilio message sent by the
+    system.
+
+    A message handler lets us handle an incoming message using the message-
+    handling cycle:
+        - Receive message.
+        - Get user's intent and our last outbound message (i.e., context)
+        - Extract data from message based on the context. Each context has its
+            method to extract/store data from the incoming message.
+        - Validate extracted data based on the context. Each context has its
+            method to validate extracted data and store the results.
+        - If the data is invalid - reply 'do-not-understand'.
+        - If the data is valid - process input, ascertain/set next context,
+            reply with right message.
+
+    Attributes
+    ----------
+    message : TwilioInboundMessage
+        Twilio inbound message we're handling
+    """
+    message = None
+    dataset = None
+
+    # Context
+    intent_key = None
+    message_key = None
+
+    def __init__(self, message, intent_key, message_key):
+        """
+        Parameters
+        ----------
+        message : TwilioInboundMessage
+            Twilio inbound message we're handling
+        """
+        if message == None:
+            raise Exception('message cannot be null')
+
+        self.message = message
+        self.intent_key = intent_key
+        self.message_key = message_key
+
+        self.get_or_create_dataset()
+
+    def run(self):
+        """Extract/validate/store data, return reply message body, and set new
+        context.
+
+        Override.
+        """
+        return None
+
+    def get_or_create_dataset(self):
+        """Read/create dataset for this message.
+
+        Do not override.
+        """
+        self.dataset, _ = models.MessageDataset.objects.get_or_create(
+            message=self.message,
+            intent_key=self.intent_key,
+            message_key=self.message_key
+        )
+
+    def save_string(self, value, is_valid):
+        """Save extracted string for this message/context.
+
+        Do not override.
+        """
+        models.MessageDataString.objects.create(
+            dataset=self.dataset,
+            value=value,
+            is_valid=is_valid
+        )
+
+    def save_float(self, value, is_valid):
+        """Save extracted float for this message/context.
 
-lambda conditions for options is passed to each handle
-THIS MAKES IT HARDER TO DEBUG
+        Do not override.
+        """
+        models.MessageDataFloat.objects.create(
+            dataset=self.dataset,
+            value=value,
+            is_valid=is_valid
+        )
 
+    def save_boolean(self, value, is_valid):
+        """Save extracted boolean for this message/context.
 
+        Do not override.
+        """
+        models.MessageDataBoolean.objects.create(
+            dataset=self.dataset,
+            value=value,
+            is_valid=is_valid
+        )
 
+class SPEAK_HUMAN__CONFIRM_HUMAN(MessageHandler):
+    def run(self):
+        pass
 
+class EXPLAIN_SERVICE__EXPLAIN_SERVICE(MessageHandler):
+    def run(self):
+        pass
 
+# REGISTER intent
 
+class REGISTER__REGISTER__GET_NAME(MessageHandler):
+    pass
 
+class REGISTER__MENU(MessageHandler):
+    pass
 
+# DISCUSS_W_BUYER intent
 
+class DISCUSS_W_BUYER__SUPPLY__GET_PRODUCT(MessageHandler):
+    pass
 
-A CONTEXT IS TIED TO THIS IN METHOD
-OUT METHODS ARE TIED TO CHOICES, OR if there are no choices, then we have only
-one out method
+class DISCUSS_W_BUYER__SUPPLY__GET_AVAILABILITY(MessageHandler):
+    pass
 
+class DISCUSS_W_BUYER__SUPPLY__GET_COUNTRY_STATE(MessageHandler):
+    pass
 
+class DISCUSS_W_BUYER__SUPPLY__CONFIRM_PACKING(MessageHandler):
+    pass
 
-handler is configured to handle a context - one to one
-when a message comes in
-a handler has 0 or more choices
+class DISCUSS_W_BUYER__SUPPLY__GET_PACKING(MessageHandler):
+    pass
 
+class DISCUSS_W_BUYER__SUPPLY__GET_QUANTITY(MessageHandler):
+    pass
 
+class DISCUSS_W_BUYER__SUPPLY__GET_TIMEFRAME(MessageHandler):
+    pass
 
+class DISCUSS_W_BUYER__SUPPLY__GET_PRICE(MessageHandler):
+    pass
 
 
+# NEW_SUPPLY intent
 
+class NEW_SUPPLY__SUPPLY__GET_PRODUCT(MessageHandler):
+    pass
 
+class NEW_SUPPLY__SUPPLY__GET_AVAILABILITY(MessageHandler):
+    pass
 
+class NEW_SUPPLY__SUPPLY__GET_COUNTRY_STATE(MessageHandler):
+    pass
 
+class NEW_SUPPLY__SUPPLY__CONFIRM_PACKING(MessageHandler):
+    pass
 
+class NEW_SUPPLY__SUPPLY__GET_PACKING(MessageHandler):
+    pass
 
+class NEW_SUPPLY__SUPPLY__GET_QUANTITY(MessageHandler):
+    pass
 
+class NEW_SUPPLY__SUPPLY__GET_TIMEFRAME(MessageHandler):
+    pass
 
+class NEW_SUPPLY__SUPPLY__GET_PRICE(MessageHandler):
+    pass
 
+class NEW_SUPPLY__SUPPLY__GET_DEPOSIT(MessageHandler):
+    pass
 
-say, a points to b
-b points to c or d
+class NEW_SUPPLY__SUPPLY__GET_ACCEPT_LC(MessageHandler):
+    pass
 
+class NEW_SUPPLY__SUPPLY__THANK_YOU(MessageHandler):
+    pass
 
-the graph has a set of inputs to get started
-let this be the message object
 
-the graph has a starting point - say node a
+# NEW_DEMAND intent
 
-a has conditions
-b has conditions
-c has conditions
+class NEW_DEMAND__DEMAND__GET_PRODUCT(MessageHandler):
+    pass
 
+class NEW_DEMAND__DEMAND__GET_COUNTRY_STATE(MessageHandler):
+    pass
 
-a's condition is that either ph or usr is new
-b's condition is that the user has an active context
+class NEW_DEMAND__DEMAND__GET_QUANTITY(MessageHandler):
+    pass
 
+class NEW_DEMAND__DEMAND__GET_PRICE(MessageHandler):
+    pass
 
-what if 2 nodes are in conflict?, the one with the shortest path 
+class NEW_DEMAND__DEMAND__THANK_YOU(MessageHandler):
+    pass
 
 
+# DISCUSS_W_SELLER intent
 
+class DISCUSS_W_SELLER__DEMAND__GET_PRODUCT(MessageHandler):
+    pass
 
+class DISCUSS_W_SELLER__DEMAND__GET_COUNTRY_STATE(MessageHandler):
+    pass
 
+class DISCUSS_W_SELLER__DEMAND__GET_QUANTITY(MessageHandler):
+    pass
 
+class DISCUSS_W_SELLER__DEMAND__GET_PRICE(MessageHandler):
+    pass
 
+class DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTERESTED(MessageHandler):
+    pass
 
+class DISCUSS_W_SELLER__STILL_INTERESTED__CONFIRM(MessageHandler):
+    pass
 
+class DISCUSS_W_SELLER__STILL_INTERESTED__THANK_YOU(MessageHandler):
+    pass
 
+class DISCUSS_W_SELLER__DISCUSS__CONFIRM_UPDATED(MessageHandler):
+    pass
 
+class DISCUSS_W_SELLER__DISCUSS__ASK(MessageHandler):
+    pass
 
+class DISCUSS_W_SELLER__DISCUSS__THANK_YOU(MessageHandler):
+    pass
 
+class DISCUSS_W_SELLER__DISCUSS__ALREADY_CONNECTED(MessageHandler):
+    pass
 
 
+# DISCUSS_W_BUYER intent
 
+class DISCUSS_W_BUYER__SUPPLY__GET_DEPOSIT(MessageHandler):
+    pass
 
+class DISCUSS_W_BUYER__SUPPLY__GET_ACCEPT_LC(MessageHandler):
+    pass
 
-I know the user's context
+class DISCUSS_W_BUYER__DISCUSS__CONFIRM_INTERESTED(MessageHandler):
+    pass
 
+class DISCUSS_W_BUYER__STILL_INTERESTED__CONFIRM(MessageHandler):
+    pass
 
-What is a 'context'
-Messages I have sent the user, and is awaiting a reply
+class DISCUSS_W_BUYER__STILL_INTERESTED__THANK_YOU(MessageHandler):
+    pass
 
-What if the user has 2 running contexts? how do I ascertain which context
-is the user replying to?
-- I can classify the user's message body
-- I can verify with the user - by sending a testing question
-- the same can be applied to 
+class DISCUSS_W_BUYER__DISCUSS__CONFIRM_UPDATED(MessageHandler):
+    pass
 
+class DISCUSS_W_BUYER__DISCUSS__ASK(MessageHandler):
+    pass
 
+class DISCUSS_W_BUYER__DISCUSS__THANK_YOU(MessageHandler):
+    pass
 
+class DISCUSS_W_BUYER__DISCUSS__ALREADY_CONNECTED(MessageHandler):
+    pass
 
 
+# STOP_DISCUSSION intent
 
+class STOP_DISCUSSION__STOP_DISCUSSION__REASON(MessageHandler):
+    pass
 
+class STOP_DISCUSSION__STOP_DISCUSSION__THANK_YOU(MessageHandler):
+    pass
 
 
-"""
+# CONNECT intent
+
+class CONNECT__PLEASE_PAY(MessageHandler):
+    pass
+
+
+# No intent
+
+class NO_INTENT__MENU(MessageHandler):
+    pass
+
+class NO_INTENT__DO_NOT_UNDERSTAND(MessageHandler):
+    pass
+
+class NO_INTENT__YOUR_QUESTION(MessageHandler):
+    pass
+
+class NO_INTENT__YOUR_ANSWER(MessageHandler):
+    pass
+
+class NO_INTENT__PAYEE_CONNECTED(MessageHandler):
+    pass
+
+class NO_INTENT__NON_PAYEE_CONNECTED(MessageHandler):
+    pass
+
+class NO_INTENT__NO_MESSAGE(MessageHandler):
+    def run(self):
+        # Check if user has name, if not - register him
+
+        
+        pass
