51126368ecb7b777db1e9fff16badcf3b37b4df2
Kevin Ng <kevinngsw@gmail.com>
2021-05-26 17:37:46 +0800

    Refactor message handling cycle into views.py

--- chat/views.py
+++ chat/views.py
@@ -4,20 +4,43 @@ from http import HTTPStatus
 from rest_framework.views import APIView
 from rest_framework import status
 
-from twilio.request_validator import RequestValidator
-from twilio.twiml.messaging_response import MessagingResponse
-
-from . import models
-from .libraries import chat
+from chat import models
+from chat.libraries import model_utils, context_utils, handler_context_map
 from everybase.settings import (TWILIO_AUTH_TOKEN,
     TWILIO_WEBHOOK_INCOMING_MESSAGES_URL,
     TWILIO_WEBHOOK_STATUS_UPDATE_URL)
 
-class TwilioIncomingMessageView(APIView):
-    """Webhook to receiving incoming Twilio message via a POST request.
+from twilio.request_validator import RequestValidator
+from twilio.twiml.messaging_response import MessagingResponse
 
-    Last updated: 17 May 2021, 7:27 PM
+def reply(message):
+    """Returns TwilML response to a Twilio incoming message model row reference.
+    This function ascertains the best reply for the user and replies the user.
+
+    Parameters
+    ----------
+    message
+        Incoming message model row reference. Message must have valid users and
+        phone numbers - i.e., they must have been created if this is the first
+        message sent by the user.
     """
+
+    # Get user's context
+    intent_key, message_key = context_utils.get_context(message.from_user)
+    
+    # Get handler for context
+    handler = handler_context_map.get_handler(message, intent_key, message_key)
+
+    # Run handler and get message body
+    body = handler.run()
+
+    # Return TwilML response string
+    response = MessagingResponse()
+    response.message(body)
+    return str(response)
+
+class TwilioIncomingMessageView(APIView):
+    """Webhook to receiving incoming Twilio message via a POST request."""
     def post(self, request, format=None):
         signature = request.headers.get('X-Twilio-Signature')
         validator = RequestValidator(TWILIO_AUTH_TOKEN)
@@ -27,13 +50,12 @@ class TwilioIncomingMessageView(APIView):
             return HttpResponse(status=HTTPStatus.UNAUTHORIZED)
 
         try:
-            (message, ph_is_new, usr_is_new, _, _) = \
-                chat.save_message(request)
-            chat.save_message_log(request, message)
-            chat.save_message_medias(request, message)
+            message, _, _, _, _ = model_utils.save_message(request)
+            model_utils.save_message_log(request, message)
+            model_utils.save_message_medias(request, message)
 
             return HttpResponse(
-                chat.reply(message, ph_is_new, usr_is_new),
+                reply(message),
                 status=HTTPStatus.OK,
             )
 
