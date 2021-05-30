c2b216a1a37436dfe514bf91a37e66f6a62f0c0d
Kevin Ng <kevinngsw@gmail.com>
2021-05-30 12:02:52 +0800

    Refactor test_register: use default setup/tearDown

--- chat/tests/test_register.py
+++ chat/tests/test_register.py
@@ -2,28 +2,16 @@ from chat.tests import utils
 from chat.libraries import intents, messages
 
 class RegisterTestCase(utils.ChatFlowTest):
-    def setUp(self):
-        self.setup_user(None)
-
-    def tearDown(self):
-        self.tear_down_user()
-
     def test_register(self):
         # User says hi
-        msg_1 = self.receive('Hi')
-
-        # Reply
-        self.reply(msg_1)
+        self.receive_reply('Hi')
 
         # Test
         self.assert_context(intents.REGISTER, messages.REGISTER__GET_NAME)
         self.assertEqual(self.user.name, None)
 
         # User gives name
-        msg_2 = self.receive('Kevin')
-
-        # Reply
-        self.reply(msg_2)
+        self.receive_reply('Kevin')
 
         # Test
         self.assert_context(intents.MENU, messages.MENU)
