4571e3d488bc39b33b26612c9324eb08af612731
Kevin Ng <kevinngsw@gmail.com>
2021-06-03 17:50:15 +0800

    Add new-supply/confirm-packing tests

--- chat/tests/test_new_supply__confirm_packing.py
+++ chat/tests/test_new_supply__confirm_packing.py
@@ -1,60 +1,48 @@
 from chat.tests import utils
-from chat.libraries import intents, messages, context_utils
+from chat.libraries import intents, messages, datas, context_utils, model_utils
 
-class NewSupplyConfirmPackingReadyOTGKnownPackingTest(utils.ChatFlowTest):
+class NewSupplyConfirmPacking_ReadyOTG_Test(utils.ChatFlowTest):
     def setUp(self):
         super().setUp()
         context_utils.start_context(self.user, intents.NEW_SUPPLY, messages.SUPPLY__CONFIRM_PACKING)
-
-        # TODO: set up ready/otg
-
-        # TODO: set up known packing
+        self.set_up_data_value_string(
+            intents.NEW_SUPPLY,
+            messages.SUPPLY__GET_AVAILABILITY,
+            datas.NEW_SUPPLY__SUPPLY__GET_AVAILABILITY__AVAILABILITY__CHOICE,
+            datas.NEW_SUPPLY__SUPPLY__GET_AVAILABILITY__AVAILABILITY__READY_OTG
+        )
 
     def test_choose_yes_with_number(self):
-        # self.receive_reply_assert('1', intents.NEW_SUPPLY, messages.SUPPLY__GET_PRODUCT)
-        pass
+        self.receive_reply_assert('1', intents.NEW_SUPPLY, messages.SUPPLY__GET_QUANTITY_READY_OTG_KNOWN_PACKING)
 
     def test_choose_yes_with_text(self):
-        pass
+        self.receive_reply_assert('yes', intents.NEW_SUPPLY, messages.SUPPLY__GET_QUANTITY_READY_OTG_KNOWN_PACKING)
 
     def test_choose_no_with_number(self):
-        pass
+        self.receive_reply_assert('2', intents.NEW_SUPPLY, messages.SUPPLY__GET_PACKING)
 
     def test_choose_no_with_text(self):
-        pass
+        self.receive_reply_assert('no', intents.NEW_SUPPLY, messages.SUPPLY__GET_PACKING)
 
-class NewSupplyConfirmPackingReadyOTGUnknownPackingTest(utils.ChatFlowTest):
+class NewSupplyConfirmPacking_PreOrder_Test(utils.ChatFlowTest):
     def setUp(self):
         super().setUp()
         context_utils.start_context(self.user, intents.NEW_SUPPLY, messages.SUPPLY__CONFIRM_PACKING)
+        self.set_up_data_value_string(
+            intents.NEW_SUPPLY,
+            messages.SUPPLY__GET_AVAILABILITY,
+            datas.NEW_SUPPLY__SUPPLY__GET_AVAILABILITY__AVAILABILITY__CHOICE,
+            datas.NEW_SUPPLY__SUPPLY__GET_AVAILABILITY__AVAILABILITY__PRE_ORDER
+        )
 
     def test_choose_yes_with_number(self):
-        # self.receive_reply_assert('1', intents.NEW_SUPPLY, messages.SUPPLY__GET_PRODUCT)
-        pass
+        self.receive_reply_assert('1', intents.NEW_SUPPLY, messages.SUPPLY__GET_QUANTITY_PRE_ORDER)
 
     def test_choose_yes_with_text(self):
-        pass
+        self.receive_reply_assert('yes', intents.NEW_SUPPLY, messages.SUPPLY__GET_QUANTITY_PRE_ORDER)
 
     def test_choose_no_with_number(self):
-        pass
+        self.receive_reply_assert('2', intents.NEW_SUPPLY, messages.SUPPLY__GET_PACKING)
 
     def test_choose_no_with_text(self):
-        pass
-
-class NewSupplyConfirmPackingPreOrderTest(utils.ChatFlowTest):
-    def setUp(self):
-        super().setUp()
-        context_utils.start_context(self.user, intents.NEW_SUPPLY, messages.SUPPLY__CONFIRM_PACKING)
-
-    def test_choose_yes_with_number(self):
-        # self.receive_reply_assert('1', intents.NEW_SUPPLY, messages.SUPPLY__GET_PRODUCT)
-        pass
-
-    def test_choose_yes_with_text(self):
-        pass
-
-    def test_choose_no_with_number(self):
-        pass
-
-    def test_choose_no_with_text(self):
-        pass
+        self.receive_reply_assert('no', intents.NEW_SUPPLY, messages.SUPPLY__GET_PACKING)
