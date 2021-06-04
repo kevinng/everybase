b257a8951f95557d6e114dfc0c3012df3bd42e48
Kevin Ng <kevinngsw@gmail.com>
2021-06-04 13:28:16 +0800

    Implement new-supply/get-quantity-otg-unknown-pack

--- chat/handlers.py
+++ chat/handlers.py
@@ -220,7 +220,12 @@ class NEW_SUPPLY__SUPPLY__GET_QUANTITY_READY_OTG_KNOWN_PACKING(MessageHandler):
         )
 
 class NEW_SUPPLY__SUPPLY__GET_QUANTITY_READY_OTG_UNKNOWN_PACKING(MessageHandler):
-    pass
+    def run(self):
+        self.save_body_as_string(datas.NEW_SUPPLY__SUPPLY__GET_QUANTITY_READY_OTG_UNKNOWN_PACKING__QUANTITY__STRING)
+        return self.done_reply(
+            intents.NEW_SUPPLY,
+            messages.SUPPLY__GET_PRICE_READY_OTG_UNKNOWN_PACKING
+        )
 
 class NEW_SUPPLY__SUPPLY__GET_QUANTITY_PRE_ORDER(MessageHandler):
     pass
