7435080553b5334542830bd10466cfa5a3e428d6
Kevin Ng <kevinngsw@gmail.com>
2021-06-04 17:49:01 +0800

    Add new-demand/get-product test

---
+++ chat/tests/test_demand_get_product.py
@@ -0,0 +1,14 @@
+from chat.tests import utils
+from chat.libraries import intents, messages, datas, context_utils
+
+class NewSupplyGetProductTestCase(utils.ChatFlowTest):
+    def setUp(self):
+        super().setUp(intents.NEW_DEMAND, messages.DEMAND__GET_PRODUCT)
+
+    def test_enter_product(self):
+        input = 'nitrile gloves'
+        self.receive_reply_assert(input, intents.NEW_DEMAND, messages.DEMAND__GET_COUNTRY_STATE)
+        self.assert_value(
+            datas.NEW_DEMAND__DEMAND__GET_PRODUCT__PRODUCT_TYPE__STRING,
+            input
+        )
