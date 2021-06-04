8fe9d6512c40d740e430ec20d5f6f3bc201c658a
Kevin Ng <kevinngsw@gmail.com>
2021-06-04 16:09:13 +0800

    Add supply-get-deposit tests

--- chat/tests/test_supply_get_deposit.py
+++ chat/tests/test_supply_get_deposit.py
@@ -1,14 +1,23 @@
-# from chat.tests import utils
-# from chat.libraries import intents, messages, datas, context_utils
+from chat.tests import utils
+from chat.libraries import intents, messages, datas, context_utils
 
-# class NewSupplyGetDepositTestCase(utils.ChatFlowTest):
-#     def setUp(self):
-#         super().setUp(intents.NEW_SUPPLY, messages.SUPPLY__GET_DEPOSIT)
+class NewSupplyGetDepositTestCase(utils.ChatFlowTest):
+    def setUp(self):
+        super().setUp(intents.NEW_SUPPLY, messages.SUPPLY__GET_DEPOSIT)
 
-#     def test_enter_product(self):
-#         input = '40'
-#         self.receive_reply_assert(input, intents.NEW_SUPPLY, messages.SUPPLY__GET_AVAILABILITY)
-#         self.assert_value(
-#             datas.NEW_SUPPLY__SUPPLY__GET_PRODUCT__PRODUCT_TYPE__STRING,
-#             input
-#         )
+    def test_enter_bad_value(self):
+        self.receive_reply_assert('hello', intents.NEW_SUPPLY, messages.SUPPLY__GET_DEPOSIT)
+
+    def test_enter_deposit_1(self):
+        self.receive_reply_assert('40', intents.NEW_SUPPLY, messages.SUPPLY__GET_ACCEPT_LC)
+        self.assert_value(
+            datas.NEW_SUPPLY__SUPPLY__GET_DEPOSIT__DEPOSIT__NUMBER,
+            value_float=40
+        )
+
+    def test_enter_deposit_2(self):
+        self.receive_reply_assert('10.5', intents.NEW_SUPPLY, messages.SUPPLY__GET_ACCEPT_LC)
+        self.assert_value(
+            datas.NEW_SUPPLY__SUPPLY__GET_DEPOSIT__DEPOSIT__NUMBER,
+            value_float=10.5
+        )
