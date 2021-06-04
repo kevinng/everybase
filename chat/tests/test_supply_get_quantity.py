6663e383b0b098e26b4a059294a8dd2cbd237881
Kevin Ng <kevinngsw@gmail.com>
2021-06-04 09:59:14 +0800

    Add test cases for new-supply/get-quantity
    - Add new constants to datas.py and migrate
    - Add new test cases

--- chat/tests/test_supply_get_quantity.py
+++ chat/tests/test_supply_get_quantity.py
@@ -1,10 +1,38 @@
 from chat.tests import utils
-from chat.libraries import intents, messages, context_utils
+from chat.libraries import intents, messages, datas
 
 class NewSupplyGetQuantity_ReadyOTG_KnownPacking_Test(utils.ChatFlowTest):
     def setUp(self):
         super().setUp(intents.NEW_SUPPLY, messages.SUPPLY__GET_QUANTITY_READY_OTG_KNOWN_PACKING)
     
     def test_get_quantity(self):
-        pass
+        input = '10000 boxes'
+        self.receive_reply_assert(input, intents.NEW_SUPPLY, messages.SUPPLY__GET_PRICE_READY_OTG_KNOWN_PACKING)
+        self.assert_value(
+            datas.NEW_SUPPLY__SUPPLY__GET_QUANTITY_READY_OTG_KNOWN_PACKING__QUANTITY__STRING,
+            input
+        )
     
+class NewSupplyGetQuantity_ReadyOTG_UnknownPacking_Test(utils.ChatFlowTest):
+    def setUp(self):
+        super().setUp(intents.NEW_SUPPLY, messages.SUPPLY__GET_QUANTITY_READY_OTG_UNKNOWN_PACKING)
+    
+    def test_get_quantity(self):
+        input = '10000 boxes'
+        self.receive_reply_assert(input, intents.NEW_SUPPLY, messages.SUPPLY__GET_PRICE_READY_OTG_UNKNOWN_PACKING)
+        self.assert_value(
+            datas.NEW_SUPPLY__SUPPLY__GET_QUANTITY_READY_OTG_UNKNOWN_PACKING__QUANTITY__STRING,
+            input
+        )
+
+class NewSupplyGetQuantity_ReadyOTG_PreOrder_Test(utils.ChatFlowTest):
+    def setUp(self):
+        super().setUp(intents.NEW_SUPPLY, messages.SUPPLY__GET_QUANTITY_READY_OTG_UNKNOWN_PACKING)
+    
+    def test_get_quantity(self):
+        input = '10000 boxes'
+        self.receive_reply_assert(input, intents.NEW_SUPPLY, messages.SUPPLY__GET_PRICE_PRE_ORDER)
+        self.assert_value(
+            datas.NEW_SUPPLY__SUPPLY__GET_QUANTITY_PREORDER__QUANTITY__STRING,
+            input
+        )
