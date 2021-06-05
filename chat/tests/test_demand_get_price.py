3c6c434e6e5259a4321195c0904f803332682189
Kevin Ng <kevinngsw@gmail.com>
2021-06-06 00:59:19 +0800

    Add new-demand/get-price (known product) test
    - Add new data key and migrate
    - Add test

---
+++ chat/tests/test_demand_get_price.py
@@ -0,0 +1,14 @@
+from chat.tests import utils
+from chat.libraries import intents, messages, datas
+
+class NewDemandGetPrice_KnownProductType_Test(utils.ChatFlowTest):
+    def setUp(self):
+        super().setUp(intents.NEW_DEMAND, messages.DEMAND__GET_PRICE_KNOWN_PRODUCT_TYPE)
+    
+    def test_get_price(self):
+        input = 'USD 20'
+        self.receive_reply_assert(input, intents.NEW_DEMAND, messages.DEMAND__THANK_YOU)
+        self.assert_value(
+            datas.NEW_DEMAND__DEMAND__GET_PRICE_KNOWN_PRODUCT_TYPE__PRICE__STRING,
+            input
+        )
