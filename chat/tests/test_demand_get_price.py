7be30e3c63b3b561e4599c3f2f11e923db1a5306
Kevin Ng <kevinngsw@gmail.com>
2021-06-06 01:17:42 +0800

    Add new-demand/get-price (unknown product) test
    - Add data key for input
    - Add test

--- chat/tests/test_demand_get_price.py
+++ chat/tests/test_demand_get_price.py
@@ -12,3 +12,15 @@ class NewDemandGetPrice_KnownProductType_Test(utils.ChatFlowTest):
             datas.NEW_DEMAND__DEMAND__GET_PRICE_KNOWN_PRODUCT_TYPE__PRICE__STRING,
             input
         )
+
+class NewDemandGetPrice_UnknownProductType_Test(utils.ChatFlowTest):
+    def setUp(self):
+        super().setUp(intents.NEW_DEMAND, messages.DEMAND__GET_PRICE_UNKNOWN_PRODUCT_TYPE)
+    
+    def test_get_price(self):
+        input = 'USD 20 per box'
+        self.receive_reply_assert(input, intents.NEW_DEMAND, messages.DEMAND__THANK_YOU)
+        self.assert_value(
+            datas.NEW_DEMAND__DEMAND__GET_PRICE_UNKNOWN_PRODUCT_TYPE__PRICE__STRING,
+            input
+        )
