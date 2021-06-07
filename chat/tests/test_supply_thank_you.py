a9a8cdb941a583cb0632f8cebf8ceef930bfe102
Kevin Ng <kevinngsw@gmail.com>
2021-06-07 09:48:43 +0800

    Fix test case context

--- chat/tests/test_supply_thank_you.py
+++ chat/tests/test_supply_thank_you.py
@@ -1,9 +1,32 @@
 from chat.tests import utils
-from chat.libraries import intents, messages
+from chat.libraries import intents, messages, datas
 
 class SupplyThankYouTest(utils.ChatFlowTest):
     def setUp(self):
         super().setUp(intents.NEW_SUPPLY, messages.SUPPLY__THANK_YOU)
     
-    def test_get_quantity(self):
-        self.receive_reply_assert('hello', intents.MENU, messages.MENU)
+    def test_choose_non_choice_with_number(self):
+        self.receive_reply_assert('10', intents.NEW_SUPPLY, messages.SUPPLY__THANK_YOU)
+
+    def test_choose_non_choice_with_number(self):
+        self.receive_reply_assert('hello', intents.NEW_SUPPLY, messages.SUPPLY__THANK_YOU)
+
+    def test_choose_new_supply_with_number(self):
+        self.receive_reply_assert('1',
+            intents.NEW_SUPPLY,
+            messages.SUPPLY__GET_PRODUCT
+        )
+        self.assert_value(
+            datas.MENU__MENU__OPTION__CHOICE,
+            datas.MENU__MENU__OPTION__FIND_BUYER
+        )
+
+    def test_choose_new_supply_with_text(self):
+        self.receive_reply_assert('find buyers',
+            intents.NEW_SUPPLY,
+            messages.SUPPLY__GET_PRODUCT
+        )
+        self.assert_value(
+            datas.MENU__MENU__OPTION__CHOICE,
+            datas.MENU__MENU__OPTION__FIND_BUYER
+        )
