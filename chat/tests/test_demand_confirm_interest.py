a4f99e172250b29499cf7967a4fc247310714f89
Kevin Ng <kevinngsw@gmail.com>
2021-06-07 11:06:21 +0800

    Rename test and fix context setting
    Context is set to discuss with buyer not seller - fix it

---
+++ chat/tests/test_demand_confirm_interest.py
@@ -0,0 +1,52 @@
+from chat.tests import utils
+from chat.libraries import intents, messages, datas
+
+class DemandConfirmInterestTest(utils.ChatFlowTest):
+    def setUp(self):
+        super().setUp(intents.DISCUSS_W_SELLER, messages.DISCUSS__CONFIRM_INTEREST, None)
+
+    def test_choose_non_choice_with_number(self):
+        self.receive_reply_assert('10', intents.DISCUSS_W_SELLER, messages.DISCUSS__CONFIRM_INTEREST)
+
+    def test_choose_non_choice_with_number(self):
+        self.receive_reply_assert('hello', intents.DISCUSS_W_SELLER, messages.DISCUSS__CONFIRM_INTEREST)
+
+    def test_choose_yes_with_number(self):
+        self.receive_reply_assert('1',
+            intents.DISCUSS_W_SELLER,
+            messages.DISCUSS__CONFIRM_INTEREST
+        )
+        self.assert_value(
+            datas.DISCUSS_W_SELLER__CONFIRM_INTEREST__INTERESTED__CHOICE,
+            datas.DISCUSS_W_SELLER__CONFIRM_INTEREST__INTERESTED__YES
+        )
+
+    def test_choose_new_supply_with_text(self):
+        self.receive_reply_assert('yes',
+            intents.DISCUSS_W_SELLER,
+            messages.DISCUSS__CONFIRM_INTEREST
+        )
+        self.assert_value(
+            datas.DISCUSS_W_SELLER__CONFIRM_INTEREST__INTERESTED__CHOICE,
+            datas.DISCUSS_W_SELLER__CONFIRM_INTEREST__INTERESTED__YES
+        )
+
+    def test_choose_new_demand_with_number(self):
+        self.receive_reply_assert('2',
+            intents.DISCUSS_W_SELLER,
+            messages.DISCUSS__CONFIRM_INTEREST
+        )
+        self.assert_value(
+            datas.DISCUSS_W_SELLER__CONFIRM_INTEREST__INTERESTED__CHOICE,
+            datas.DISCUSS_W_SELLER__CONFIRM_INTEREST__INTERESTED__NO
+        )
+
+    def test_choose_new_demand_with_text(self):
+        self.receive_reply_assert('no',
+            intents.DISCUSS_W_SELLER,
+            messages.DISCUSS__CONFIRM_INTEREST
+        )
+        self.assert_value(
+            datas.DISCUSS_W_SELLER__CONFIRM_INTEREST__INTERESTED__CHOICE,
+            datas.DISCUSS_W_SELLER__CONFIRM_INTEREST__INTERESTED__NO
+        )
