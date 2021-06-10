4e87564e54631fe1e9a4863b884e3418770ceb3f
Kevin Ng <kevinngsw@gmail.com>
2021-06-10 12:08:52 +0800

    Add supply/confirm-interest handler

--- chat/handlers.py
+++ chat/handlers.py
@@ -473,78 +473,126 @@ class DISCUSS_W_SELLER__DEMAND__GET_PRICE_UNKNOWN_PRODUCT_TYPE(MessageHandler):
     pass
 
 class DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST(MessageHandler):
+    def _get_connected_env_var(self):
+        """Returns true if both users are connected, false otherwise.
+        """
+        user_1_id = self.get_latest_value(
+            intents.DISCUSS_W_SELLER,
+            messages.DISCUSS__CONFIRM_INTEREST,
+            datas.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST__USER_1__ID,
+            False
+        ).value_id
+
+        user_2_id = self.get_latest_value(
+            intents.DISCUSS_W_SELLER,
+            messages.DISCUSS__CONFIRM_INTEREST,
+            datas.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST__USER_2__ID,
+            False
+        ).value_id
+
+        try:
+            connection = relmods.Connection.objects.get(
+                user_1=user_1_id,
+                user_2_id=user_2_id
+            )
+        except relmods.Connection.DoesNotExist:
+            return False
+
+        return connection is not None
+        
+    def _get_yes_not_connected_params(self):
+        """Get template parameters for yes/not-connected outcome
+        """
+        supply_id = self.get_latest_value(
+            intents.DISCUSS_W_SELLER,
+            messages.DISCUSS__CONFIRM_INTEREST,
+            datas.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST__SUPPLY__ID,
+            False
+        ).value_id
+        supply = relmods.Supply.objects.get(pk=supply_id)
+        return {
+            'buying': True,
+            'supply': supply
+        }
+    
+    def _get_yes_connected_params(self):
+        """Get template parameters for yes/connected outcome
+        """
+        user_1_id = self.get_latest_value(
+            intents.DISCUSS_W_SELLER,
+            messages.DISCUSS__CONFIRM_INTEREST,
+            datas.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST__USER_1__ID,
+            False
+        ).value_id
+
+        user_2_id = self.get_latest_value(
+            intents.DISCUSS_W_SELLER,
+            messages.DISCUSS__CONFIRM_INTEREST,
+            datas.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST__USER_2__ID,
+            False
+        ).value_id
+
+        if self.message.from_user.id == user_1_id:
+            # User 1 is this user - pass user 2's contact
+            contact_id = user_2_id
+        elif self.message.from_user.id == user_2_id:
+            # User 2 is this user - pass user 1's contact
+            contact_id = user_1_id
+        else:
+            # Unlikely, but raise an error just in case
+            raise Exception("User 1 and 2's ID do not match this user's ID")
+
+        contact = relmods.User.objects.get(pk=contact_id)
+
+        return {
+            'buying': True,
+            'contact': contact
+        }
+
+    def _get_no_params(self):
+        """Get template parameters for no outcome
+        """
+        product_type_id = self.get_latest_value(
+            intents.DISCUSS_W_SELLER,
+            messages.DISCUSS__CONFIRM_INTEREST,
+            datas.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST__PRODUCT_TYPE__ID,
+            False
+        ).value_id
+
+        product_type = relmods.ProductType.objects.get(pk=product_type_id)
+
+        return {
+            'product_type_name': product_type.name
+        }
+
     def run(self):
-        pass
+        self.set_env_var('CONNECTED', value_func=self._get_connected_env_var)
+        connected = self.get_env_var('CONNECTED')
 
-        # # Get user IDs to ascertain connection
+        # Set outcomes if the user selects 'yes' depending on whether the user
+        # is already connected with the seller or not.
+        if connected:
+            yes_message_key = messages.DISCUSS__ALREADY_CONNECTED
+            yes_params_func = self._get_yes_connected_params
+        else:
+            yes_message_key = messages.DISCUSS__CONFIRM_DETAILS
+            yes_params_func = self._get_yes_not_connected_params
 
-        # user_1_id = self.get_latest_value(
-        #     intents.DISCUSS_W_SELLER,
-        #     messages.DISCUSS__CONFIRM_INTEREST,
-        #     datas.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST__USER_1__ID
-        # ).value_id
-
-        # user_2_id = self.get_latest_value(
-        #     intents.DISCUSS_W_SELLER,
-        #     messages.DISCUSS__CONFIRM_INTEREST,
-        #     datas.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST__USER_2__ID
-        # ).value_id
-
-        # try:
-        #     connection = relmods.Connection.objects.get(
-        #         user_1=user_1_id,
-        #         user_2_id=user_2_id
-        #     )
-        # except relmods.Connection.DoesNotExist:
-        #     connection = None
-
-        # if connection is None:
-        #     # Users are not connected - show supply details
-
-        #     # I also need to pass in supply ID - in case they're not connected
-        #     # I already have both user's ID, I need to ascertain which of those is this user - I can actually set it
-
-        #     supply_id = self.get_latest_value(
-        #         intents.DISCUSS_W_SELLER,
-        #         messages.DISCUSS__CONFIRM_INTEREST,
-        #         datas.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST__SUPPLY__ID
-        #     ).value_id
-        #     supply = relmods.Supply.objects.get(pk=supply_id)
-        #     params = {
-        #         'buying': True,
-        #         'supply': supply
-        #     }
-
-        # else:
-        #     pass
-        #     # TODO: fill in contact details
-
-        # self.add_option([('1', 0), ('yes', 0)],
-        #     intents.DISCUSS_W_SELLER,
-        #     messages.STILL_INTERESTED__CONFIRM, {},
-        #     datas.DISCUSS_W_SELLER__CONFIRM_INTEREST__INTERESTED__CHOICE,
-        #     datas.DISCUSS_W_SELLER__CONFIRM_INTEREST__INTERESTED__YES
-        # )
-
-        # # Read product type ID set by the system when sending out the confirm-
-        # # interest message
-        # product_type_id = self.get_latest_value(
-        #     intents.DISCUSS_W_SELLER,
-        #     messages.DISCUSS__CONFIRM_INTEREST,
-        #     datas.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST__PRODUCT_TYPE__ID
-        # ).value_id
-
-        # # Get product type
-        # product_type = relmods.ProductType.objects.get(pk=product_type_id)
-
-        # self.add_option([('2', 0), ('no', 0)],
-        #     intents.DISCUSS_W_SELLER,
-        #     messages.STILL_INTERESTED__CONFIRM,
-        #     { 'product_type_name': product_type.name },
-        #     datas.DISCUSS_W_SELLER__CONFIRM_INTEREST__INTERESTED__CHOICE,
-        #     datas.DISCUSS_W_SELLER__CONFIRM_INTEREST__INTERESTED__NO
-        # )
-        # return self.reply_option()
+        self.add_option([('1', 0), ('yes', 0)],
+            intents.DISCUSS_W_SELLER,
+            yes_message_key,
+            yes_params_func,
+            datas.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST__INTERESTED__CHOICE,
+            datas.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST__INTERESTED__YES
+        )
+        self.add_option([('2', 0), ('no', 0)],
+            intents.DISCUSS_W_SELLER,
+            messages.STILL_INTERESTED__CONFIRM,
+            self._get_no_params,
+            datas.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST__INTERESTED__CHOICE,
+            datas.DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST__INTERESTED__NO
+        )
+        return self.reply_option()
 
 class DISCUSS_W_SELLER__STILL_INTERESTED__CONFIRM(MessageHandler):
     pass
