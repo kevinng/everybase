23cb683a7ed9a6a234572b0b1607b345e9e8d9f3
Kevin Ng <kevinngsw@gmail.com>
2021-06-11 21:29:07 +0800

    Rename/separate test_demand_get_price.py
    - Separate/rename test_demand_get_price into 2 files
        - test_new_demand__demand__get_price_known_product_type
        - test_new_demadn__demand__get_price_unknown_product_type

---
+++ chat/tests/test_new_demand__demand__get_price_known_product_type.py
@@ -0,0 +1,22 @@
+from chat.libraries import intents, messages, datas
+from chat.libraries.message_handler_test import MessageHandlerTest
+
+class NewDemandDemandGetPriceKnownProductTypeTest(MessageHandlerTest):
+    def setUp(self):
+        super().setUp(
+            intents.NEW_DEMAND,
+            messages.DEMAND__GET_PRICE_KNOWN_PRODUCT_TYPE
+        )
+    
+    def test_get_price(self):
+        input = 'USD 20'
+        self.receive_reply_assert(
+            input,
+            intents.NEW_DEMAND,
+            messages.DEMAND__THANK_YOU
+        )
+        self.assert_value(
+            datas.\
+                NEW_DEMAND__DEMAND__GET_PRICE_KNOWN_PRODUCT_TYPE__PRICE__STRING,
+            value_string=input
+        )
---
+++ chat/tests/test_new_demand__demand__get_price_unknown_product_type.py
@@ -0,0 +1,22 @@
+from chat.libraries import intents, messages, datas
+from chat.libraries.message_handler_test import MessageHandlerTest
+
+class NewDemandGetPriceUnknownProductTypeTest(MessageHandlerTest):
+    def setUp(self):
+        super().setUp(
+            intents.NEW_DEMAND,
+            messages.DEMAND__GET_PRICE_UNKNOWN_PRODUCT_TYPE
+        )
+    
+    def test_get_price(self):
+        input = 'USD 20 per box'
+        self.receive_reply_assert(
+            input,
+            intents.NEW_DEMAND,
+            messages.DEMAND__THANK_YOU
+        )
+        self.assert_value(
+            datas.\
+            NEW_DEMAND__DEMAND__GET_PRICE_UNKNOWN_PRODUCT_TYPE__PRICE__STRING,
+            value_string=input
+        )
