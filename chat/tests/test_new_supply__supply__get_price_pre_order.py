5d4744ff6feb636e8637fb79da245e4e448c577a
Kevin Ng <kevinngsw@gmail.com>
2021-06-11 22:03:41 +0800

    Rename/separate test_new_supply__supply_get_availability.py
    To
    - test_new_supply__supply__get_price_pre_order.py
    - test_new_supply__supply__get_price_ready_otg_known_packing.py
    - test_new_supply__supply__get_price_ready_otg_unknown_packing

---
+++ chat/tests/test_new_supply__supply__get_price_pre_order.py
@@ -0,0 +1,18 @@
+from chat.libraries import intents, messages, datas
+from chat.libraries.message_handler_test import MessageHandlerTest
+
+class NewSupplyGetPricePreOrderTest(MessageHandlerTest):
+    def setUp(self):
+        super().setUp(intents.NEW_SUPPLY, messages.SUPPLY__GET_PRICE_PRE_ORDER)
+    
+    def test_get_quantity(self):
+        input = 'USD 20 per box'
+        self.receive_reply_assert(
+            input,
+            intents.NEW_SUPPLY,
+            messages.SUPPLY__GET_DEPOSIT
+        )
+        self.assert_value(
+            datas.NEW_SUPPLY__SUPPLY__GET_PRICE_PREORDER__PRICE__STRING,
+            value_string=input
+        )
