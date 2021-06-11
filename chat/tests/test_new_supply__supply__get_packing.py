fd5026621e25eddfc18c4d133c09c4ea9cfc809d
Kevin Ng <kevinngsw@gmail.com>
2021-06-11 21:58:20 +0800

    Rename to test_new_supply__supply__get_packing.py

--- chat/tests/test_new_supply__supply__get_packing.py
+++ chat/tests/test_new_supply__supply__get_packing.py
@@ -1,7 +1,7 @@
 from chat.libraries import intents, messages, datas
 from chat.libraries.message_handler_test import MessageHandlerTest
 
-class NewSupplyGetPacking_ReadyOTG_Test(MessageHandlerTest):
+class NewSupplySupplyGetPacking_ReadyOTG_Test(MessageHandlerTest):
     def setUp(self):
         super().setUp(intents.NEW_SUPPLY, messages.SUPPLY__GET_PACKING)
         self.set_up_data_value(
@@ -23,7 +23,7 @@ class NewSupplyGetPacking_ReadyOTG_Test(MessageHandlerTest):
             value_string=input
         )
 
-class NewSupplyGetPacking_PreOrder_Test(MessageHandlerTest):
+class NewSupplySupplyGetPacking_PreOrder_Test(MessageHandlerTest):
     def setUp(self):
         super().setUp(intents.NEW_SUPPLY, messages.SUPPLY__GET_PACKING)
         self.set_up_data_value(
