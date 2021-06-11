ff15b751eae70f54ffb0eb8a63e7fddaae7e24ff
Kevin Ng <kevinngsw@gmail.com>
2021-06-11 21:37:39 +0800

    Rename to test_new_demand__demand_get_product.py

--- chat/tests/test_new_demand__demand_get_product.py
+++ chat/tests/test_new_demand__demand_get_product.py
@@ -1,7 +1,7 @@
 from chat.libraries import intents, messages, datas
 from chat.libraries.message_handler_test import MessageHandlerTest
 
-class NewDemandGetProductTestCase(MessageHandlerTest):
+class NewDemandDemandGetProductTest(MessageHandlerTest):
     def setUp(self):
         super().setUp(intents.NEW_DEMAND, messages.DEMAND__GET_PRODUCT)
 
