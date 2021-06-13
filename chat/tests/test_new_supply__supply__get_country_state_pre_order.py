c4bef482e89792563f36cc9dd5dad9fdc999ab39
Kevin Ng <kevinngsw@gmail.com>
2021-06-13 18:19:41 +0800

    Refactor test supply/country-state
    - Add comments
    - Update search string for unknown product type to make it more unlikely
        to match

--- chat/tests/test_new_supply__supply__get_country_state_pre_order.py
+++ chat/tests/test_new_supply__supply__get_country_state_pre_order.py
@@ -4,6 +4,8 @@ from chat.tests import texts
 
 class NewSupplySupplyGetCountryStateTest(MessageHandlerTest):
     def set_up_known_product(self):
+        # Set up a product, and have user enter a search phrase that matches
+        # this product exactly
         _, _, kw = self.set_up_product_type(
             uom_description='200 pieces in 1 box')
         self.set_up_data_value(
@@ -14,11 +16,13 @@ class NewSupplySupplyGetCountryStateTest(MessageHandlerTest):
         )
 
     def set_up_unknown_product(self):
+        # Set up the user to enter a string that's unlikely to match a known
+        # product
         self.set_up_data_value(
             intents.NEW_SUPPLY,
             messages.SUPPLY__GET_PRODUCT,
             datas.NEW_SUPPLY__SUPPLY__GET_PRODUCT__PRODUCT_TYPE__STRING,
-            'unknown product'
+            'SR2EdiLuc1K1BUAVsPg4' # Unlikely string to match a known product
         )
 
 class NewSupplySupplyGetCountryStatePreOrder_KnownProduct_Test(
