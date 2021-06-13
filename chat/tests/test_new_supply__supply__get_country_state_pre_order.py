0d751a16a3076de19f21b11f77fc2861187a977f
Kevin Ng <kevinngsw@gmail.com>
2021-06-13 15:16:16 +0800

    Separate new-supply/get-country-state tests
    Separate into 2 files for ready/OTG and pre-order

--- chat/tests/test_new_supply__supply__get_country_state_pre_order.py
+++ chat/tests/test_new_supply__supply__get_country_state_pre_order.py
@@ -19,48 +19,6 @@ class NewSupplySupplyGetCountryStateTest(MessageHandlerTest):
             'unknown product'
         )
 
-class NewSupplySupplyGetCountryStateReadyOTG_KnownProduct_Test(
-    NewSupplySupplyGetCountryStateTest):
-    def setUp(self):
-        super().setUp(
-            intents.NEW_SUPPLY,
-            messages.SUPPLY__GET_COUNTRY_STATE_READY_OTG
-        )
-        self.set_up_known_product()
-
-    def test_enter_country_state(self):
-        input = 'singapore'
-        self.receive_reply_assert(
-            input,
-            intents.NEW_SUPPLY,
-            messages.SUPPLY__CONFIRM_PACKING
-        )
-        self.assert_value(
-            datas.NEW_SUPPLY__SUPPLY__GET_COUNTRY_STATE__COUNTRY_STATE__STRING,
-            value_string=input
-        )
-
-class NewSupplySupplyGetCountryStateReadyOTG_UnknownProduct_Test(
-    NewSupplySupplyGetCountryStateTest):
-    def setUp(self):
-        super().setUp(
-            intents.NEW_SUPPLY,
-            messages.SUPPLY__GET_COUNTRY_STATE_READY_OTG
-        )
-        self.set_up_unknown_product()
-
-    def test_enter_country_state(self):
-        input = 'singapore'
-        self.receive_reply_assert(
-            input,
-            intents.NEW_SUPPLY,
-            messages.SUPPLY__GET_PACKING
-        )
-        self.assert_value(
-            datas.NEW_SUPPLY__SUPPLY__GET_COUNTRY_STATE__COUNTRY_STATE__STRING,
-            value_string=input
-        )
-
 class NewSupplySupplyGetCountryStatePreOrder_KnownProduct_Test(
     NewSupplySupplyGetCountryStateTest):
     def setUp(self):
---
+++ chat/tests/test_new_supply__supply__get_country_state_ready_otg.py
@@ -0,0 +1,62 @@
+from chat.libraries import intents, messages, datas
+from chat.libraries.message_handler_test import MessageHandlerTest
+
+class NewSupplySupplyGetCountryStateTest(MessageHandlerTest):
+    def set_up_known_product(self):
+        _, _, kw = self.set_up_product_type()
+        self.set_up_data_value(
+            intents.NEW_SUPPLY,
+            messages.SUPPLY__GET_PRODUCT,
+            datas.NEW_SUPPLY__SUPPLY__GET_PRODUCT__PRODUCT_TYPE__STRING,
+            kw.keyword
+        )
+
+    def set_up_unknown_product(self):
+        self.set_up_data_value(
+            intents.NEW_SUPPLY,
+            messages.SUPPLY__GET_PRODUCT,
+            datas.NEW_SUPPLY__SUPPLY__GET_PRODUCT__PRODUCT_TYPE__STRING,
+            'unknown product'
+        )
+
+class NewSupplySupplyGetCountryStateReadyOTG_KnownProduct_Test(
+    NewSupplySupplyGetCountryStateTest):
+    def setUp(self):
+        super().setUp(
+            intents.NEW_SUPPLY,
+            messages.SUPPLY__GET_COUNTRY_STATE_READY_OTG
+        )
+        self.set_up_known_product()
+
+    def test_enter_country_state(self):
+        input = 'singapore'
+        self.receive_reply_assert(
+            input,
+            intents.NEW_SUPPLY,
+            messages.SUPPLY__CONFIRM_PACKING
+        )
+        self.assert_value(
+            datas.NEW_SUPPLY__SUPPLY__GET_COUNTRY_STATE__COUNTRY_STATE__STRING,
+            value_string=input
+        )
+
+class NewSupplySupplyGetCountryStateReadyOTG_UnknownProduct_Test(
+    NewSupplySupplyGetCountryStateTest):
+    def setUp(self):
+        super().setUp(
+            intents.NEW_SUPPLY,
+            messages.SUPPLY__GET_COUNTRY_STATE_READY_OTG
+        )
+        self.set_up_unknown_product()
+
+    def test_enter_country_state(self):
+        input = 'singapore'
+        self.receive_reply_assert(
+            input,
+            intents.NEW_SUPPLY,
+            messages.SUPPLY__GET_PACKING
+        )
+        self.assert_value(
+            datas.NEW_SUPPLY__SUPPLY__GET_COUNTRY_STATE__COUNTRY_STATE__STRING,
+            value_string=input
+        )
