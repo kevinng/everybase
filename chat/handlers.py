41050babd40b2266478606e116e5e3b8d6fb7218
Kevin Ng <kevinngsw@gmail.com>
2021-06-14 23:08:06 +0800

    Add discuss-seller/get-price/unknown-product
    Add handler, data keys, migrations, tests

--- chat/handlers.py
+++ chat/handlers.py
@@ -906,7 +906,15 @@ class DISCUSS_W_SELLER__DEMAND__GET_PRICE_KNOWN_PRODUCT_TYPE(MessageHandler):
         )
 
 class DISCUSS_W_SELLER__DEMAND__GET_PRICE_UNKNOWN_PRODUCT_TYPE(MessageHandler):
-    pass
+    def run(self):
+        # Save user input without validation
+        self.save_body_as_string(datas.\
+        DISCUSS_W_SELLER__DEMAND__GET_PRICE_UNKNOWN_PRODUCT_TYPE__PRICE__STRING)
+
+        return self.done_reply(
+            intents.DISCUSS_W_SELLER,
+            messages.DEMAND__THANK_YOU
+        )
 
 class DISCUSS_W_SELLER__DISCUSS__CONFIRM_INTEREST(MessageHandler):
     def _get_connected_env_var(self):
---
+++ chat/tests/test_discuss_w_seller__demand__get_price_unknown_product_type.py
@@ -0,0 +1,23 @@
+from chat.libraries import intents, messages, datas
+from chat.libraries.message_handler_test import MessageHandlerTest
+from chat.tests import texts
+
+class DiscussWSellerGetPriceUnknownProductTypeTest(MessageHandlerTest):
+    def setUp(self):
+        super().setUp(
+            intents.DISCUSS_W_SELLER,
+            messages.DEMAND__GET_PRICE_UNKNOWN_PRODUCT_TYPE
+        )
+    
+    def test_get_price(self):
+        input = 'USD 20 per box'
+        self.receive_reply_assert(
+            input,
+            intents.DISCUSS_W_SELLER,
+            messages.DEMAND__THANK_YOU,
+            texts.DISCUSS_W_SELLER__DEMAND__THANK_YOU
+        )
+        self.assert_value(
+datas.DISCUSS_W_SELLER__DEMAND__GET_PRICE_UNKNOWN_PRODUCT_TYPE__PRICE__STRING,
+            value_string=input
+        )
