cab53f0406a89b461b9a200bb52cb6399f6ce97a
Kevin Ng <kevinngsw@gmail.com>
2021-06-14 21:41:41 +0800

    Add discuss-buyer/get-accept-lc
    Add handlers, data keys, migrations, tests

--- chat/handlers.py
+++ chat/handlers.py
@@ -362,7 +362,20 @@ class DISCUSS_W_BUYER__SUPPLY__GET_DEPOSIT(MessageHandler):
         )
 
 class DISCUSS_W_BUYER__SUPPLY__GET_ACCEPT_LC(MessageHandler):
-    pass
+    def run(self):
+        self.add_option([('1', 0), ('yes', 0)],
+            intents.DISCUSS_W_BUYER,
+            messages.SUPPLY__THANK_YOU, None,
+            datas.DISCUSS_W_BUYER__SUPPLY__GET_ACCEPT_LC__ACCEPT_LC__CHOICE,
+            datas.DISCUSS_W_BUYER__SUPPLY__GET_ACCEPT_LC__ACCEPT_LC__YES
+        )
+        self.add_option([('2', 0), ('no', 0)],
+            intents.DISCUSS_W_BUYER,
+            messages.SUPPLY__THANK_YOU, None,
+            datas.DISCUSS_W_BUYER__SUPPLY__GET_ACCEPT_LC__ACCEPT_LC__CHOICE,
+            datas.DISCUSS_W_BUYER__SUPPLY__GET_ACCEPT_LC__ACCEPT_LC__NO
+        )
+        return self.reply_option()
 
 class DISCUSS_W_BUYER__STILL_INTERESTED__CONFIRM(MessageHandler):
     pass
---
+++ chat/tests/test_discuss_w_buyer__supply__get_accept_lc.py
@@ -0,0 +1,59 @@
+from chat.libraries import intents, messages, datas
+from chat.libraries.message_handler_test import MessageHandlerTest
+from chat.tests import texts
+
+class DiscussWBuyerSupplyGetAcceptLCTest(MessageHandlerTest):
+    def setUp(self):
+        super().setUp(intents.DISCUSS_W_BUYER, messages.SUPPLY__GET_ACCEPT_LC)
+    
+    def choose_non_choice(self, input):
+        self.receive_reply_assert(
+            input,
+            intents.DISCUSS_W_BUYER,
+            messages.SUPPLY__GET_ACCEPT_LC,
+            texts.DO_NOT_UNDERSTAND_OPTION
+        )
+
+    def test_choose_non_choice_with_number(self):
+        self.choose_non_choice('3')
+
+    def test_choose_non_choice_with_text(self):
+        self.choose_non_choice('hello')
+
+    def choose_yes(self, input):
+        self.receive_reply_assert(
+            input,
+            intents.DISCUSS_W_BUYER,
+            messages.SUPPLY__THANK_YOU,
+            texts.DISCUSS_W_BUYER__SUPPLY__THANK_YOU
+        )
+        self.assert_value(
+            datas.DISCUSS_W_BUYER__SUPPLY__GET_ACCEPT_LC__ACCEPT_LC__CHOICE,
+            value_string=\
+                datas.DISCUSS_W_BUYER__SUPPLY__GET_ACCEPT_LC__ACCEPT_LC__YES
+        )
+    
+    def test_choose_yes_with_number(self):
+        self.choose_yes('1')
+
+    def test_choose_yes_with_text(self):
+        self.choose_yes('yes')
+
+    def choose_no(self, input):
+        self.receive_reply_assert(
+            input,
+            intents.DISCUSS_W_BUYER,
+            messages.SUPPLY__THANK_YOU,
+            texts.DISCUSS_W_BUYER__SUPPLY__THANK_YOU
+        )
+        self.assert_value(
+            datas.DISCUSS_W_BUYER__SUPPLY__GET_ACCEPT_LC__ACCEPT_LC__CHOICE,
+            value_string=\
+                datas.DISCUSS_W_BUYER__SUPPLY__GET_ACCEPT_LC__ACCEPT_LC__NO
+        )
+
+    def test_choose_no_with_number(self):
+        self.choose_no('2')
+
+    def test_choose_no_with_text(self):
+        self.choose_no('no')
