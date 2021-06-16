63678e889c37a5cf587c2d4a5218bc35c0a3a285
Kevin Ng <kevinngsw@gmail.com>
2021-06-16 11:18:23 +0800

    Add explain-service
    Add handlers, data keys, target body, tests

--- chat/handlers.py
+++ chat/handlers.py
@@ -46,7 +46,25 @@ class SPEAK_HUMAN__CONFIRM_HUMAN(MessageHandler):
 
 class EXPLAIN_SERVICE__EXPLAIN_SERVICE(MessageHandler):
     def run(self):
-        pass
+        self.add_option([('1', 0), ('find buyers', 3)],
+            intents.NEW_SUPPLY,
+            messages.SUPPLY__GET_PRODUCT, None,
+            datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__CHOICE,
+            datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__FIND_BUYER
+        )
+        self.add_option([('2', 0), ('find sellers', 3)],
+            intents.NEW_DEMAND,
+            messages.DEMAND__GET_PRODUCT, None,
+            datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__CHOICE,
+            datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__FIND_SELLER
+        )
+        self.add_option([('3', 0)],
+            intents.SPEAK_HUMAN,
+            messages.CONFIRM_HUMAN, None,
+            datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__CHOICE,
+            datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__SPEAK_HUMAN
+        )
+        return self.reply_option()
 
 # Registration
 
---
+++ chat/tests/test_explain_service__explain_service.py
@@ -0,0 +1,68 @@
+from chat.libraries import intents, messages, datas
+from chat.libraries.message_handler_test import MessageHandlerTest
+
+class ExplainService__ExplainService__Test(MessageHandlerTest):
+    def setUp(self):
+        super().setUp(intents.EXPLAIN_SERVICE, messages.EXPLAIN_SERVICE)
+
+    def choose_non_choice(self, input):
+        self.receive_reply_assert(
+            input,
+            intents.EXPLAIN_SERVICE,
+            messages.EXPLAIN_SERVICE,
+            target_body_intent_key=intents.NO_INTENT,
+            target_body_message_key=messages.DO_NOT_UNDERSTAND_OPTION
+        )
+
+    def test_choose_non_choice_with_number(self):
+        self.choose_non_choice('10')
+
+    def test_choose_non_choice_with_text(self):
+        self.choose_non_choice('hello')
+
+    def choose_new_supply(self, input):
+        self.receive_reply_assert(
+            input,
+            intents.NEW_SUPPLY,
+            messages.SUPPLY__GET_PRODUCT
+        )
+        self.assert_value(
+            datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__CHOICE,
+            value_string=\
+                datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__FIND_BUYER
+        )
+
+    def test_choose_new_supply_with_number(self):
+        self.choose_new_supply('1')
+
+    def test_choose_new_supply_with_text(self):
+        self.choose_new_supply('find buyers')
+
+    def choose_new_demand(self, input):
+        self.receive_reply_assert(input,
+            intents.NEW_DEMAND,
+            messages.DEMAND__GET_PRODUCT
+        )
+        self.assert_value(
+            datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__CHOICE,
+            value_string=\
+                datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__FIND_SELLER
+        )
+
+    def test_choose_new_demand_with_number(self):
+        self.choose_new_demand('2')
+
+    def test_choose_new_demand_with_text(self):
+        self.choose_new_demand('find sellers')
+
+    def test_speak_human(self):
+        self.receive_reply_assert(
+            '3', # Only number option available
+            intents.SPEAK_HUMAN,
+            messages.CONFIRM_HUMAN
+        )
+        self.assert_value(
+            datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__CHOICE,
+            value_string=\
+                datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__SPEAK_HUMAN
+        )
