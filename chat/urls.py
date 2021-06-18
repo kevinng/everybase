19af8fd9fe1cc7b000c7623b9c490e69e4dec018
Kevin Ng <kevinngsw@gmail.com>
2021-06-18 12:20:55 +0800

    Rename app name from chat_system to chat

--- chat/urls.py
+++ chat/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-app_name = 'chat_system'
+app_name = 'chat'
 webhook_root = 'webhooks'
 urlpatterns = [
+    # Webhooks
     path(f'{webhook_root}/message_v1/',
         views.TwilioIncomingMessageView.as_view()),
 ]
