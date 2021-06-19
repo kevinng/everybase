b1053cf66b9ac73bb816d6dde7c230735d08c043
Kevin Ng <kevinngsw@gmail.com>
2021-06-19 13:41:44 +0800

    Minor , removal in urls_root.py

--- chat/urls_root.py
+++ chat/urls_root.py
@@ -5,5 +5,5 @@ from . import views
 
 app_name = 'chat_root'
 urlpatterns = [
-    path('wa/<str:id>/', views.redirect_whatsapp_phone_number, name='whatsapp'),
+    path('wa/<str:id>/', views.redirect_whatsapp_phone_number, name='whatsapp')
 ]
