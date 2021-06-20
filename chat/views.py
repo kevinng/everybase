a4004473b3e3e541fac8c5ed05574f9fa471dfa1
Kevin Ng <kevinngsw@gmail.com>
2021-06-20 21:19:49 +0800

    Update chat/views.py
    - Log outbound message in reply
    - Integrate Sentry
    - Render error page on error
    - Add redirect_checkout_page

--- chat/views.py
+++ chat/views.py
@@ -2,15 +2,17 @@ import traceback
 from http import HTTPStatus
 
 from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from rest_framework.views import APIView
+import sentry_sdk
 
 from everybase.settings import (TWILIO_AUTH_TOKEN,
-    TWILIO_WEBHOOK_INCOMING_MESSAGES_URL, EVERYBASE_WA_NUMBER_COUNTRY_CODE,
-    EVERYBASE_WA_NUMBER_NATIONAL_NUMBER)
+    TWILIO_WEBHOOK_INCOMING_MESSAGES_URL, STRIPE_PAYMENT_METHOD_TYPES,
+    CHATBOT_PHONE_NUMBER_PK, STRIPE_PUBLISHABLE_API_KEY)
 
 from common.libraries.get_ip_address import get_ip_address
 
+from chat import models
 from relationships import models as relmods
 from payments import models as paymods
 
@@ -19,6 +21,7 @@ from chat.libraries.utilities.save_message_medias import save_message_medias
 from chat.libraries.utilities.save_message import save_message
 from chat.libraries.utilities.get_context import get_context
 from chat.libraries.utilities.get_handler import get_handler
+from chat.libraries.utilities.get_whatsapp_link import get_whatsapp_link
 
 from twilio.request_validator import RequestValidator
 from twilio.twiml.messaging_response import MessagingResponse
@@ -45,6 +48,16 @@ def reply(message):
 
     # Run handler and get message body
     body = handler.run()
+
+    # Log
+    models.TwilioOutboundMessage.objects.create(
+        intent_key=intent_key,
+        message_key=message_key,
+        body=body,
+        to_user=message.from_user,
+        to_phone_number=message.from_phone_number,
+        twilml_response_to=message
+    )
 
     # Return TwilML response string
     response = MessagingResponse()
@@ -97,10 +110,6 @@ def redirect_whatsapp_phone_number(request, id):
         device_family=ua.device.family
     )
 
-    # Note: we use temporary redirects so search engines do not associate our
-    # URLs with WhatsApp phone number links
-    response = HttpResponse(status=302) # Temporary redirect
-
     try:
         hash = relmods.PhoneNumberHash.objects.get(
             pk=id,
@@ -108,21 +117,93 @@ def redirect_whatsapp_phone_number(request, id):
         )
         access.hash = hash
         access.save()
-    except relmods.PhoneNumberHash.DoesNotExist:
-        traceback.print_exc()
-        # TODO: redirect to error page, and send me a notification
-        
+    except relmods.PhoneNumberHash.DoesNotExist as err:
+        sentry_sdk.capture_exception(err)
+
         # Update log status
         access.outcome = relmods.PHONE_NUMBER_ACCESS_FAILED
         access.save()
 
-        return response
+        return render(request, 'chat/pages/error.html', {})
 
-    response['Location'] = 'https://wa.me/' + hash.phone_number.country_code + \
-        hash.phone_number.national_number
+    # Note: we use temporary redirects so search engines do not associate our
+    # URLs with WhatsApp phone number links
+    response = HttpResponse(status=302) # Temporary redirect
+    response['Location'] = get_whatsapp_link(hash.phone_number.country_code,
+        hash.phone_number.national_number)
     
     # Update log status
     access.outcome = relmods.PHONE_NUMBER_ACCESS_SUCCESSFUL
     access.save()
 
     return response
+
+def redirect_checkout_page(request, id):
+    # Log access
+    ua = request.user_agent
+    access = paymods.PaymentLinkAccess.objects.create(
+        ip_address=get_ip_address(request),
+        is_mobile=ua.is_mobile,
+        is_tablet=ua.is_tablet,
+        is_touch_capable=ua.is_touch_capable,
+        is_pc=ua.is_pc,
+        is_bot=ua.is_bot,
+        browser=ua.browser,
+        browser_family=ua.browser.family,
+        browser_version=ua.browser.version,
+        browser_version_string=ua.browser.version_string,
+        os=ua.os,
+        os_family=ua.os.family,
+        os_version=ua.os.version,
+        os_version_string=ua.os.version_string,
+        device=ua.device,
+        device_family=ua.device.family
+    )
+
+    try:
+        hash = paymods.PaymentHash.objects.get(pk=id)
+        access.hash = hash
+        access.save()
+
+        if hash.expired is not None:
+            return render(request, 'chat/pages/expired.html', {})
+    except paymods.PaymentHash.DoesNotExist as err:
+        sentry_sdk.capture_exception(err)
+
+        # Update log status
+        access.outcome = paymods.PAYMENT_LINK_ACCESS_FAILED
+        access.save()
+
+        return render(request, 'chat/pages/error.html', {})
+
+    # Chatbot phone number - for success/cancel URL
+    chatbot_ph = relmods.PhoneNumber.objects.get(pk=CHATBOT_PHONE_NUMBER_PK)
+    end_url = get_whatsapp_link(chatbot_ph.country_code,
+        chatbot_ph.national_number)
+
+    # Create session
+    session = stripe.checkout.Session.create(
+        payment_method_types=STRIPE_PAYMENT_METHOD_TYPES,
+        line_items=[{
+            'price_data': {
+                'currency': hash.currency.programmatic_key,
+                'product_data': { 'name': hash.product_name },
+                'unit_amount': hash.unit_amount
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=end_url,
+        cancel_url=end_url,
+    )
+
+    # Update log status
+    access.outcome = paymods.PAYMENT_LINK_ACCESS_SUCCESSFUL
+    access.save()
+
+    context = {
+        'session_id': session.id,
+        'stripe_publishable_api_key': STRIPE_PUBLISHABLE_API_KEY
+    }
+
+    return render(request, 'chat/pages/checkout.html', context)
