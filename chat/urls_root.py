4872c4ed62094caf1e777296805d688d89fcb946
Kevin Ng <kevinngsw@gmail.com>
2021-06-20 19:25:45 +0800

    Comment unused route for now

--- chat/urls_root.py
+++ chat/urls_root.py
@@ -6,5 +6,5 @@ from . import views
 app_name = 'chat_root'
 urlpatterns = [
     path('wa/<str:id>/', views.redirect_whatsapp_phone_number, name='whatsapp'),
-    path('pay/<str:id>/', views.redirect_checkout_page, name='payment')
+    # path('pay/<str:id>/', views.redirect_checkout_page, name='payment')
 ]
