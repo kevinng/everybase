306c86d050c9ec7ae5870f88c789f53aef3df8c4
Kevin Ng <kevinngsw@gmail.com>
2021-06-20 21:23:01 +0800

    Add methods constant file

---
+++ chat/libraries/constants/methods.py
@@ -0,0 +1,13 @@
+"""Method Keys
+
+Track the methods (and their version) used to process a piece of data.
+
+Key format:
+
+<Data Value Name>__<Method Name>__<Method Semver Version Number>
+"""
+
+# Choices for model fields
+# Note: remember to makemigrations when updating this list
+choices = [
+]
