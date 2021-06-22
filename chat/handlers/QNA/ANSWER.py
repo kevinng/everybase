1b430684d399f3eea047953ff5d301182667e1d8
Kevin Ng <kevinngsw@gmail.com>
2021-06-22 12:16:24 +0800

    Add QNA/answer
    - Handler
    - Target body texts
    - Tests

---
+++ chat/handlers/QNA/ANSWER.py
@@ -0,0 +1,37 @@
+from relationships import models as relmods
+
+from chat.libraries.constants import intents, messages, datas
+from chat.libraries.classes.message_handler import MessageHandler
+
+class Handler(MessageHandler):
+    def _get_match_id(self):
+        if not hasattr(self, '_match_id') or self._match_id is None:
+            # Lazy-instantiate, once-only
+            self._match_id = self.get_latest_value(
+                intents.QNA,
+                messages.YOUR_QUESTION,
+                datas.QNA__YOUR_QUESTION__MATCH_ID__ID,
+                False
+            ).value_id
+        
+        return self._match_id
+
+    def _get_buying_boolean(self):
+        match = relmods.Match.objects.get(pk=self._get_match_id())
+        if match.supply.user == self.message.from_user:
+            # User is seller
+            return False
+        elif match.demand.user == self.message.from_user:
+            # User is buyer
+            return True
+        return None
+
+    def run(self):
+        # Save user input without validation
+        self.save_body_as_string(datas.\
+            QNA__ANSWER__INPUT__STRING)
+        return self.done_reply(
+            intents.QNA,
+            messages.ANSWER_THANK_YOU,
+            { 'buying': self._get_buying_boolean() }
+        )
--- chat/tests/test_qna__answer.py
+++ chat/tests/test_qna__answer.py
@@ -1,11 +1,35 @@
+from relationships import models as relmods
+
 from chat.libraries.constants import intents, messages, datas
 from chat.libraries.classes.message_handler_test import MessageHandlerTest
 
-class QNAAnswerTest(MessageHandlerTest):
+class QNAAnswer_Buying_Test(MessageHandlerTest):
     def setUp(self):
         super().setUp(
             intents.QNA,
             messages.ANSWER
+        )
+
+        # Set up supply - from seller. We use system user to stand-in for the
+        # seller.
+        supply = relmods.Supply.objects.create(user=self.sys_user)
+
+        # Set up demand - from this user.
+        demand = self.set_up_demand()
+
+        # Set up match
+        match = relmods.Match.objects.create(
+            supply=supply,
+            demand=demand
+        )
+
+        # Set up match ID
+        self.set_up_data_value(
+            intents.QNA,
+            messages.YOUR_QUESTION,
+            datas.QNA__YOUR_QUESTION__MATCH_ID__ID,
+            value_id=match.id,
+            inbound=False
         )
 
     def test_any_input(self):
@@ -13,7 +37,50 @@ class QNAAnswerTest(MessageHandlerTest):
         self.receive_reply_assert(
             input,
             intents.QNA,
-            messages.ANSWER_THANK_YOU
+            messages.ANSWER_THANK_YOU,
+            target_body_variation_key='BUYING'
+        )
+        self.assert_value(
+            datas.QNA__ANSWER__INPUT__STRING,
+            value_string=input
+        )
+
+class QNAAnswer_Selling_Test(MessageHandlerTest):
+    def setUp(self):
+        super().setUp(
+            intents.QNA,
+            messages.ANSWER
+        )
+
+        # Set up supply - from this user.        
+        supply = self.set_up_supply()
+
+        # Set up demand - from buyer. We use system user to stand-in for the
+        # buyer.
+        demand = relmods.Demand.objects.create(user=self.sys_user)
+
+        # Set up match
+        match = relmods.Match.objects.create(
+            supply=supply,
+            demand=demand
+        )
+
+        # Set up match ID
+        self.set_up_data_value(
+            intents.QNA,
+            messages.YOUR_QUESTION,
+            datas.QNA__YOUR_QUESTION__MATCH_ID__ID,
+            value_id=match.id,
+            inbound=False
+        )
+
+    def test_any_input(self):
+        input = 'hello world'
+        self.receive_reply_assert(
+            input,
+            intents.QNA,
+            messages.ANSWER_THANK_YOU,
+            target_body_variation_key='SELLING'
         )
         self.assert_value(
             datas.QNA__ANSWER__INPUT__STRING,
