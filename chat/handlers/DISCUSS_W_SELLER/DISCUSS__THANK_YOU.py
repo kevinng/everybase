cf8122b084ae09f62c3daea86ad57a10e32387e3
Kevin Ng <kevinngsw@gmail.com>
2021-06-22 16:29:45 +0800

    Update discuss-seller/thank-you save invalid
    - Handler
    - Data keys and migrations
    - Tests

--- chat/handlers/DISCUSS_W_SELLER/DISCUSS__THANK_YOU.py
+++ chat/handlers/DISCUSS_W_SELLER/DISCUSS__THANK_YOU.py
@@ -1,4 +1,7 @@
+from chat.libraries.constants import datas
 from chat.libraries.classes.menu_handler import MenuHandler
 
 class Handler(MenuHandler):
-    pass
+    def __init__(self, message, intent_key, message_key):
+        super().__init__(message, intent_key, message_key,
+        datas.DISCUSS_W_SELLER__DISCUSS__THANK_YOU__INVALID_CHOICE__STRING)
