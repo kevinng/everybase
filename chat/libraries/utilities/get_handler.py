aa07ddcfcdcc5a7d3f789a9d9e73eb463a3e1edc
Kevin Ng <kevinngsw@gmail.com>
2021-07-01 19:10:38 +0800

    Update get_handler documentation and add typing

--- chat/libraries/utilities/get_handler.py
+++ chat/libraries/utilities/get_handler.py
@@ -1,19 +1,28 @@
 import importlib
+from chat.models import TwilioInboundMessage
+from chat.libraries.classes.message_handler import MessageHandler
 
-def get_handler(message, intent_key, message_key):
-    """Instantiate the right handler for a context (i.e., a unique
-    intent/message pair) and return it
+def get_handler(
+        message: TwilioInboundMessage,
+        intent_key: str,
+        message_key: str
+    ) -> MessageHandler:
+    """Instantiate and return the message handler for a context (i.e., a unique
+    intent/message pair). Returns None if no message handler found.
 
     Parameters
     ----------
-    message : TwilioInboundMessage
+    message
         Incoming message to be handled
-    intent_key : String
-        Intent key for the user's context
-    message_key : String
-        Message key for the user's context
+    intent_key
+        Intent key for the context
+    message_key
+        Message key for the context
     """
     path = 'chat.handlers.%s.%s' % (intent_key, message_key)
-    module = importlib.import_module(path)
-    handler = module.Handler(message, intent_key, message_key)
-    return handler
+    try:
+        module = importlib.import_module(path)
+    except ModuleNotFoundError:
+        return None
+
+    return module.Handler(message, intent_key, message_key)
