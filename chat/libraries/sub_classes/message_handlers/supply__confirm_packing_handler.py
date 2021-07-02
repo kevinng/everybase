6cb4931562825a31656d061a24eddf5dbdccc6ae
Kevin Ng <kevinngsw@gmail.com>
2021-07-03 02:05:27 +0800

    Fix bug supply/confirm-packing parent handler

---
+++ chat/libraries/sub_classes/message_handlers/supply__confirm_packing_handler.py
@@ -0,0 +1,29 @@
+from chat.libraries.constants import messages, datas
+from chat.libraries.classes.message_handler import MessageHandler
+from chat.libraries.classes.context_logic import ContextLogic
+
+class SupplyConfirmPackingHandler(MessageHandler):
+    def run(self):
+        def yes_message_key():
+            logic = ContextLogic(self)
+            if logic.is_ready_otg():
+                if logic.is_known_packing():
+                    return messages.SUPPLY__GET_QUANTITY_READY_OTG_KNOWN_PACKING
+                else:
+                    return messages.\
+                        SUPPLY__GET_QUANTITY_READY_OTG_UNKNOWN_PACKING
+            elif logic.is_pre_order():
+                return messages.SUPPLY__GET_QUANTITY_PRE_ORDER
+
+        self.add_option([('1', 0), ('yes', 0)],
+            self.intent_key,
+            params_func=yes_message_key,
+            data_key=datas.CONFIRM_PACKING,
+            data_value=datas.CONFIRM_PACKING__YES)
+        self.add_option([('2', 0), ('no', 0)],
+            self.intent_key,
+            messages.SUPPLY__GET_PACKING,
+            datas.CONFIRM_PACKING,
+            datas.CONFIRM_PACKING__NO)
+
+        return self.reply_option()
