27aab81bd7780caf5a3a87f91b488ac1bd71faa7
Kevin Ng <kevinngsw@gmail.com>
2021-07-03 00:50:20 +0800

    Add dis-buy/get-country test

--- chat/tests/test_discuss_w_buyer__supply__get_country_state_pre_order.py
+++ chat/tests/test_discuss_w_buyer__supply__get_country_state_pre_order.py
@@ -2,25 +2,25 @@ from chat.libraries.constants import intents, messages, datas
 from chat.libraries.classes.message_handler_test import MessageHandlerTest
 
 class DiscussWBuyerSupplyGetCountryStateTest(MessageHandlerTest):
-    def set_up_known_product(self):
+    def setup_known_product(self):
         # Set up a product, and have user enter a search phrase that matches
         # this product exactly
-        _, _, kw = self.set_up_product_type(
+        _, _, kw = self.setup_product_type(
             uom_description='200 pieces in 1 box')
-        self.set_up_data_value(
+        self.setup_data_value(
             intents.DISCUSS_W_BUYER,
             messages.SUPPLY__GET_PRODUCT,
-            datas.DISCUSS_W_BUYER__SUPPLY__GET_PRODUCT__PRODUCT_TYPE__STRING,
+            datas.PRODUCT,
             kw.keyword
         )
 
-    def set_up_unknown_product(self):
+    def setup_unknown_product(self):
         # Set up the user to enter a string that's unlikely to match a known
         # product
-        self.set_up_data_value(
+        self.setup_data_value(
             intents.DISCUSS_W_BUYER,
             messages.SUPPLY__GET_PRODUCT,
-            datas.DISCUSS_W_BUYER__SUPPLY__GET_PRODUCT__PRODUCT_TYPE__STRING,
+            datas.PRODUCT,
             'SR2EdiLuc1K1BUAVsPg4' # Unlikely string to match a known product
         )
 
@@ -31,7 +31,7 @@ class DiscussWBuyerSupplyGetCountryStatePreOrder_KnownProduct_Test(
             intents.DISCUSS_W_BUYER,
             messages.SUPPLY__GET_COUNTRY_STATE_PRE_ORDER
         )
-        self.set_up_known_product()
+        self.setup_known_product()
 
     def test_enter_country_state(self):
         input = 'singapore'
@@ -40,8 +40,8 @@ class DiscussWBuyerSupplyGetCountryStatePreOrder_KnownProduct_Test(
             intents.DISCUSS_W_BUYER,
             messages.SUPPLY__CONFIRM_PACKING
         )
-        self.assert_value(datas.\
-    DISCUSS_W_BUYER__SUPPLY__GET_COUNTRY_STATE_PRE_ORDER__COUNTRY_STATE__STRING,
+        self.assert_value(
+            datas.COUNTRY_STATE,
             value_string=input
         )
 
@@ -52,7 +52,7 @@ class DiscussWBuyerSupplyGetCountryStatePreOrder_UnknownProduct_Test(
             intents.DISCUSS_W_BUYER,
             messages.SUPPLY__GET_COUNTRY_STATE_PRE_ORDER
         )
-        self.set_up_unknown_product()
+        self.setup_unknown_product()
 
     def test_enter_country_state(self):
         input = 'singapore'
@@ -61,7 +61,7 @@ class DiscussWBuyerSupplyGetCountryStatePreOrder_UnknownProduct_Test(
             intents.DISCUSS_W_BUYER,
             messages.SUPPLY__GET_PACKING
         )
-        self.assert_value(datas.\
-    DISCUSS_W_BUYER__SUPPLY__GET_COUNTRY_STATE_PRE_ORDER__COUNTRY_STATE__STRING,
+        self.assert_value(
+            datas.COUNTRY_STATE,
             value_string=input
         )
