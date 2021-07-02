bd47fc5a4c12e4c9e3183c9ecc779c38e1e24bef
Kevin Ng <kevinngsw@gmail.com>
2021-07-02 16:14:21 +0800

    Add test QNA/please-pay

---
+++ chat/tests/test_qna__please_pay.py
@@ -0,0 +1,93 @@
+from chat.libraries.constants import intents, messages, datas
+from chat.libraries.classes.message_handler_test import (MessageHandlerTest,
+    SupplyAvailabilityOption)
+
+class QNAPleasePayTest(MessageHandlerTest):
+    fixtures = [
+        'setup/common__country.json',
+        'setup/20210528__payments__currency.json',
+        'setup/20210527__relationships__availability.json'
+    ]
+
+    def setUp(self):
+        super().setUp(intents.QNA, messages.PLEASE_PAY)
+
+    def _test_stray_input(
+            self,
+            intent_key: str,
+            message_key: str,
+            target_body_variation_key: str = None
+        ):
+        input = 'hello world'
+        self.receive_reply_assert(
+            input,
+            intent_key,
+            message_key,
+            target_body_variation_key=target_body_variation_key
+        )
+        self.assert_value(
+            datas.STRAY_INPUT,
+            value_string=input
+        )
+
+class QNAPleasePay_AnsweredNoFollowUpQuestion_Buying_Test(QNAPleasePayTest):
+    """User is buying, answered seller's question, not followed up with a
+    question to the seller."""
+
+    def setUp(self):
+        super().setUp()
+        self.setup_buyer(SupplyAvailabilityOption.OTG)
+        self.setup_qna(answering=True, answered=True)
+
+    def test_stray_input(self):
+        self._test_stray_input(
+            intents.QNA,
+            messages.QNA__THANK_YOU,
+            target_body_variation_key='NOT_INITIAL_BUYING'
+        )
+
+class QNAPleasePay_AnsweredNoFollowUpQuestion_Selling_Test(QNAPleasePayTest):
+    """User is selling, answered seller's question, not followed up with a
+    question to the seller."""
+    def setUp(self):
+        super().setUp()
+        self.setup_seller()
+        self.setup_qna(answering=True, answered=True)
+
+    def test_stray_input(self):
+        self._test_stray_input(
+            intents.QNA,
+            messages.QNA__THANK_YOU,
+            target_body_variation_key='NOT_INITIAL_SELLING'
+        )
+
+class QNAPleasePay_NotAnsweredNoFollowUpQuestion_Buying_Test(QNAPleasePayTest):
+    """User is buying, NOT answered seller's question, not followed up with a
+    question to the seller."""
+
+    def setUp(self):
+        super().setUp()
+        self.setup_buyer(SupplyAvailabilityOption.OTG)
+        self.setup_qna(answering=True, answered=False)
+
+    def test_stray_input(self):
+        self._test_stray_input(
+            intents.QNA,
+            messages.YOUR_QUESTION,
+            target_body_variation_key='BUYING'
+        )
+
+class QNAPleasePay_NotAnsweredNoFollowUpQuestion_Selling_Test(QNAPleasePayTest):
+    """User is selling, NOT answered seller's question, not followed up with a
+    question to the seller."""
+    def setUp(self):
+        super().setUp()
+        self.setup_seller()
+        self.setup_qna(answering=True, answered=False)
+
+    def test_stray_input(self):
+        self._test_stray_input(
+            intents.QNA,
+            messages.YOUR_QUESTION,
+            target_body_variation_key='SELLING'
+        )
