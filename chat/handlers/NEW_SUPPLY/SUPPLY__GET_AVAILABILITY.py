100bfa248570b10348da815d1bca5d4b885094d9
Kevin Ng <kevinngsw@gmail.com>
2021-07-02 23:25:20 +0800

    Add new-sup/get-avail handler

--- chat/handlers/NEW_SUPPLY/SUPPLY__GET_AVAILABILITY.py
+++ chat/handlers/NEW_SUPPLY/SUPPLY__GET_AVAILABILITY.py
@@ -1,21 +1,5 @@
-from chat.libraries.constants import intents, messages, datas
-from chat.libraries.classes.message_handler import MessageHandler
+from chat.libraries.sub_classes.message_handlers.\
+    supply__get_availability_handler import SupplyGetAvailabilityHandler
 
-class Handler(MessageHandler):
-    def run(self):
-        self.add_option([('1', 0), ('otg', 0), ('ready', 1)],
-            intents.NEW_SUPPLY,
-            messages.SUPPLY__GET_COUNTRY_STATE_READY_OTG,
-            None,
-            datas.NEW_SUPPLY__SUPPLY__GET_AVAILABILITY__AVAILABILITY__CHOICE,
-            datas.NEW_SUPPLY__SUPPLY__GET_AVAILABILITY__AVAILABILITY__READY_OTG
-        )
-        self.add_option([('2', 0), ('pre order', 3)],
-            intents.NEW_SUPPLY,
-            messages.SUPPLY__GET_COUNTRY_STATE_PRE_ORDER,
-            None,
-            datas.NEW_SUPPLY__SUPPLY__GET_AVAILABILITY__AVAILABILITY__CHOICE,
-            datas.NEW_SUPPLY__SUPPLY__GET_AVAILABILITY__AVAILABILITY__PRE_ORDER
-        )
-        return self.reply_option(
-            datas.NEW_SUPPLY__SUPPLY__GET_AVAILABILITY__INVALID_CHOICE__STRING)
+class Handler(SupplyGetAvailabilityHandler):
+    pass
