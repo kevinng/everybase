f37876b875d5e23ba73dee642710898e7282239f
Kevin Ng <kevinngsw@gmail.com>
2021-07-02 21:56:21 +0800

    Improve match-closed check stop-dis-reason

--- chat/handlers/QNA/STOP_DISCUSSION__REASON.py
+++ chat/handlers/QNA/STOP_DISCUSSION__REASON.py
@@ -1,12 +1,13 @@
 from chat.libraries.constants import intents, messages, datas
 from chat.libraries.classes.message_handler import MessageHandler
+from chat.libraries.classes.context_logic import ContextLogic
 
 class Handler(MessageHandler):
     def run(self):
-        # Save user input without validation
-        self.save_body_as_string(datas.\
-            QNA__STOP_DISCUSSION__REASON__INPUT__STRING)
-        return self.done_reply(
-            intents.QNA,
-            messages.STOP_DISCUSSION__THANK_YOU
-        )
+        self.save_body_as_string(datas.STOP_DISCUSSION__REASON)
+
+        match = ContextLogic(self).get_match()
+        if match is not None and match.closed is not None:
+            return self.done_reply(intents.MENU, messages.MENU)
+
+        return self.done_reply(intents.QNA, messages.STOP_DISCUSSION__THANK_YOU)
