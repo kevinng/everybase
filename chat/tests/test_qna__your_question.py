7951041f5abeedaa6893756f2993d0091b4ebad2
Kevin Ng <kevinngsw@gmail.com>
2021-07-02 16:37:37 +0800

    Add qna/your-question test

--- chat/tests/test_qna__your_question.py
+++ chat/tests/test_qna__your_question.py
@@ -1,53 +1,17 @@
-from django.utils import timezone
-
-from relationships import models as relmods
-from payments import models as paymods
-
+from chat.libraries.classes.context_logic import ContextLogic
 from chat.libraries.constants import intents, messages, datas
-from chat.libraries.classes.message_handler_test import MessageHandlerTest
+from chat.libraries.classes.message_handler_test import MessageHandlerTest, SupplyAvailabilityOption
 from chat.libraries.utilities.get_payment_link import get_payment_link
 
-class QNAYourQuestion_Buying_Test(MessageHandlerTest):
+class QNAYourQuestion(MessageHandlerTest):
     fixtures = [
-        'setup/20210528__payments__currency.json'
+        'setup/common__country.json',
+        'setup/20210528__payments__currency.json',
+        'setup/20210527__relationships__availability.json'
     ]
 
     def setUp(self):
-        super().setUp(
-            intents.QNA,
-            messages.YOUR_QUESTION
-        )
-        
-        # Set up supply - from seller. We use system user to stand-in for the
-        # seller.
-        supply = relmods.Supply.objects.create(user=self.sys_user)
-
-        # Set up demand - from this user.
-        demand = self.set_up_demand()
-
-        # Set up match
-        match = relmods.Match.objects.create(
-            supply=supply,
-            demand=demand
-        )
-
-        # Set up match ID
-        self.set_up_data_value(
-            intents.QNA,
-            messages.YOUR_QUESTION,
-            datas.QNA__YOUR_QUESTION__MATCH_ID__ID,
-            value_id=match.id,
-            inbound=False
-        )
-
-        # Set up payment hash
-        usd = paymods.Currency.objects.get(pk=1)
-        self._hash = paymods.PaymentHash.objects.create(
-            user=self.user,
-            match=match,
-            currency=usd,
-            unit_amount=5.67
-        )
+        super().setUp(intents.QNA, messages.YOUR_QUESTION)
 
     def choose_non_choice(self, input):
         self.receive_reply_assert(
@@ -57,10 +21,48 @@ class QNAYourQuestion_Buying_Test(MessageHandlerTest):
             target_body_intent_key=intents.NO_INTENT,
             target_body_message_key=messages.DO_NOT_UNDERSTAND_OPTION
         )
-        self.assert_value(
-            datas.QNA__YOUR_QUESTION__INVALID_CHOICE__STRING,
-            value_string=input
+        self.assert_value(datas.INVALID_CHOICE, value_string=input)
+
+    def _test_choose_answer(self, target_body_variation_key):
+        self.receive_reply_assert(
+            '1',
+            intents.QNA,
+            messages.ANSWER,
+            target_body_variation_key=target_body_variation_key
         )
+        self.assert_value(datas.QNA, value_string=datas.QNA__ANSWER_QUESTION)
+
+    def _test_choose_buy_contact(self, target_body_variation_key):
+        def get_params():
+            return { 'payment_link': get_payment_link(self.payment_hash) }
+        
+        self.receive_reply_assert(
+            '2',
+            intents.QNA,
+            messages.PLEASE_PAY,
+            target_body_variation_key=target_body_variation_key,
+            target_body_params_func=get_params
+        )
+        self.assert_value(datas.QNA, value_string=datas.QNA__BUY_CONTACT)
+
+    def _test_choose_stop_discussion(self, target_body_variation_key):
+        self.receive_reply_assert(
+            '3',
+            intents.QNA,
+            messages.STOP_DISCUSSION__REASON,
+            target_body_variation_key=target_body_variation_key
+        )
+        self.assert_value(datas.QNA, value_string=datas.QNA__STOP_DISCUSSION)
+
+class QNAYourQuestion_Buying_Test(QNAYourQuestion):
+    def setUp(self):
+        super().setUp()
+        self.setup_buyer(SupplyAvailabilityOption.OTG)
+        self.setup_payment_hash()
+        self.setup_qna()
+
+    # Note: non-choice must be tested in the subclass because the setup is not
+    # complete
 
     def test_choose_non_choice_with_number(self):
         self.choose_non_choice('10')
@@ -69,101 +71,23 @@ class QNAYourQuestion_Buying_Test(MessageHandlerTest):
         self.choose_non_choice('hello')
 
     def test_choose_answer(self):
-        self.receive_reply_assert(
-            '1',
-            intents.QNA,
-            messages.ANSWER,
-            target_body_variation_key='BUYING'
-        )
-        self.assert_value(
-            datas.QNA__YOUR_QUESTION__OPTION__CHOICE,
-            value_string=datas.QNA__YOUR_QUESTION__OPTION__ANSWER_QUESTION
-        )
-
-    def _get_please_pay_params(self):
-        return {
-            'payment_link': get_payment_link(self._hash.id)
-        }
+        self._test_choose_answer('BUYING')
 
     def test_choose_buy_contact(self):
-        self.receive_reply_assert(
-            '2',
-            intents.CONNECT_QUESTION,
-            messages.PLEASE_PAY,
-            target_body_variation_key='BUYING',
-            target_body_params_func=self._get_please_pay_params
-        )
-        self.assert_value(
-            datas.QNA__YOUR_QUESTION__OPTION__CHOICE,
-            value_string=datas.QNA__YOUR_QUESTION__OPTION__BUY_CONTACT
-        )
+        self._test_choose_buy_contact('BUYING')
 
     def test_choose_stop_discussion(self):
-        self.receive_reply_assert(
-            '3',
-            intents.QNA,
-            messages.STOP_DISCUSSION__REASON,
-            target_body_variation_key='BUYING'
-        )
-        self.assert_value(
-            datas.QNA__YOUR_QUESTION__OPTION__CHOICE,
-            value_string=datas.QNA__YOUR_QUESTION__OPTION__STOP_DISCUSSION
-        )
+        self._test_choose_stop_discussion('BUYING')
 
-class QNAYourQuestion_Selling_Test(MessageHandlerTest):
-    fixtures = [
-        'setup/20210528__payments__currency.json'
-    ]
-
+class QNAYourQuestion_Selling_Test(QNAYourQuestion):
     def setUp(self):
-        super().setUp(
-            intents.QNA,
-            messages.YOUR_QUESTION
-        )
-        
-        # Set up supply - from this user.        
-        supply = self.set_up_supply()
+        super().setUp()
+        self.setup_seller()
+        self.setup_payment_hash()
+        self.setup_qna()
 
-        # Set up demand - from buyer. We use system user to stand-in for the
-        # buyer.
-        demand = relmods.Demand.objects.create(user=self.sys_user)
-
-        # Set up match
-        match = relmods.Match.objects.create(
-            supply=supply,
-            demand=demand
-        )
-
-        # Set up match ID
-        self.set_up_data_value(
-            intents.QNA,
-            messages.YOUR_QUESTION,
-            datas.QNA__YOUR_QUESTION__MATCH_ID__ID,
-            value_id=match.id,
-            inbound=False
-        )
-
-        # Set up payment hash
-        usd = paymods.Currency.objects.get(pk=1)
-        self._hash = paymods.PaymentHash.objects.create(
-            user=self.user,
-            match=match,
-            currency=usd,
-            unit_amount=5.67
-        )
-
-    def choose_non_choice(self, input):
-        self.receive_reply_assert(
-            input,
-            intents.QNA,
-            messages.YOUR_QUESTION,
-            target_body_intent_key=intents.NO_INTENT,
-            target_body_message_key=messages.DO_NOT_UNDERSTAND_OPTION
-        )
-        self.assert_value(
-            datas.QNA__YOUR_QUESTION__INVALID_CHOICE__STRING,
-            value_string=input
-        )
+    # Note: non-choice must be tested in the subclass because the setup is not
+    # complete
 
     def test_choose_non_choice_with_number(self):
         self.choose_non_choice('10')
@@ -172,80 +96,19 @@ class QNAYourQuestion_Selling_Test(MessageHandlerTest):
         self.choose_non_choice('hello')
 
     def test_choose_answer(self):
-        self.receive_reply_assert(
-            '1',
-            intents.QNA,
-            messages.ANSWER,
-            target_body_variation_key='SELLING'
-        )
-        self.assert_value(
-            datas.QNA__YOUR_QUESTION__OPTION__CHOICE,
-            value_string=datas.QNA__YOUR_QUESTION__OPTION__ANSWER_QUESTION
-        )
-
-    def _get_please_pay_params(self):
-        return {
-            'payment_link': get_payment_link(self._hash.id)
-        }
+        self._test_choose_answer('SELLING')
 
     def test_choose_buy_contact(self):
-        self.receive_reply_assert(
-            '2',
-            intents.CONNECT_QUESTION,
-            messages.PLEASE_PAY,
-            target_body_variation_key='SELLING',
-            target_body_params_func=self._get_please_pay_params
-        )
-        self.assert_value(
-            datas.QNA__YOUR_QUESTION__OPTION__CHOICE,
-            value_string=datas.QNA__YOUR_QUESTION__OPTION__BUY_CONTACT
-        )
+        self._test_choose_buy_contact('SELLING')
 
     def test_choose_stop_discussion(self):
-        self.receive_reply_assert(
-            '3',
-            intents.QNA,
-            messages.STOP_DISCUSSION__REASON,
-            target_body_variation_key='SELLING'
-        )
-        self.assert_value(
-            datas.QNA__YOUR_QUESTION__OPTION__CHOICE,
-            value_string=datas.QNA__YOUR_QUESTION__OPTION__STOP_DISCUSSION
-        )
+        self._test_choose_stop_discussion('SELLING')
 
-class QNAYourQuestion_MatchClosed_Test(MessageHandlerTest):
+class QNAYourQuestion_MatchClosed_Test(QNAYourQuestion):
     def setUp(self):
-        super().setUp(
-            intents.QNA,
-            messages.YOUR_QUESTION
-        )
-
-        # Set user's name
-        self.user.name = 'Kevin Ng'
-        self.user.save()
-        
-        # Set up supply - from seller. We use system user to stand-in for the
-        # seller.
-        supply = relmods.Supply.objects.create(user=self.sys_user)
-
-        # Set up demand - from this user.
-        demand = self.set_up_demand()
-
-        # Set up match
-        match = relmods.Match.objects.create(
-            supply=supply,
-            demand=demand,
-            closed=timezone.now()
-        )
-
-        # Set up match ID
-        self.set_up_data_value(
-            intents.QNA,
-            messages.YOUR_QUESTION,
-            datas.QNA__YOUR_QUESTION__MATCH_ID__ID,
-            value_id=match.id,
-            inbound=False
-        )
+        super().setUp()
+        self.setup_seller(closed=True)
+        self.setup_qna()
 
     def test_choose_any_option(self):
         self.receive_reply_assert(
