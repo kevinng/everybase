0a45d07a5dbcd57951ce59ab97b24cf74324943a
Kevin Ng <kevinngsw@gmail.com>
2021-07-02 22:33:26 +0800

    Add comments to explain-ser handler

--- chat/handlers/EXPLAIN_SERVICE/EXPLAIN_SERVICE.py
+++ chat/handlers/EXPLAIN_SERVICE/EXPLAIN_SERVICE.py
@@ -1,19 +1,6 @@
-from chat.libraries.constants import intents, messages, datas
-from chat.libraries.classes.message_handler import MessageHandler
+from chat.libraries.sub_classes.message_handlers.menu_handler import MenuHandler
 
-class Handler(MessageHandler):
-    def run(self):
-        self.add_option([('1', 0), ('find buyers', 3)],
-            intents.NEW_SUPPLY,
-            messages.SUPPLY__GET_PRODUCT, None,
-            datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__CHOICE,
-            datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__FIND_BUYER
-        )
-        self.add_option([('2', 0), ('find sellers', 3)],
-            intents.NEW_DEMAND,
-            messages.DEMAND__GET_PRODUCT, None,
-            datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__CHOICE,
-            datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__OPTION__FIND_SELLER
-        )
-        return self.reply_option(
-            datas.EXPLAIN_SERVICE__EXPLAIN_SERVICE__INVALID_OPTION__STRING)
+class Handler(MenuHandler):
+    """We're extending menu handler even though we're not presenting the last
+    option - which leads to this page"""
+    pass
