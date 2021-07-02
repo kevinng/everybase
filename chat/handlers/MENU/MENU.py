4164e9176b3892f4e12155f8f7a038fede690048
Kevin Ng <kevinngsw@gmail.com>
2021-07-02 22:45:14 +0800

    Add menu handler

--- chat/handlers/MENU/MENU.py
+++ chat/handlers/MENU/MENU.py
@@ -1,7 +1,4 @@
-from chat.libraries.constants import datas
-from chat.libraries.classes.menu_handler import MenuHandler
+from chat.libraries.sub_classes.message_handlers.menu_handler import MenuHandler
 
 class Handler(MenuHandler):
-    def __init__(self, message, intent_key, message_key):
-        super().__init__(message, intent_key, message_key,
-        datas.MENU__MENU__OPTION__INVALID_CHOICE__STRING)
+    pass
