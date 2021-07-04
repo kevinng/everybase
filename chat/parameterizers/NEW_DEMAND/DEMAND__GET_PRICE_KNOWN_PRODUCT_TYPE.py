debcd1bbca48c0a86ea91018b7575cfe4d189649
Kevin Ng <kevinngsw@gmail.com>
2021-07-04 20:30:23 +0800

    Add demand/price/known parameterizer

---
+++ chat/parameterizers/NEW_DEMAND/DEMAND__GET_PRICE_KNOWN_PRODUCT_TYPE.py
@@ -0,0 +1,6 @@
+from chat.libraries.sub_classes.parameterizers.\
+    demand__get_price_known_product_type_parameterizer import \
+    DemandGetPriceKnownProductTypeParameterizer
+
+class Parameterizer(DemandGetPriceKnownProductTypeParameterizer):
+    pass
