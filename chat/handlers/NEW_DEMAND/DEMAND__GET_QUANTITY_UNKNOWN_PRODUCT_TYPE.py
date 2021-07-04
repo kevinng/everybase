0d482c9b79a406c7f95b4c383a13bfba7e9593c8
Kevin Ng <kevinngsw@gmail.com>
2021-07-04 20:50:55 +0800

    Add demand/quantity/product handler

--- chat/handlers/NEW_DEMAND/DEMAND__GET_QUANTITY_UNKNOWN_PRODUCT_TYPE.py
+++ chat/handlers/NEW_DEMAND/DEMAND__GET_QUANTITY_UNKNOWN_PRODUCT_TYPE.py
@@ -1,13 +1,6 @@
-from chat.libraries.constants import intents, messages, datas
-from chat.libraries.classes.message_handler import MessageHandler
+from chat.libraries.sub_classes.message_handlers.\
+    demand__get_quantity_unknown_product_type_handler import \
+    DemandGetQuantityUnknownProductTypeHandler
 
-class Handler(MessageHandler):
-    def run(self):
-        # Save user input without validation
-        self.save_body_as_string(datas.\
-        NEW_DEMAND__DEMAND__GET_QUANTITY_UNKNOWN_PRODUCT_TYPE__QUANTITY__STRING)
-
-        return self.done_reply(
-            intents.NEW_DEMAND,
-            messages.DEMAND__GET_PRICE_UNKNOWN_PRODUCT_TYPE
-        )
+class Handler(DemandGetQuantityUnknownProductTypeHandler):
+    pass
