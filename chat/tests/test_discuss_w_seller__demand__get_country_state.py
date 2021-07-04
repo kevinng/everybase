232bfe85218abc9b4cb173973e4eaed12dc0c22d
Kevin Ng <kevinngsw@gmail.com>
2021-07-04 19:47:02 +0800

    Add dis-sell/get-country test

--- chat/tests/test_discuss_w_seller__demand__get_country_state.py
+++ chat/tests/test_discuss_w_seller__demand__get_country_state.py
@@ -5,23 +5,23 @@ class DiscussWSellerDemandGetCountryStateTest(MessageHandlerTest):
     def set_up_known_product(self):
         # Set up a product, and have the user enter a term that matches the
         # known product
-        _, _, kw = self.set_up_product_type(
+        _, _, kw = self.setup_product_type(
             uom_description='200 jams in 1 jar',
             uom_plural_name='jars'
         )
-        self.set_up_data_value(
+        self.setup_data_value(
             intents.DISCUSS_W_SELLER,
             messages.DEMAND__GET_PRODUCT,
-            datas.DISCUSS_W_SELLER__DEMAND__GET_PRODUCT__PRODUCT_TYPE__STRING,
+            datas.PRODUCT,
             kw.keyword
         )
 
     def set_up_unknown_product(self):
         # Have the user enter a term that does not match any known product
-        self.set_up_data_value(
+        self.setup_data_value(
             intents.DISCUSS_W_SELLER,
             messages.DEMAND__GET_PRODUCT,
-            datas.DISCUSS_W_SELLER__DEMAND__GET_PRODUCT__PRODUCT_TYPE__STRING,
+            datas.PRODUCT,
             'yWisrFJcMovxRwFyrHHH' # Unlikely term to match any known product
         )
 
@@ -42,7 +42,7 @@ class DiscussWSellerGetCountryState_KnownProduct_Test(
             messages.DEMAND__GET_QUANTITY_KNOWN_PRODUCT_TYPE
         )
         self.assert_value(
-    datas.DISCUSS_W_SELLER__DEMAND__GET_COUNTRY_STATE__COUNTRY_STATE__STRING,
+            datas.COUNTRY_STATE,
             value_string=input
         )
 
@@ -63,6 +63,6 @@ class DiscussWSellerGetCountryState_UnknownProduct_Test(
             messages.DEMAND__GET_QUANTITY_UNKNOWN_PRODUCT_TYPE
         )
         self.assert_value(
-    datas.DISCUSS_W_SELLER__DEMAND__GET_COUNTRY_STATE__COUNTRY_STATE__STRING,
+            datas.COUNTRY_STATE,
             value_string=input
         )
