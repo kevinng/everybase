77fe383c6666f9d7d63976b7df980716ac64571e
Kevin Ng <kevinngsw@gmail.com>
2021-07-04 19:58:13 +0800

    Add demand/get-qty/known-ptype parameterizer

---
+++ chat/parameterizers/NEW_DEMAND/DEMAND__GET_QUANTITY_KNOWN_PRODUCT_TYPE.py
@@ -0,0 +1,6 @@
+from chat.libraries.sub_classes.parameterizers.\
+    demand__get_quantity_known_product_type_parameterizer import \
+    DemandGetQuantityKnownProductTypeParameterizer
+
+class Parameterizer(DemandGetQuantityKnownProductTypeParameterizer):
+    pass
