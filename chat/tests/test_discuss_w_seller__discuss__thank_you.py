2f02150b3a95b35b26be405f068e54b83b545d46
Kevin Ng <kevinngsw@gmail.com>
2021-07-05 11:42:26 +0800

    Add dis-sell/ty test

--- chat/tests/test_discuss_w_seller__discuss__thank_you.py
+++ chat/tests/test_discuss_w_seller__discuss__thank_you.py
@@ -18,7 +18,7 @@ class DiscussWSellerDiscussThankYouTest(MessageHandlerTest):
             target_body_message_key=messages.DO_NOT_UNDERSTAND_OPTION
         )
         self.assert_value(
-            datas.DISCUSS_W_SELLER__DISCUSS__THANK_YOU__INVALID_CHOICE__STRING,
+            datas.INVALID_CHOICE,
             value_string=input
         )
     
@@ -35,15 +35,15 @@ class DiscussWSellerDiscussThankYouTest(MessageHandlerTest):
             messages.SUPPLY__GET_PRODUCT
         )
         self.assert_value(
-            datas.MENU__MENU__OPTION__CHOICE,
-            value_string=datas.MENU__MENU__OPTION__FIND_BUYER
+            datas.MENU,
+            value_string=datas.MENU__FIND_BUYERS
         )
 
     def test_choose_new_supply_with_number(self):
         self.choose_new_supply('1')
 
     def test_choose_new_supply_with_text(self):
-        self.choose_new_supply('find buyers')
+        self.choose_new_supply('buyers')
 
     def choose_new_demand(self, input):
         self.receive_reply_assert(
@@ -52,15 +52,15 @@ class DiscussWSellerDiscussThankYouTest(MessageHandlerTest):
             messages.DEMAND__GET_PRODUCT
         )
         self.assert_value(
-            datas.MENU__MENU__OPTION__CHOICE,
-            value_string=datas.MENU__MENU__OPTION__FIND_SELLER
+            datas.MENU,
+            value_string=datas.MENU__FIND_SELLERS
         )
 
     def test_choose_new_demand_with_number(self):
         self.choose_new_demand('2')
 
     def test_choose_new_demand_with_text(self):
-        self.choose_new_demand('find sellers')
+        self.choose_new_demand('sellers')
 
     def test_learn_more(self):
         self.receive_reply_assert(
@@ -69,6 +69,6 @@ class DiscussWSellerDiscussThankYouTest(MessageHandlerTest):
             messages.EXPLAIN_SERVICE
         )
         self.assert_value(
-            datas.MENU__MENU__OPTION__CHOICE,
-            value_string=datas.MENU__MENU__OPTION__LEARN_MORE
+            datas.MENU,
+            value_string=datas.MENU__LEARN_MORE
         )
