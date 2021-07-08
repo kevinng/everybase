ea5d3b94153864f1e74fababa6b4244d3b339cdd
Kevin Ng <kevinngsw@gmail.com>
2021-07-08 18:42:41 +0800

    Fix get_chatbot return type

--- chat/libraries/utilities/get_chatbot.py
+++ chat/libraries/utilities/get_chatbot.py
@@ -1,6 +1,6 @@
 from everybase import settings
 from relationships import models as relmods
 
-def get_chatbot() -> relmods.PhoneNumber:
+def get_chatbot() -> relmods.User:
     """Returns system chatbot user"""
     return relmods.User.objects.get(pk=settings.CHATBOT_PHONE_NUMBER_PK)
