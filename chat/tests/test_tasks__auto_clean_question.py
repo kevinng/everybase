8dc58eb613982ba85b9744d5c97d3f45c0e15dfe
Kevin Ng <kevinngsw@gmail.com>
2021-07-10 19:47:19 +0800

    Add auto clean qns test

---
+++ chat/tests/test_tasks__auto_clean_question.py
@@ -0,0 +1,33 @@
+from chat.libraries.classes.message_handler_test import MessageHandlerTest
+from chat.libraries.test_funcs.supply_availability_options import \
+    SupplyAvailabilityOption
+from chat.tasks.auto_clean_question import auto_clean_question
+
+class TasksAutoCleanQuestionTest(MessageHandlerTest):
+    fixtures = [
+        'setup/common__country.json',
+        'setup/20210528__payments__currency.json',
+        'setup/20210527__relationships__availability.json'
+    ]
+
+    def test_run(self):
+        self.setup_match(True, SupplyAvailabilityOption.OTG)
+        qna = self.setup_qna(
+            answered=True,
+            question_captured=\
+                'My email is kevin@everybase.co and friend@everybase.co'
+        )
+        
+        # Run task
+        auto_clean_question(qna, True)
+
+        # Assert
+        self.assertEqual(
+            qna.auto_cleaned_question,
+            'My email is * and *'
+        )
+        self.assertEqual(
+            qna.auto_cleaned_question_w_mark_up,
+            'My email is <email>kevin@everybase.co</email>\
+ and <email>friend@everybase.co</email>'
+        )
