b4df8b1cb2998f6f1d119c94826cdc3864bea6bc
Kevin Ng <kevinngsw@gmail.com>
2021-07-10 19:54:29 +0800

    Fix send_message to deal with no-external-call

--- chat/libraries/utility_funcs/send_message.py
+++ chat/libraries/utility_funcs/send_message.py
@@ -14,7 +14,7 @@ def send_message(
         message_key: str = None,
         media_url: List[str] = None,
         no_external_calls: bool = False
-    ):
+    ) -> models.TwilioOutboundMessage:
     """Send Twilio WhatsApp message
 
     Parameters
@@ -37,6 +37,7 @@ def send_message(
         made correctly.
     """
     # Send message
+    message = None
     if no_external_calls == False:
         client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
         message = client.messages.create(
@@ -52,18 +53,18 @@ def send_message(
         intent_key=intent_key,
         message_key=message_key,
 
-        date_created=message.date_created,
-        date_sent=message.date_sent,
-        direction=message.direction,
-        account_sid=message.account_sid,
-        message_sid=message.sid,
-        from_str=message.from_,
-        to_str=message.to,
-        body=message.body,
-        uri=message.uri,
-        error_message=message.error_message,
-        error_code=message.error_code,
-        api_version=message.api_version,
+        date_created=message.date_created if message is not None else None,
+        date_sent=message.date_sent if message is not None else None,
+        direction=message.direction if message is not None else None,
+        account_sid=message.account_sid if message is not None else None,
+        message_sid=message.sid if message is not None else None,
+        from_str=message.from_ if message is not None else None,
+        to_str=message.to if message is not None else None,
+        body=message.body if message is not None else body, # Use body
+        uri=message.uri if message is not None else None,
+        error_message=message.error_message if message is not None else None,
+        error_code=message.error_code if message is not None else None,
+        api_version=message.api_version if message is not None else None,
 
         from_user=from_ph.user,
         to_user=to_ph.user,
