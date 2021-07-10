c4b1c3a4fa4e78ac2d0279268a00125ac43e411c
Kevin Ng <kevinngsw@gmail.com>
2021-07-10 19:38:48 +0800

    Add remove email task

--- chat/libraries/cleaning_funcs/remove_emails.py
+++ chat/libraries/cleaning_funcs/remove_emails.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 from chat.libraries.constants import tags
 from chat.libraries.nlp_funcs.get_this_word import get_this_word
 from chat.libraries.nlp_funcs.mark_string import mark_string
@@ -6,7 +7,8 @@ from chat.libraries.nlp_funcs.mark_clean_text import mark_clean_text
 
 def remove_email(
         text: str,
-        replacement: str ='*'):
+        replacement: str = '*'
+    ) -> Tuple[str]:
     """Remove emails from text
 
     Returns
@@ -15,15 +17,15 @@ def remove_email(
 
     (cleaned_text, marked_text, positions)
     """
-
     # Find all email's start/end positions
     positions = []
     pos = 0
     while pos < len(text):
         pos = text.find('@', pos, len(text))
         if pos != -1:
-            word = get_this_word(text, pos)
-            positions += (pos, pos + len(word))
+            _, start_pos, end_pos = get_this_word(text, pos)
+            positions.append((start_pos, end_pos))
+            pos = end_pos
         else:
             break
 
