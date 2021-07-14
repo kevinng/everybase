80a36b5e63017ff27cd70cf9c6304a839b174442
Kevin Ng <kevinngsw@gmail.com>
2021-07-14 17:20:22 +0800

    Update chat URL

--- chat/urls.py
+++ chat/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path(f'{webhook_root}/message/',
         views.TwilioIncomingMessageView.as_view()),
     # path(f'{webhook_root}/status/',
-        # ),        
-    path(f'{webhook_root}/send_confirm_interests/',
+        # ?),
+    path(f'send_confirm_interests/',
         views.SendConfirmInterestsView.as_view())
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
