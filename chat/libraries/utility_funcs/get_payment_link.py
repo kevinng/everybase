7dfd706bf54692a597394af33d089ad383fa089f
Kevin Ng <kevinngsw@gmail.com>
2021-07-15 04:47:52 +0800

    Use reverse in get_payment_link

--- chat/libraries/utility_funcs/get_payment_link.py
+++ chat/libraries/utility_funcs/get_payment_link.py
@@ -1,5 +1,7 @@
-import urllib.parse
+from urllib.parse import urljoin
+from django.urls import reverse
 from everybase.settings import BASE_URL
 
 def get_payment_link(hash):
-    return urllib.parse.urljoin(BASE_URL, str(hash.id))
+    return urljoin(BASE_URL,
+        reverse('chat_root:payment', kwargs={ 'id': hash.id }))
