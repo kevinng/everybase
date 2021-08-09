7bee75632d75fb2b98774e71372bf3f3f83d4cac
Kevin Ng <kevinngsw@gmail.com>
2021-08-09 14:09:17 +0800

    Update F2F load data scripts
    - Update timestamps
    - Add print message if new entry created

--- scripts/load_data__fibre2fashion_buying_offer_idempotent.py
+++ scripts/load_data__fibre2fashion_buying_offer_idempotent.py
@@ -1,12 +1,10 @@
-# Updated: 25 Nov 2020
+# Updated: 9 Aug 21
 
 import pytz
 from datetime import datetime
 from scripts.shared import helpers
 from growth.models import Fibre2FashionBuyingOffer
 from relationships.shared import record_email
-from django.core.exceptions import ValidationError
-import traceback
 
 _NAMESPACE = 'fibre2fashion_buying_offer'
 
@@ -17,35 +15,34 @@ def parse_row(row, import_job):
 
     # Record this email
     (email, invalid_email) = record_email(email_str, import_job)
+
+    source_url = helpers.clean_string(row.get('url', None))
     
-    offer, created = Fibre2FashionBuyingOffer.objects.get_or_create(
-        source_url=helpers.clean_string(row.get('url', None)),
-        defaults={
-            'harvested': datetime(2020, 11, 24, tzinfo=sgtz) # Set to right date/time
-        }
-    )
-
-    # Do not override the import_job for updates. We'd know if an entry has
-    # been updated if its updated and created timestamps are different.
-    if created == True:
-        offer.import_job = import_job
-
-    offer.category = helpers.clean_string(row.get('cat', None))
-    offer.sub_category = helpers.clean_string(row.get('subcat', None))
-    offer.title = helpers.clean_string(row.get('title', None))
-    offer.reference_no = helpers.clean_string(row.get('ref_no', None))
-    offer.description = helpers.clean_string(row.get('coy_des', None))
-    offer.email_str = email_str
-    offer.company_name = helpers.clean_string(row.get('coy_name', None))
-    offer.product_info_html = helpers.clean_string(row.get('prod_info_html', None))
-    offer.email = email
-    offer.invalid_email = invalid_email
-
+    created = False
     try:
-        offer.full_clean()
+        offer = Fibre2FashionBuyingOffer.objects.get(source_url=source_url)
+    except Fibre2FashionBuyingOffer.DoesNotExist:
+        offer = Fibre2FashionBuyingOffer.objects.create(
+            source_url=source_url,
+            import_job=import_job,
+            harvested = datetime(2021, 8, 9, tzinfo=sgtz) # Set to right date/time
+        )
+        created = True
+
+    # Only add details if new
+    if created:
+        offer.category = helpers.clean_string(row.get('cat', None))
+        offer.sub_category = helpers.clean_string(row.get('subcat', None))
+        offer.title = helpers.clean_string(row.get('title', None))
+        offer.reference_no = helpers.clean_string(row.get('ref_no', None))
+        offer.description = helpers.clean_string(row.get('coy_des', None))
+        offer.email_str = email_str
+        offer.company_name = helpers.clean_string(row.get('coy_name', None))
+        offer.product_info_html = helpers.clean_string(row.get('prod_info_html', None))
+        offer.email = email
+        offer.invalid_email = invalid_email
         offer.save()
-    except ValidationError:
-        traceback.print_exc()
+        print('Created %d' % offer.id)
     
 def run():
     helpers.load(parse_row, _NAMESPACE)
--- scripts/load_data__fibre2fashion_selling_offer_idempotent.py
+++ scripts/load_data__fibre2fashion_selling_offer_idempotent.py
@@ -16,36 +16,35 @@ def parse_row(row, import_job):
 
     # Record this email
     (email, invalid_email) = record_email(email_str, import_job)
-    
-    offer, created = Fibre2FashionSellingOffer.objects.get_or_create(
-        source_url=helpers.clean_string(row.get('url', None)),
-        defaults={
-            'harvested': datetime(2020, 2, 29, tzinfo=sgtz) # Set to right date/time
-        }
-    )
 
-    # Do not override the import_job for updates. We'd know if an entry has
-    # been updated if its updated and created timestamps are different.
-    if created == True:
-        offer.import_job = import_job
+    source_url = helpers.clean_string(row.get('url', None))
 
-    offer.category = helpers.clean_string(row.get('cat', None))
-    offer.sub_category = helpers.clean_string(row.get('subcat', None))
-    offer.title = helpers.clean_string(row.get('title', None))
-    offer.reference_no = helpers.clean_string(row.get('ref_no', None))
-    offer.description = helpers.clean_string(row.get('coy_des', None))
-    offer.email_str = email_str
-    offer.company_name = helpers.clean_string(row.get('coy_name', None))
-    offer.company_address = helpers.clean_string(row.get('coy_addr', None))
-    offer.product_info_html = helpers.clean_string(row.get('prod_info_html', None))
-    offer.email = email
-    offer.invalid_email = invalid_email
-
+    created = False
     try:
-        offer.full_clean()
+        offer = Fibre2FashionSellingOffer.objects.get(source_url=source_url)
+    except Fibre2FashionSellingOffer.DoesNotExist:
+        offer = Fibre2FashionSellingOffer.objects.create(
+            source_url=source_url,
+            import_job=import_job,
+            harvested = datetime(2021, 8, 9, tzinfo=sgtz) # Set to right date/time
+        )
+        created = True
+
+    # Only add details if new
+    if created == True:
+        offer.category = helpers.clean_string(row.get('cat', None))
+        offer.sub_category = helpers.clean_string(row.get('subcat', None))
+        offer.title = helpers.clean_string(row.get('title', None))
+        offer.reference_no = helpers.clean_string(row.get('ref_no', None))
+        offer.description = helpers.clean_string(row.get('coy_des', None))
+        offer.email_str = email_str
+        offer.company_name = helpers.clean_string(row.get('coy_name', None))
+        offer.company_address = helpers.clean_string(row.get('coy_addr', None))
+        offer.product_info_html = helpers.clean_string(row.get('prod_info_html', None))
+        offer.email = email
+        offer.invalid_email = invalid_email
         offer.save()
-    except ValidationError:
-        traceback.print_exc()
+        print('Created %s' % offer.id)
     
 def run():
     helpers.load(parse_row, _NAMESPACE)
