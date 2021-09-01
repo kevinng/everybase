e41818f945c02671248e3899bdd87e9c3991c478
Kevin Ng <kevinngsw@gmail.com>
2021-09-01 23:17:06 +0800

    Find buyers/sellers get-lead/thank-you handlers/tests

---
+++ chat/tests/test_find_buyers___get_lead__thank_you.py
@@ -0,0 +1,21 @@
+from chat.libraries.constants import intents, messages
+from chat.libraries.classes.chat_test import ChatTest
+from chat.tests.test_menu___menu import MenuRegisteredTest, MenuUnregisteredTest
+
+class FIND_BUYERS___GET_LEAD__THANK_YOU___Unregistered___Test(
+    MenuUnregisteredTest, ChatTest):
+    def setUp(self):
+        super().setUp(
+            intents.FIND_BUYERS,
+            messages.GET_LEAD__THANK_YOU,
+            registered=False
+        )
+
+class FIND_BUYERS___GET_LEAD__THANK_YOU___Registered_Test(
+    MenuRegisteredTest, ChatTest):
+    def setUp(self):
+        super().setUp(
+            intents.FIND_BUYERS,
+            messages.GET_LEAD__THANK_YOU,
+            registered=True
+        )
