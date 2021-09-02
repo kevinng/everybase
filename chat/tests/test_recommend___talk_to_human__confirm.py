f0c2bbadc28161eb901de8e07a1d2a49306a3d19
Kevin Ng <kevinngsw@gmail.com>
2021-09-02 18:18:21 +0800

    Add recommend/talk-to-human-cfm handler/tests

---
+++ chat/tests/test_recommend___talk_to_human__confirm.py
@@ -0,0 +1,21 @@
+from chat.libraries.constants import intents, messages
+from chat.libraries.classes.chat_test import ChatTest
+from chat.tests.test_menu___menu import MenuUnregisteredTest, MenuRegisteredTest
+
+class RECOMMEND___TALK_TO_HUMAN__CONFIRM___Unregistered___Test(
+    MenuUnregisteredTest, ChatTest):
+    def setUp(self):
+        super().setUp(
+            intents.RECOMMEND,
+            messages.TALK_TO_HUMAN__CONFIRM,
+            registered=False
+        )
+
+class RECOMMEND___TALK_TO_HUMAN__CONFIRM___Registered___Test(
+    MenuRegisteredTest, ChatTest):
+    def setUp(self):
+        super().setUp(
+            intents.RECOMMEND,
+            messages.TALK_TO_HUMAN__CONFIRM,
+            registered=True
+        )
