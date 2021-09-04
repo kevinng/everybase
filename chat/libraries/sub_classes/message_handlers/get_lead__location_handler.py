2cd29e9cdbf098e6c2de5917612ab429a65ea40d
Kevin Ng <kevinngsw@gmail.com>
2021-09-04 13:14:06 +0800

    Save location in user.current_location

--- chat/libraries/sub_classes/message_handlers/get_lead__location_handler.py
+++ chat/libraries/sub_classes/message_handlers/get_lead__location_handler.py
@@ -1,10 +1,18 @@
 from amplitude.constants import events
-from chat.libraries.constants import datas, messages
+from chat.libraries.constants import messages
 from chat.libraries.classes.message_handler import MessageHandler
+from relationships.models import Lead, CAPTURE_METHOD_TYPE__MENU_OPTION
 
 class GetLeadLocationHandler(MessageHandler):
     def run(self, buying):
-        self.save_body_as_string(datas.LOCATION)
+        user = self.message.from_user
+        lead = Lead.objects.create(
+            owner=user,
+            capture_method_type=CAPTURE_METHOD_TYPE__MENU_OPTION,
+            location=self.message.body.strip() # Store location
+        )
+        user.current_lead = lead
+        user.save()
         self.send_event(events.ENTERED_FREE_TEXT)
         return self.done_reply(
             self.intent_key,
--- chat/tests/test_find_buyers___get_lead__location.py
+++ chat/tests/test_find_buyers___get_lead__location.py
@@ -1,18 +1,17 @@
-from chat.libraries.constants import intents, messages, datas
+from django.db import transaction
+from chat.libraries.constants import intents, messages
 from chat.libraries.classes.chat_test import ChatTest
 
 class FIND_BUYERS___GET_LEAD__LOCATION___Test(ChatTest):
     def setUp(self):
         super().setUp(intents.FIND_BUYERS, messages.GET_LEAD__LOCATION)
 
-    def test_enter_bad_email(self):
+    def test_enter_location(self):
         location = 'Singapore'
-        self.receive_reply_assert(
-            location,
-            intents.FIND_BUYERS,
-            messages.GET_LEAD__DETAILS
-        )
-        self.assert_value(
-            datas.LOCATION,
-            value_string=location
-        )
+        with transaction.atomic():
+            self.receive_reply_assert(
+                location,
+                intents.FIND_BUYERS,
+                messages.GET_LEAD__DETAILS
+            )
+        self.assertEqual(self.user.current_lead.location, location)
