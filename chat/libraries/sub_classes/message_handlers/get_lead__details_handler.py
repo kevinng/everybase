4181358c90508af0272c7694b3b7bd5858061128
Kevin Ng <kevinngsw@gmail.com>
2021-09-06 14:40:38 +0800

    Add ID to context admin display fields

--- chat/libraries/sub_classes/message_handlers/get_lead__details_handler.py
+++ chat/libraries/sub_classes/message_handlers/get_lead__details_handler.py
@@ -1,7 +1,8 @@
+from chat.tasks.save_lead_media import save_lead_media
 from chat.libraries.constants import messages
 from chat.libraries.classes.message_handler import MessageHandler
-from chat.tasks.save_lead_media import save_lead_media
 from chat.libraries.utility_funcs.render_message import render_message
+from chat.libraries.classes.context_logic import ContextLogic
 from relationships.models import LeadText
 
 class GetLeadDetailsHandler(MessageHandler):
@@ -11,9 +12,13 @@ class GetLeadDetailsHandler(MessageHandler):
             save_lead_media.delay(lead.id, m.content_type, m.url)
 
         if self.message.body.strip() == 'done':
+            c = ContextLogic(self)
             return self.done_reply(
                 self.intent_key,
-                messages.GET_LEAD__THANK_YOU
+                messages.GET_LEAD__THANK_YOU,
+                params_func=lambda : {
+                    'buying': c.is_buying()
+                }
             )
         else:
             LeadText.objects.create(
