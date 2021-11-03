087462f171184544254884aa5a428b6fd4919f7d
Kevin Ng <kevinngsw@gmail.com>
2021-11-03 17:12:48 +0800

    Update template mockups

--- common/urls.py
+++ common/urls.py
@@ -3,20 +3,20 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('country/',
-        views.CountryList.as_view()),
-    path('country/<int:pk>/',
-        views.CountryDetail.as_view()),
+    # path('country/',
+    #     views.CountryList.as_view()),
+    # path('country/<int:pk>/',
+    #     views.CountryDetail.as_view()),
 
-    path('state/',
-        views.StateList.as_view()),
-    path('state/<int:pk>/',
-        views.StateDetail.as_view()),
+    # path('state/',
+    #     views.StateList.as_view()),
+    # path('state/<int:pk>/',
+    #     views.StateDetail.as_view()),
 
-    path('import_job/',
-        views.ImportJobList.as_view()),
-    path('import_job/<int:pk>/',
-        views.ImportJobDetail.as_view()),
+    # path('import_job/',
+    #     views.ImportJobList.as_view()),
+    # path('import_job/<int:pk>/',
+    #     views.ImportJobDetail.as_view()),
 
     path('e/<str:file_to_render>', views.e),
     path('m/<str:file_to_render>', views.m),
