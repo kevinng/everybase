16d980984475a94de88ef03da3d2d4a08a2eae50
Kevin Ng <kevinngsw@gmail.com>
2021-11-03 17:12:29 +0800

    Update files app

--- files/urls.py
+++ files/urls.py
@@ -6,8 +6,9 @@ app_name = 'files'
 urlpatterns = [
     path('read/', views.ReadOnlyPresignedURLView.as_view()),
     path('write/', views.WriteOnlyPresignedURLView.as_view()),
-    path('file/', views.FileList.as_view()),
-    # path('file/<int:pk>', views.FileDetail.as_view())
     path('file/<int:id>', views.get_file, name='get_file')
+
+    # path('file/', views.FileList.as_view()),
+    # path('file/<int:pk>', views.FileDetail.as_view())
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
--- files/views.py
+++ files/views.py
@@ -9,10 +9,9 @@ from rest_framework import mixins, generics, permissions
 class ReadOnlyPresignedURLView(
     mixins.CreateModelMixin,
     generics.GenericAPIView):
-    # Note: following comment do not respect maximum line width so it shows up
-    # properly (i.e. without linebreaks) in the browser.
     """
-    Get AWS S3 object read-only pre-signed URLs for the specified file via a HTTP POST request.
+    Get AWS S3 object read-only pre-signed URLs for the specified file via a
+    HTTP POST request.
     """
     serializer_class = serializers.ReadOnlyPresignedURLSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -23,24 +22,15 @@ class ReadOnlyPresignedURLView(
 class WriteOnlyPresignedURLView(
     mixins.CreateModelMixin,
     generics.GenericAPIView):
-    # Note: following comment do not respect maximum line width so it shows up
-    # properly (i.e. without linebreaks) in the browser.
     """
-    Get AWS S3 object write-only pre-signed URLs for the specified file via a HTTP POST request.
+    Get AWS S3 object write-only pre-signed URLs for the specified file via a
+    HTTP POST request.
     """
     serializer_class = serializers.WriteOnlyPresignedURLSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
-
-class FileList(generics.ListCreateAPIView):
-    queryset = models.File.objects.all()
-    serializer_class = serializers.FileSerializer
-
-class FileDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = models.File.objects.all()
-    serializer_class = serializers.FileSerializer
 
 def get_file(_, id):
     """Redirect user to file in S3 with a pre-signed URL"""
@@ -62,3 +52,11 @@ def get_file(_, id):
     )
 
     return response
+
+class FileList(generics.ListCreateAPIView):
+    queryset = models.File.objects.all()
+    serializer_class = serializers.FileSerializer
+
+class FileDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.File.objects.all()
+    serializer_class = serializers.FileSerializer
