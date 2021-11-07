9e08d46d80be3e14906b1f4f7007c96eae1176cb
Kevin Ng <kevinngsw@gmail.com>
2021-11-07 12:25:11 +0800

    Update admin for Lead

--- leads/admin.py
+++ leads/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from common import admin as comadm
 from leads import models
 
-_lead_fields = ['author', 'is_buying', 'author_type', 'title', 'details',
+_lead_fields = ['author', 'lead_type', 'author_type', 'title', 'details',
     'country_string', 'country', 'commission_pct', 'commission_payable_after',
     'other_commission_details']
 @admin.register(models.Lead)
@@ -10,7 +10,7 @@ class LeadAdmin(comadm.StandardAdmin):
     # List page settings
     list_display = comadm.standard_list_display + _lead_fields
     list_editable = comadm.standard_list_editable + _lead_fields
-    list_filter = comadm.standard_list_filter + ['is_buying', 'author_type',
+    list_filter = comadm.standard_list_filter + ['lead_type', 'author_type',
         'country', 'commission_payable_after']
     search_fields = comadm.standard_search_fields + ['title', 'details',
         'other_commission_details']
