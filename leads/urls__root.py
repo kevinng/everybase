37863e2db7fc29b12ddbb6b4225e2d29584f030e
Kevin Ng <kevinngsw@gmail.com>
2022-01-29 02:09:00 +0800

    Add I-Need-Agent routing

--- leads/urls__root.py
+++ leads/urls__root.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'leads__root'
 urlpatterns = [
     path('', views.AgentListView.as_view(), name='agents'),
-    path('i-need-agents', views.i_need_agents, name='i_need_agents')
+    path('i-need-agents', views.INeedAgentListView.as_view(), name='i_need_agents')
 ]
