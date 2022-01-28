5715c0bc3ab69ffce17e7ba825b83ad959bacd70
Kevin Ng <kevinngsw@gmail.com>
2022-01-28 14:21:55 +0800

    Clean up lead-root URLs

--- leads/urls__root.py
+++ leads/urls__root.py
@@ -3,16 +3,5 @@ from . import views
 
 app_name = 'leads__root'
 urlpatterns = [
-    # this should not be in use anymore TODO
-    # path('', views.LeadListView.as_view(), name='list'),
-
-
-    path('', views.AgentListView.as_view(), name='agents'),
-
-
-    # These should not be in use TODO
-    # path('contact_requests/<str:uuid>/', views.contact_request_detail,
-    #     name='contact_request_detail'),
-    # path('contact_requests/', views.ContactRequestListView.as_view(),
-    #     name='contact_request_list'),
+    path('', views.AgentListView.as_view(), name='agents')
 ]
