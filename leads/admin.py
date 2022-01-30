4074ded611f1730b8a6dc02f2998d6893cd3b51f
Kevin Ng <kevinngsw@gmail.com>
2022-01-31 01:04:12 +0800

    Add new lead admins

--- leads/admin.py
+++ leads/admin.py
@@ -85,18 +85,73 @@ class FilterFormPostAdmin(comadm.StandardAdmin):
         [('Details', {'fields': _filter_form_post_fields})]
     autocomplete_fields = ['user']
 
-_whatsapp_lead_author_click_admin = ['lead', 'contactor', 'count']
+_whatsapp_lead_author_click_fields = ['lead', 'contactor', 'count']
 @admin.register(models.WhatsAppLeadAuthorClick)
 class WhatsAppLeadAuthorClickAdmin(comadm.StandardAdmin):
     # List page settings
     list_display = comadm.standard_list_display + \
-        _whatsapp_lead_author_click_admin
+        _whatsapp_lead_author_click_fields
     list_editable = comadm.standard_list_editable + \
-        _whatsapp_lead_author_click_admin
+        _whatsapp_lead_author_click_fields
     search_fields = comadm.standard_search_fields + \
-        _whatsapp_lead_author_click_admin
+        _whatsapp_lead_author_click_fields
 
     # Details page settings
     fieldsets = comadm.standard_fieldsets + \
-        [('Details', {'fields': _whatsapp_lead_author_click_admin})]
+        [('Details', {'fields': _whatsapp_lead_author_click_fields})]
     autocomplete_fields = ['lead', 'contactor']
+
+_whatsapp_click_fields = ['contactee', 'contactor', 'source', 'count']
+@admin.register(models.WhatsAppClick)
+class WhatsAppClickAdmin(comadm.StandardAdmin):
+    # List page settings
+    list_display = comadm.standard_list_display + _whatsapp_click_fields
+    list_editable = comadm.standard_list_editable + _whatsapp_click_fields
+    search_fields = comadm.standard_search_fields + _whatsapp_click_fields
+
+    # Details page settings
+    fieldsets = comadm.standard_fieldsets + \
+        [('Details', {'fields': _whatsapp_click_fields})]
+    autocomplete_fields = ['contactee', 'contactor']
+
+_whatsapp_message_body_fields = ['contactee', 'contactor', 'body']
+@admin.register(models.WhatsAppMessageBody)
+class WhatsAppMessageBodyAdmin(comadm.StandardAdmin):
+    # List page settings
+    list_display = comadm.standard_list_display + _whatsapp_message_body_fields
+    list_editable = comadm.standard_list_editable + \
+        _whatsapp_message_body_fields
+    search_fields = comadm.standard_search_fields + \
+        _whatsapp_message_body_fields
+
+    # Details page settings
+    fieldsets = comadm.standard_fieldsets + \
+        [('Details', {'fields': _whatsapp_message_body_fields})]
+    autocomplete_fields = ['contactee', 'contactor']
+
+_agent_query_fields = ['user', 'search', 'country']
+@admin.register(models.AgentQuery)
+class AgentQueryAdmin(comadm.StandardAdmin):
+    # List page settings
+    list_display = comadm.standard_list_display + _agent_query_fields
+    list_editable = comadm.standard_list_editable + _agent_query_fields
+    search_fields = comadm.standard_search_fields + _agent_query_fields
+
+    # Details page settings
+    fieldsets = comadm.standard_fieldsets + \
+        [('Details', {'fields': _agent_query_fields})]
+    autocomplete_fields = ['user', 'country']
+
+_i_need_agent_query_fields = ['user', 'search', 'wants_to', 'buy_country',
+    'sell_country']
+@admin.register(models.INeedAgentQuery)
+class INeedAgentQueryAdmin(comadm.StandardAdmin):
+    # List page settings
+    list_display = comadm.standard_list_display + _i_need_agent_query_fields
+    list_editable = comadm.standard_list_editable + _i_need_agent_query_fields
+    search_fields = comadm.standard_search_fields + _i_need_agent_query_fields
+
+    # Details page settings
+    fieldsets = comadm.standard_fieldsets + \
+        [('Details', {'fields': _i_need_agent_query_fields})]
+    autocomplete_fields = ['user', 'buy_country', 'sell_country']
