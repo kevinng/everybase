3931cb628852ac9fcd33dd5dd17169b6f140ad34
Kevin Ng <kevinngsw@gmail.com>
2022-02-03 14:51:09 +0800

    Remove URL comments

--- leads/urls__root.py
+++ leads/urls__root.py
@@ -8,9 +8,4 @@ urlpatterns = [
     path('leads/create', views.lead_create, name='lead_create'),
     path('leads/edit/<int:pk>', views.LeadEdit.as_view(), name='lead_edit'),
     path('leads/<int:pk>', views.LeadDetail.as_view(), name='lead_detail'),
-
-    # path('u/<int:pk>/i-need-agents')
-
-
-    # path('i-need-agents/author/<int:user_pk>')
 ]
