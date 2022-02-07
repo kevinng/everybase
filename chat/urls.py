583e71a50abb27536be45f07ed640865adf7efda
Kevin Ng <kevinngsw@gmail.com>
2022-02-07 15:18:45 +0800

    Remove unused commented URLs in chat

--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 app_name = 'chat'
@@ -11,7 +10,4 @@ urlpatterns = [
         name='status_update_message'),
     path(f'{webhook_root}/status/',
         views.TwilioIncomingStatusView.as_view(), name='status_update'),
-    # path(f'{webhook_root}/fulfill/',
-    #     views.StripeFulfilmentCallbackView.as_view()),
-    # path(f'send_confirm_interests/', views.SendConfirmInterestsView.as_view())
 ]
