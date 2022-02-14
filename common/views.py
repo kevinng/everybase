5116e06191719a464b78f30f2a01826084071163
Kevin Ng <kevinngsw@gmail.com>
2022-02-14 22:44:31 +0800

    Home page
    - Home page
    - Unit tests

--- common/views.py
+++ common/views.py
@@ -58,3 +58,6 @@ def e(request, file_to_render):
 def m(request, file_to_render):
     template_name = 'mock_ups/%s' % file_to_render
     return TemplateResponse(request, template_name, {})
+
+def home(request):
+    return TemplateResponse(request, 'home.html', {})
