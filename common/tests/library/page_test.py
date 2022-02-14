9251a060750f0ef8398a2c31c9f31950be2af187
Kevin Ng <kevinngsw@gmail.com>
2022-02-14 23:19:39 +0800

    Unit test - home page logged-in

--- common/tests/library/page_test.py
+++ common/tests/library/page_test.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test import Client
 from common.libraries.tear_down import tear_down
+from relationships import models as relmods
 
 class PageTest(TestCase):
     """Base class for client-access page automated test cases."""
@@ -16,7 +17,8 @@ class PageTest(TestCase):
     def setUp(
             self,
             url : str,
-            method : str
+            method : str,
+            logged_in : bool = False
         ):
         """TestCase setUp method with additonal parameters for overriding.
 
@@ -28,10 +30,16 @@ class PageTest(TestCase):
             'get' if HTTP GET, 'post' if HTTP POST
         """
         c = Client()
+
+        if logged_in:
+            self.user = relmods.User.objects.get(pk=3)
+            c.force_login(self.user.django_user)
+
         if method == 'get':
             self.response = c.get(url)
         elif method == 'post':
             self.response = c.post(url)
+        
         super().setUp()
 
     def tearDown(self):
--- relationships/tests/test_home_page.py
+++ relationships/tests/test_home_page.py
@@ -41,11 +41,11 @@ class HomePageNotLoggedInTest(PageTest):
     def test_nav_bar_desktop_i_need_agents_link_exists(self) -> None:
         self.assert_leads_lead_list('nav_bar_desktop_leads_link')
 
-    def test_nav_bar_desktop_register_link_exists(self) -> None:
-        self.assert_register('nav_bar_desktop_register_link')
-
     def test_nav_bar_desktop_login_link_exists(self) -> None:
         self.assert_login('nav_bar_desktop_login_link')
+
+    def test_nav_bar_desktop_register_link_exists(self) -> None:
+        self.assert_register('nav_bar_desktop_register_link')
 
     def test_nav_bar_mobile_agents_link_exists(self) -> None:
         self.assert_users_agent_list('nav_bar_mobile_agents_link')
@@ -53,52 +53,66 @@ class HomePageNotLoggedInTest(PageTest):
     def test_nav_bar_mobile_i_need_agents_link_exists(self) -> None:
         self.assert_leads_lead_list('nav_bar_mobile_leads_link')
 
-    def test_nav_bar_mobile_register_link_exists(self) -> None:
-        self.assert_register('nav_bar_mobile_register_link')
-
     def test_nav_bar_mobile_login_link_exists(self) -> None:
         self.assert_login('nav_bar_mobile_login_link')
 
-# class HomePageLoggedInTest(TestCase):
-#     def setUp(self) -> None:
-#         c = Client()
-#         self.response = c.get('/')
-#         return super().setUp()
+    def test_nav_bar_mobile_register_link_exists(self) -> None:
+        self.assert_register('nav_bar_mobile_register_link')
 
-    # def assert_logout(self, tag_id):
-    #     url = reverse('logout')
-    #     self.assert_link(tag_id, url)
+class HomePageLoggedInTest(PageTest):
+    def setUp(self) -> None:
+        return super().setUp('/', 'get', logged_in=True)
 
-    # def assert_my_profile(self, tag_id):
-    #     url = reverse('users:user_comments')
-    #     self.assert_link(tag_id, url)
+    def assert_link(self, tag_id, url):
+        content = f'<a ut-tag-id="{tag_id}" href="{url}"'
+        self.assert_contains(content)
 
-#     def test_hero_agents_link_exists(self) -> None:
-#         pass
+    def assert_users_agent_list(self, tag_id):
+        url = reverse('users:agent_list')
+        self.assert_link(tag_id, url)
 
-#     def test_hero_i_need_agents_link_exists(self) -> None:
-#         pass
+    def assert_leads_lead_list(self, tag_id):
+        url = reverse('leads:lead_list')
+        self.assert_link(tag_id, url)
 
-#     def test_nav_bar_desktop_agents(self) -> None:
-#         pass
+    def assert_logout(self, tag_id):
+        url = reverse('logout')
+        self.assert_link(tag_id, url)
 
-#     def test_nav_bar_desktop_i_need_agents(self) -> None:
-#         pass
+    def assert_my_profile(self, tag_id):
+        url = reverse('users:user_comments', args=[self.user.id])
+        self.assert_link(tag_id, url)
 
-    # def test_nav_bar_desktop_logout_link_exists(self) -> None:
-    #     self.assert_logout('nav_bar_desktop_logout_link')
+    def test_logo_link_exists(self) -> None:
+        url = reverse('home')
+        self.assert_link('home_logo_link', url)
 
-    # def test_nav_bar_desktop_my_profile_link_exists(self) -> None:
-    #     self.assert_my_profile('nav_bar_desktop_my_profile_link')
+    def test_hero_agents_link_exists(self) -> None:
+        self.assert_users_agent_list('hero_agents_link')
+    
+    def test_hero_i_need_agents_link_exists(self) -> None:
+        self.assert_leads_lead_list('hero_leads_link')
 
-#     def test_nav_bar_mobile_agents(self) -> None:
-#         pass
+    def test_nav_bar_desktop_agents_link_exists(self) -> None:
+        self.assert_users_agent_list('nav_bar_desktop_agents_link')
 
-#     def test_nav_bar_mobile_i_need_agents(self) -> None:
-#         pass
+    def test_nav_bar_desktop_i_need_agents_link_exists(self) -> None:
+        self.assert_leads_lead_list('nav_bar_desktop_leads_link')
 
-    # def test_nav_bar_mobile_logout_link_exists(self) -> None:
-    #     self.assert_logout('nav_bar_mobile_logout_link')
+    def test_nav_bar_desktop_logout_link_exists(self) -> None:
+        self.assert_logout('nav_bar_desktop_logout_link')
 
-    # def test_nav_bar_mobile_my_profile_link_exists(self) -> None:
-    #     self.assert_my_profile('nav_bar_mobile_my_profile_link')
+    def test_nav_bar_desktop_my_profile_link_exists(self) -> None:
+        self.assert_my_profile('nav_bar_desktop_my_profile_link')
+
+    def test_nav_bar_mobile_agents_link_exists(self) -> None:
+        self.assert_users_agent_list('nav_bar_mobile_agents_link')
+
+    def test_nav_bar_mobile_i_need_agents_link_exists(self) -> None:
+        self.assert_leads_lead_list('nav_bar_mobile_leads_link')
+
+    def test_nav_bar_mobile_logout_link_exists(self) -> None:
+        self.assert_logout('nav_bar_mobile_logout_link')
+
+    def test_nav_bar_mobile_my_profile_link_exists(self) -> None:
+        self.assert_my_profile('nav_bar_mobile_my_profile_link')
