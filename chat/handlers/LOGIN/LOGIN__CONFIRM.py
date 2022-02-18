59f003fc1fd5621449968007c5f9d57b8835875f
Kevin Ng <kevinngsw@gmail.com>
2022-02-18 17:26:57 +0800

    Update register/login handlers to use tokens

--- chat/handlers/LOGIN/LOGIN__CONFIRM.py
+++ chat/handlers/LOGIN/LOGIN__CONFIRM.py
@@ -1,25 +1,29 @@
 import datetime
+from os import kill
 import pytz, datetime
 from everybase import settings
 from chat.constants import intents, messages
 from chat.utilities.render_message import render_message
 from chat.handlers.library import MessageHandler
 from relationships import models
+from relationships.utilities.kill_login_tokens import kill_login_tokens
 
 class Handler(MessageHandler):
     def run(self):
         if self.message.body.strip().lower() == 'yes':
-            # Only the latest token is used
+            # Use latest token
             token = models.LoginToken.objects.filter(
-                user=self.message.from_user.id,
-                activated__isnull=True # Token not activated
-            ).order_by('-created').first()
+                user=self.message.from_user.id).order_by('-created').first()
 
-            if token is not None:
+            if token is not None and token.activated is None:
+                # Activate token
                 sgtz = pytz.timezone(settings.TIME_ZONE)
                 now = datetime.datetime.now(tz=sgtz)
                 token.activated = now
                 token.save()
+
+                # Kill other tokens
+                kill_login_tokens(self.message.from_user, token.id)
 
                 return self.done_reply(
                     intents.LOGIN,
