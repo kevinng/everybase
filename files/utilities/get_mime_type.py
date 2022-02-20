0fe098ee3b24e7ff830ee4fa3dc7cf48d9b8834d
Kevin Ng <kevinngsw@gmail.com>
2022-02-21 01:03:48 +0800

    Add get_mime_type utility

---
+++ files/utilities/get_mime_type.py
@@ -0,0 +1,9 @@
+import magic
+
+def get_mime_type(file):
+    """Get MIME by reading the header of the file in memory"""
+    initial_pos = file.tell()
+    file.seek(0)
+    mime_type = magic.from_buffer(file.read(1024), mime=True)
+    file.seek(initial_pos)
+    return mime_type
