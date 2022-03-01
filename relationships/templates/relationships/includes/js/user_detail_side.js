a58f8339b7fc471c5d2f44c02ed8f61b0444ceec
Kevin Ng <kevinngsw@gmail.com>
2022-03-01 12:08:00 +0800

    user_detail_side - add JS include, update HTML

---
+++ relationships/templates/relationships/includes/js/user_detail_side.js
@@ -0,0 +1,31 @@
+{% comment %}
+Javascript
+https://codebeautify.org/minify-js
+
+$(document).ready(function() {
+    $toggle_save_user = $('#toggle_save_user')
+    $toggle_save_user.click(function () {
+        // Make AJAX call to save user, and update button look.
+        $.ajax({
+            type: 'POST',
+            headers: {'X-CSRFToken': '{{ csrf_token }}'},
+            url: "{% url 'users:toggle_save_user' lead.author.id %}",
+            dataType: 'json',
+            success: function (data) {
+                if (data['s'] == true) {
+                    $toggle_save_user
+                        .removeClass('text-slate-600 bg-slate-100 hover:bg-slate-200')
+                        .addClass('text-white bg-blue-500 hover:bg-blue-600')
+                        .text('Person Saved')
+                } else {
+                    $toggle_save_user
+                        .addClass('text-slate-600 bg-slate-100 hover:bg-slate-200')
+                        .removeClass('text-white bg-blue-500 hover:bg-blue-600')
+                        .text('Save Person')
+                }
+            }
+        })
+    })
+})
+{% endcomment %}
+$(document).ready((function(){$toggle_save_user=$("#toggle_save_user"),$toggle_save_user.click((function(){$.ajax({type:"POST",headers:{"X-CSRFToken":"{{ csrf_token }}"},url:"{% url 'users:toggle_save_user' lead.author.id %}",dataType:"json",success:function(e){1==e.s?$toggle_save_user.removeClass("text-slate-600 bg-slate-100 hover:bg-slate-200").addClass("text-white bg-blue-500 hover:bg-blue-600").text("Person Saved"):$toggle_save_user.addClass("text-slate-600 bg-slate-100 hover:bg-slate-200").removeClass("text-white bg-blue-500 hover:bg-blue-600").text("Save Person")}})}))}));
