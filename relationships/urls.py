2e2f864cdf6b59b5494be6d0f8ccf1680e3ccadc
Kevin Ng <kevinngsw@gmail.com>
2022-03-01 23:07:46 +0800

    Lead detail image gallery

--- relationships/urls.py
+++ relationships/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('<int:pk>/comments', views.user_comments, name='user_comments'),
     path('<int:pk>/leads', views.UserLeadListView.as_view(), name='user_leads'),
     path('<int:pk>/whatsapp', views.whatsapp, name='whatsapp'),
-    path('<int:pk>/edit', views.user_edit, name='user_edit')
+    path('<int:pk>/edit', views.user_edit, name='user_edit'),
+    path('<int:pk>/save', views.toggle_save_user, name='toggle_save_user')
 ]
