5461dd39b266b1d7462f0bab40e1a102dc51082d
Kevin Ng <kevinngsw@gmail.com>
2022-03-01 12:03:42 +0800

    Add has_saved_user template tag

---
+++ relationships/templatetags/has_saved_user.py
@@ -0,0 +1,16 @@
+from relationships import models
+from django import template
+
+register = template.Library()
+
+@register.simple_tag(name='has_saved_user')
+def has_saved_user(saver, savee):
+    try:
+        saved_user = models.SavedUser.objects.get(
+            saver=saver,
+            savee=savee
+        )
+    except models.SavedUser.DoesNotExist:
+        return False
+
+    return saved_user.active
