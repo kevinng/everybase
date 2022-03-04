d080a5e0b2f5cf89ac74cbf93136b1172b8b450b
Kevin Ng <kevinngsw@gmail.com>
2022-03-04 14:54:36 +0800

    Add is_censored utility; update growing_text_areas
    is_censored returns True if text may be posted
    
    update growing_text_areas to work on page load

--- common/static/common/js/growing_text_areas.js
+++ common/static/common/js/growing_text_areas.js
@@ -1,9 +1,21 @@
 // Dependencies
 //  JQuery
 $(document).ready(function() {
-    $(".growing").keyup(function(e) {
-        while($(this).outerHeight() < this.scrollHeight + parseFloat($(this).css("borderTopWidth")) + parseFloat($(this).css("borderBottomWidth"))) {
-            $(this).height($(this).height()+1)
+    // Run on load
+    $('.growing').each(function() {
+        while ($(this).outerHeight() < this.scrollHeight +
+            parseFloat($(this).css("borderTopWidth")) +
+            parseFloat($(this).css("borderBottomWidth"))) {
+            $(this).height($(this).height() + 1)
+        }
+    })
+
+    // Run on key up
+    $('.growing').keyup(function() {
+        while ($(this).outerHeight() < this.scrollHeight +
+            parseFloat($(this).css("borderTopWidth")) +
+            parseFloat($(this).css("borderBottomWidth"))) {
+            $(this).height($(this).height() + 1)
         }
     })
 })
