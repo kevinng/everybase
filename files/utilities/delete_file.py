dfe9e1a3d099c90fcbc4bb40ccb3015caa25b4eb
Kevin Ng <kevinngsw@gmail.com>
2022-03-08 21:06:03 +0800

    Rename to files.utilities.delete_file

--- files/utilities/delete_file.py
+++ files/utilities/delete_file.py
@@ -2,7 +2,7 @@ import datetime, pytz, boto3
 from files import models as fimods
 from everybase import settings
 
-def delete_cached_image(file_id):
+def delete_file(file_id):
     file = fimods.File.objects.get(pk=file_id)
 
     # Mark file as deleted
@@ -16,4 +16,13 @@ def delete_cached_image(file_id):
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY
     ).resource('s3')
 
+    # Delete file
     s3.Object(file.s3_bucket_name, file.s3_object_key).delete()
+
+    # Delete thumbnail if any
+    if (file.thumbnail_s3_bucket_name is not None and \
+        file.thumbnail_s3_object_key):
+        s3.Object(
+            file.thumbnail_s3_bucket_name,
+            file.thumbnail_s3_object_key)\
+            .delete()
