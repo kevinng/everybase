0f2ef89b85bff8458139e24dee6b7aad174ffe5b
Kevin Ng <kevinngsw@gmail.com>
2022-03-10 23:15:48 +0800

    Change user routes to use slugs

--- relationships/urls.py
+++ relationships/urls.py
@@ -5,21 +5,9 @@ from leads import views as leviews
 app_name = 'users'
 urlpatterns = [
     path('agents', leviews.AgentListView.as_view(), name='agent_list'),
-    path('<slug:slug>', views.user_comments, name='user_detail'),
+    path('<slug:slug>', views.user_detail, name='user_detail'),
     path('<slug:slug>/leads', views.UserLeadListView.as_view(), name='user_leads'),
     path('<slug:slug>/whatsapp', views.whatsapp, name='whatsapp'),
     path('<slug:slug>/edit', views.user_edit, name='user_edit'),
     path('<slug:slug>/save', views.toggle_save_user, name='toggle_save_user')
-
-    
-
-    # Not needed
-    # path('<int:pk>/comments', views.user_comments, name='user_comments'),
-
-    # Convert to slug
-    # path('<int:pk>', views.user_comments, name='user_detail'),
-    # path('<int:pk>/leads', views.UserLeadListView.as_view(), name='user_leads'),
-    # path('<int:pk>/whatsapp', views.whatsapp, name='whatsapp'),
-    # path('<int:pk>/edit', views.user_edit, name='user_edit'),
-    # path('<int:pk>/save', views.toggle_save_user, name='toggle_save_user')
 ]
