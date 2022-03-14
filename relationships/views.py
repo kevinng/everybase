c7a8e6fe88794dc179a0ea7f2988c8bd7a9ccc55
Kevin Ng <kevinngsw@gmail.com>
2022-03-14 15:04:20 +0800

    Add example user list code

--- relationships/views.py
+++ relationships/views.py
@@ -575,3 +575,61 @@ def toggle_save_user(request, slug):
     else:
         return HttpResponseRedirect(
             reverse('users:user_detail', args=(slug,)))
+
+# class AgentListView(ListView):
+#     template_name = 'leads/agent_list.html'
+#     context_object_name = 'agents'
+#     model = relmods.User
+#     paginate_by = 8
+
+#     def get_queryset(self, **kwargs):
+#         # search = self.request.GET.get('search')
+#         # country = self.request.GET.get('country')
+
+#         # users = relmods.User.objects
+#         # if country is not None and country != 'any_country':
+#         #     users = users.filter(country__programmatic_key=country)
+
+#         # # Save query
+#         # try:
+#         #     if self.request.user.is_authenticated:
+#         #         user = self.request.user.user
+#         #     else:
+#         #         user = None
+
+#         #     if country == 'any_country' or country is None:
+#         #         country_model = None
+#         #     else:
+#         #         country_model = commods.Country.objects.get(
+#         #             programmatic_key=country)
+                
+#         #     if user is not None and search is not None and country is not None:
+#         #         models.AgentQuery.objects.create(
+#         #             user=user,
+#         #             search=search,
+#         #             country=country_model
+#         #         )
+#         # except:
+#         #     traceback.print_exc()
+
+#         # vector = SearchVector('search_agents_veccol')
+#         # query = SearchQuery(search)
+#         # users = users.annotate(
+#         #     search_agents_veccol=RawSQL('search_agents_veccol', [],
+#         #         output_field=SearchVectorField()))\
+#         #     .annotate(rank=SearchRank(vector, query))\
+#         #     .order_by('-rank')
+            
+#         # return users
+
+#         return relmods.User.objects.all()
+        
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['countries'] = get_countries()
+
+#         # Render search and country back into the template
+#         context['search_value'] = self.request.GET.get('search')
+#         context['country_value'] = self.request.GET.get('country')
+
+#         return context
