0ee1d8e1cb7d3f85322185b88b18ed49f9b9f475
Kevin Ng <kevinngsw@gmail.com>
2022-03-14 15:30:41 +0800

    Remove agent_list routing

--- leads/urls.py
+++ leads/urls.py
@@ -3,14 +3,9 @@ from leads import views
 
 app_name = 'leads'
 urlpatterns = [
-    path('', views.LeadListView.as_view(), name='lead_list'),
-    # path('<int:pk>', views.lead_detail, name='lead_detail'),
-    
-    # path('edit/<int:pk>', views.lead_edit, name='lead_edit'),
-    # path('<int:pk>/save', views.toggle_save_lead, name='toggle_save_lead'),
-
     # Don't map slug URLs above generic links like lead_create. Otherwise,
     # 'leads/create' will go to a lead detail page with slug 'create'.
+    path('', views.lead_list, name='lead_list'),
     path('create', views.lead_create, name='lead_create'),
     path('edit/<slug:slug>', views.lead_edit, name='lead_edit'),
     path('<slug:slug>/save', views.toggle_save_lead, name='toggle_save_lead'),
