41d2a7f3aba2e4b2a32730d7f26fd863a0536f27
Kevin Ng <kevinngsw@gmail.com>
2022-04-12 20:15:10 +0800

    Add lead details tab if author is browsing

--- leads/urls.py
+++ leads/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', views.lead_list, name='lead_list'),
     path('create', views.lead_create, name='lead_create'),
     path('<slug:slug>', views.lead_detail, name='lead_detail'),
+    path('<slug:slug>/applications', views.lead_detail, name='lead_applications'),
     path('<slug:slug>/edit', views.lead_edit, name='lead_edit'),
     path('<slug:slug>/a/<int:aid>', views.application_detail, name='application_detail'),
 ]
