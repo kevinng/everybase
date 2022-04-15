e2c46b1ec2ca22018134f153a2de805944a9fe9b
Kevin Ng <kevinngsw@gmail.com>
2022-04-15 16:35:10 +0800

    Update chat constants

--- chat/constants/intents.py
+++ chat/constants/intents.py
@@ -3,11 +3,13 @@
 NO_INTENT = 'NO_INTENT' # User has no intent
 REGISTER = 'REGISTER'
 LOGIN = 'LOGIN'
+LEAD = 'LEAD'
 
 # Choices for model fields
 # Note: remember to makemigrations when updating this list
 choices = [
     (NO_INTENT, NO_INTENT),
     (REGISTER, REGISTER),
-    (LOGIN, LOGIN)
+    (LOGIN, LOGIN),
+    (LEAD, LEAD)
 ]
