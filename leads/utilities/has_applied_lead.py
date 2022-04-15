ed7d35f9a94257ed89ebc782a83376a90d454a42
Kevin Ng <kevinngsw@gmail.com>
2022-04-15 17:37:17 +0800

    Fix bug in has_applied_lead that led to crash

--- leads/utilities/has_applied_lead.py
+++ leads/utilities/has_applied_lead.py
@@ -1,6 +1,9 @@
 from leads import models as lemods
 
 def has_applied_lead(user, lead):
+    if user is None or user == '' or lead is None or lead == '':
+        return False
+
     return lemods.Application.objects.filter(
         lead=lead,
         applicant=user
