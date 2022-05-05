d2dda3023025a1cb7150a1708534b735a6aef916
Kevin Ng <kevin@everybase.co>
2022-05-05 16:46:28 +0800

    Tidy relationships forms

--- relationships/forms.py
+++ relationships/forms.py
@@ -1,217 +1,10 @@
-from urllib import request
+import phonenumbers
+from phonenumber_field.formfields import PhoneNumberField
+
 from django import forms
 from django.core.exceptions import ValidationError
-from common.utilities.is_censored import is_censored
-from . import models
 
-from phonenumber_field.formfields import PhoneNumberField
-import phonenumbers
-
-_require_msg = 'This field is required.'
-_censor_msg = 'Don\'t share contact details here. You may share contact details later.'
-
-class UserEditForm(forms.Form):
-    first_name = forms.CharField(
-        required=True,
-        min_length=1,
-        max_length=20
-    )
-    last_name = forms.CharField(
-        required=True,
-        min_length=1,
-        max_length=20
-    )
-
-    has_company = forms.BooleanField(required=False)
-
-    # Validated only if has_company is checked.
-    company_name = forms.CharField(
-        required=False,
-        max_length=50
-    )
-
-    goods_string = forms.CharField(
-        required=True,
-        min_length=1,
-        max_length=200
-    )
-
-    # No validations, for survey only.
-    is_buyer = forms.BooleanField(required=False)
-    is_seller = forms.BooleanField(required=False)
-    is_buy_agent = forms.BooleanField(required=False)
-    is_sell_agent = forms.BooleanField(required=False)
-
-    languages_string = forms.CharField(
-        required=True,
-        min_length=1,
-        max_length=200
-    )
-
-    # Next URL after registration
-    next = forms.CharField(required=False)
-
-    def clean(self):
-        super(UserEditForm, self).clean()
-
-        has_error = False
-
-        if self.cleaned_data.get('has_company'):
-            company_name = self.cleaned_data.get('company_name')
-            if company_name is None or company_name.strip() == '':
-                self.add_error('company_name', _require_msg)
-                has_error = True
-
-        if has_error:
-            raise ValidationError(None)
-
-        return self.cleaned_data
-
-class RegisterForm(forms.Form):
-    whatsapp_phone_number = PhoneNumberField(required=True)
-    
-    first_name = forms.CharField(
-        required=True,
-        min_length=1,
-        max_length=20
-    )
-    last_name = forms.CharField(
-        required=True,
-        min_length=1,
-        max_length=20
-    )
-
-    has_company = forms.BooleanField(required=False)
-
-    # Validated only if has_company is checked.
-    company_name = forms.CharField(required=False)
-    
-    email = forms.EmailField(
-        required=True
-    )
-
-    goods_string = forms.CharField(
-        required=True,
-        min_length=1,
-        max_length=200
-    )
-
-    # No validations, for survey only.
-    is_buyer = forms.BooleanField(required=False)
-    is_seller = forms.BooleanField(required=False)
-    is_buy_agent = forms.BooleanField(required=False)
-    is_sell_agent = forms.BooleanField(required=False)
-
-    languages_string = forms.CharField(
-        required=True,
-        min_length=1,
-        max_length=200
-    )
-
-    # Next URL after registration
-    next = forms.CharField(required=False)
-
-    def clean(self):
-        super(RegisterForm, self).clean()
-
-        has_error = False
-
-        phone_number = self.cleaned_data.get('whatsapp_phone_number')
-        if phone_number is not None:
-            parsed_ph = phonenumbers.parse(str(phone_number), None)
-            ph_cc = parsed_ph.country_code
-            ph_nn = parsed_ph.national_number
-
-            try:
-                phone_number = models.PhoneNumber.objects.get(
-                    country_code=ph_cc,
-                    national_number=ph_nn
-                )
-
-                user_w_ph = models.User.objects.filter(
-                    phone_number=phone_number.id, # User has phone number
-                    registered__isnull=False, # User is registered
-                    django_user__isnull=False # User has a Django user linked
-                ).first()
-
-                if user_w_ph is not None:
-                    self.add_error('whatsapp_phone_number', 'This phone number belongs to an existing user. Please ensure you\'ve entered your phone number correctly.')
-                    has_error = True
-            except models.PhoneNumber.DoesNotExist:
-                # Good - no user has this phone number
-                pass
-
-        email = self.cleaned_data.get('email')
-
-        if email is not None:
-            try:
-                email = models.Email.objects.get(email=email)
-
-                user_w_email = models.User.objects.filter(
-                    email=email.id, # User has email
-                    registered__isnull=False, # User is registered
-                    django_user__isnull=False # User has a Django user linked
-                ).first()
-
-                if user_w_email is not None:
-                    self.add_error('email', 'This email belongs to an existing user. Please ensure you \'ve entered your email correctly.')
-                    has_error = True
-            except models.Email.DoesNotExist:
-                # Good - no user has this email
-                pass
-
-        if self.cleaned_data.get('has_company'):
-            company_name = self.cleaned_data.get('company_name')
-            if company_name is None or company_name.strip() == '':
-                self.add_error('company_name', _require_msg)
-                has_error = True
-
-        if has_error:
-            raise ValidationError(None)
-
-        return self.cleaned_data
-
-class WhatsAppBodyForm(forms.Form):
-    body = forms.CharField()
-
-class LoginForm(forms.Form):
-    whatsapp_phone_number = PhoneNumberField(required=True)
-    next = forms.CharField(required=False)
-
-    def clean(self):
-        super(LoginForm, self).clean()
-
-        # Parse phone number
-        try:
-            ph_str = self.cleaned_data.get('whatsapp_phone_number')
-            parsed_ph = phonenumbers.parse(str(ph_str), None)
-            ph_cc = parsed_ph.country_code
-            ph_nn = parsed_ph.national_number
-        except Exception as e:
-            raise ValidationError(None)
-
-        # Check existence of phone number
-        try:
-            phone_number = models.PhoneNumber.objects.get(
-                country_code=ph_cc,
-                national_number=ph_nn
-            )
-        except models.PhoneNumber.DoesNotExist:
-            self.add_error('whatsapp_phone_number', "Account don't exist.")
-            raise ValidationError(None)
-
-        user = models.User.objects.filter(
-            phone_number=phone_number.id, # User has phone number
-            registered__isnull=False, # User is registered
-            django_user__isnull=False # User has a Django user linked
-        ).first()
-
-        # Check existence of user
-        if user is None:
-            self.add_error('whatsapp_phone_number', "Account don't exist.")
-            raise ValidationError(None)
-
-        return self.cleaned_data
+from relationships import models
 
 class EmailLoginForm(forms.Form):
     email = forms.EmailField()
@@ -283,11 +76,11 @@ class ProfileForm(forms.Form):
     first_name = forms.CharField()
     last_name = forms.CharField()
     company_name = forms.CharField()
-    phone_number = forms.CharField()
-    goods_string = forms.CharField()
+    phone_number = PhoneNumberField()
 
     def __init__(self, *args, **kwargs):
-        self.request = kwargs.pop("request") # request passed in into object variable
+        # Make request object passed in a class variable
+        self.request = kwargs.pop('request')
         super().__init__(*args, **kwargs)
 
     def clean(self):
@@ -321,3 +114,226 @@ class ProfileForm(forms.Form):
 
         if has_error:
             raise ValidationError(None)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from urllib import request
+# from common.utilities.is_censored import is_censored
+
+# _require_msg = 'This field is required.'
+# _censor_msg = 'Don\'t share contact details here. You may share contact details later.'
+
+# class UserEditForm(forms.Form):
+#     first_name = forms.CharField(
+#         required=True,
+#         min_length=1,
+#         max_length=20
+#     )
+#     last_name = forms.CharField(
+#         required=True,
+#         min_length=1,
+#         max_length=20
+#     )
+
+#     has_company = forms.BooleanField(required=False)
+
+#     # Validated only if has_company is checked.
+#     company_name = forms.CharField(
+#         required=False,
+#         max_length=50
+#     )
+
+#     goods_string = forms.CharField(
+#         required=True,
+#         min_length=1,
+#         max_length=200
+#     )
+
+#     # No validations, for survey only.
+#     is_buyer = forms.BooleanField(required=False)
+#     is_seller = forms.BooleanField(required=False)
+#     is_buy_agent = forms.BooleanField(required=False)
+#     is_sell_agent = forms.BooleanField(required=False)
+
+#     languages_string = forms.CharField(
+#         required=True,
+#         min_length=1,
+#         max_length=200
+#     )
+
+#     # Next URL after registration
+#     next = forms.CharField(required=False)
+
+#     def clean(self):
+#         super(UserEditForm, self).clean()
+
+#         has_error = False
+
+#         if self.cleaned_data.get('has_company'):
+#             company_name = self.cleaned_data.get('company_name')
+#             if company_name is None or company_name.strip() == '':
+#                 self.add_error('company_name', _require_msg)
+#                 has_error = True
+
+#         if has_error:
+#             raise ValidationError(None)
+
+#         return self.cleaned_data
+
+# class RegisterForm(forms.Form):
+#     whatsapp_phone_number = PhoneNumberField(required=True)
+    
+#     first_name = forms.CharField(
+#         required=True,
+#         min_length=1,
+#         max_length=20
+#     )
+#     last_name = forms.CharField(
+#         required=True,
+#         min_length=1,
+#         max_length=20
+#     )
+
+#     has_company = forms.BooleanField(required=False)
+
+#     # Validated only if has_company is checked.
+#     company_name = forms.CharField(required=False)
+    
+#     email = forms.EmailField(
+#         required=True
+#     )
+
+#     goods_string = forms.CharField(
+#         required=True,
+#         min_length=1,
+#         max_length=200
+#     )
+
+#     # No validations, for survey only.
+#     is_buyer = forms.BooleanField(required=False)
+#     is_seller = forms.BooleanField(required=False)
+#     is_buy_agent = forms.BooleanField(required=False)
+#     is_sell_agent = forms.BooleanField(required=False)
+
+#     languages_string = forms.CharField(
+#         required=True,
+#         min_length=1,
+#         max_length=200
+#     )
+
+#     # Next URL after registration
+#     next = forms.CharField(required=False)
+
+#     def clean(self):
+#         super(RegisterForm, self).clean()
+
+#         has_error = False
+
+#         phone_number = self.cleaned_data.get('whatsapp_phone_number')
+#         if phone_number is not None:
+#             parsed_ph = phonenumbers.parse(str(phone_number), None)
+#             ph_cc = parsed_ph.country_code
+#             ph_nn = parsed_ph.national_number
+
+#             try:
+#                 phone_number = models.PhoneNumber.objects.get(
+#                     country_code=ph_cc,
+#                     national_number=ph_nn
+#                 )
+
+#                 user_w_ph = models.User.objects.filter(
+#                     phone_number=phone_number.id, # User has phone number
+#                     registered__isnull=False, # User is registered
+#                     django_user__isnull=False # User has a Django user linked
+#                 ).first()
+
+#                 if user_w_ph is not None:
+#                     self.add_error('whatsapp_phone_number', 'This phone number belongs to an existing user. Please ensure you\'ve entered your phone number correctly.')
+#                     has_error = True
+#             except models.PhoneNumber.DoesNotExist:
+#                 # Good - no user has this phone number
+#                 pass
+
+#         email = self.cleaned_data.get('email')
+
+#         if email is not None:
+#             try:
+#                 email = models.Email.objects.get(email=email)
+
+#                 user_w_email = models.User.objects.filter(
+#                     email=email.id, # User has email
+#                     registered__isnull=False, # User is registered
+#                     django_user__isnull=False # User has a Django user linked
+#                 ).first()
+
+#                 if user_w_email is not None:
+#                     self.add_error('email', 'This email belongs to an existing user. Please ensure you \'ve entered your email correctly.')
+#                     has_error = True
+#             except models.Email.DoesNotExist:
+#                 # Good - no user has this email
+#                 pass
+
+#         if self.cleaned_data.get('has_company'):
+#             company_name = self.cleaned_data.get('company_name')
+#             if company_name is None or company_name.strip() == '':
+#                 self.add_error('company_name', _require_msg)
+#                 has_error = True
+
+#         if has_error:
+#             raise ValidationError(None)
+
+#         return self.cleaned_data
+
+# class WhatsAppBodyForm(forms.Form):
+#     body = forms.CharField()
+
+# class LoginForm(forms.Form):
+#     whatsapp_phone_number = PhoneNumberField(required=True)
+#     next = forms.CharField(required=False)
+
+#     def clean(self):
+#         super(LoginForm, self).clean()
+
+#         # Parse phone number
+#         try:
+#             ph_str = self.cleaned_data.get('whatsapp_phone_number')
+#             parsed_ph = phonenumbers.parse(str(ph_str), None)
+#             ph_cc = parsed_ph.country_code
+#             ph_nn = parsed_ph.national_number
+#         except Exception as e:
+#             raise ValidationError(None)
+
+#         # Check existence of phone number
+#         try:
+#             phone_number = models.PhoneNumber.objects.get(
+#                 country_code=ph_cc,
+#                 national_number=ph_nn
+#             )
+#         except models.PhoneNumber.DoesNotExist:
+#             self.add_error('whatsapp_phone_number', "Account don't exist.")
+#             raise ValidationError(None)
+
+#         user = models.User.objects.filter(
+#             phone_number=phone_number.id, # User has phone number
+#             registered__isnull=False, # User is registered
+#             django_user__isnull=False # User has a Django user linked
+#         ).first()
+
+#         # Check existence of user
+#         if user is None:
+#             self.add_error('whatsapp_phone_number', "Account don't exist.")
+#             raise ValidationError(None)
+
+#         return self.cleaned_data
