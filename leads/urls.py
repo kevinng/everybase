acaa4926867f43a4b2598bdef89ba275b4accfb4
Kevin Ng <kevin@everybase.co>
2022-05-05 16:04:46 +0800

    Update leads URL routings

--- leads/urls.py
+++ leads/urls.py
@@ -3,11 +3,15 @@ from leads import views
 
 app_name = 'leads'
 urlpatterns = [
-    # Don't map slug URLs above generic links like lead_create. Otherwise,
-    # 'leads/create' will go to a lead detail page with slug 'create'.
     path('', views.lead_list, name='lead_list'),
+    path('my', views.my_leads, name='my_leads'),
     path('create', views.lead_create, name='lead_create'),
+
+    # NOTE: map other routes above slug routes
     path('<slug:slug>', views.lead_detail, name='lead_detail'),
     path('<slug:slug>/edit', views.lead_edit, name='lead_edit'),
-    path('<slug:slug>/applications', views.LeadApplicationListView.as_view(), name='lead_applications'),    
+    path('<slug:slug>/delete', views.lead_delete, name='lead_delete'),
+
+    # Pre v5 routings
+    # path('<slug:slug>/applications', views.LeadApplicationListView.as_view(), name='lead_applications'),
 ]
