b848a23d101292449303c444fd2548bc3714078d
Kevin Ng <kevin@everybase.co>
2022-05-12 21:59:14 +0800

    Update lead country migration script

--- scripts/migrate_12_may_22_lead_country.py
+++ scripts/migrate_12_may_22_lead_country.py
@@ -2,11 +2,11 @@ from leads import models
 
 def run():
     for lead in models.Lead.objects.all():
-        if lead.lead_type == 'buying' and lead.country is not None and lead.buy_country is None:
-            lead.buy_country = lead.country
+        if lead.lead_type == 'buying' and lead.country is not None and lead.sell_country is None:
+            lead.sell_country = lead.country
             lead.save()
             print('Updated %s ' % lead)
-        elif lead.lead_type == 'selling' and lead.country is not None and lead.sell_country is None:
-            lead.sell_country = lead.country
+        elif lead.lead_type == 'selling' and lead.country is not None and lead.buy_country is None:
+            lead.buy_country = lead.country
             lead.save()
             print('Updated %s ' % lead)
