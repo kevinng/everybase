4daea840f206fea2c9b9c72184f3c544f3b8700f
Kevin Ng <kevin@everybase.co>
2022-05-16 19:01:22 +0800

    Fix clear filter form post script

--- scripts/delete_filter_form_post_with_no_title_detail.py
+++ scripts/delete_filter_form_post_with_no_title_detail.py
@@ -3,5 +3,5 @@ from leads import models
 def run():
     for f in models.FilterFormPost.objects.all():
         if (f.title is None or f.title == '') and (f.details is None or f.details == ''):
-            print('Deleted ' + f)
+            print('Deleted ' + str(f))
             f.delete()
