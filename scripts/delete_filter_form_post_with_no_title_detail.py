88914a4c9e0499be28f10e59cb899a40133ecd09
Kevin Ng <kevin@everybase.co>
2022-05-16 18:36:14 +0800

    Update delete filter form post script

--- scripts/delete_filter_form_post_with_no_title_detail.py
+++ scripts/delete_filter_form_post_with_no_title_detail.py
@@ -1,7 +1,9 @@
 from leads import models
 
 def run():
-    models.FilterFormPost.objects.filter(
-        title='',
-        details=''
-    ).delete()
+    ffps = models.FilterFormPost.objects.all()
+
+    for ffp in ffps:
+        if (ffps.title == '' or ffps.title is None) and (ffps.details == '' or ffps.details is None):
+            print('Delete ' + ffps)
+            ffp.delete()
