edf1f76d423999d50d019b1a4b77da6793f3a9fb
Kevin Ng <kevin@everybase.co>
2022-07-28 01:15:22 +0800

    Update FSF selling lead port script

--- scripts/create_selling_leads_from_f2f_selling_offers.py
+++ scripts/create_selling_leads_from_f2f_selling_offers.py
@@ -1,11 +1,13 @@
+from django.db.models import F
 from relationships import models as relmods
 from leads import models as lemods
 from growth.models import Fibre2FashionSellingOffer
 
 def run():
     print(f'Start porting {str(Fibre2FashionSellingOffer.objects.all().count())} F2F selling leads...')
-    limit = 10
-    for o in Fibre2FashionSellingOffer.objects.all():
+
+    print('Even leads only...')
+    for o in Fibre2FashionSellingOffer.objects.annotate(odd=F('id') % 2).filter(odd=False):
         print(o)
         user, _ = relmods.User.objects.get_or_create(email=o.email)
         if (o.title is not None and o.title.strip() != '') or (o.description is not None and o.description.strip() != ''):
@@ -17,6 +19,15 @@ def run():
 
             print('Created ' + str(lead))
 
-        # limit -= 1
-        # if limit < 0:
-        #     break
+    print('Odd leads only...')
+    for o in Fibre2FashionSellingOffer.objects.annotate(odd=F('id') % 2).filter(odd=True):
+        print(o)
+        user, _ = relmods.User.objects.get_or_create(email=o.email)
+        if (o.title is not None and o.title.strip() != '') or (o.description is not None and o.description.strip() != ''):
+            lead = lemods.Lead.objects.create(
+                author=user,
+                body=o.title + ' ' + o.description,
+                lead_type='selling'
+            )
+
+            print('Created ' + str(lead))
