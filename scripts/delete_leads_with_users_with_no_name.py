6f98190377198b55f468357a5292aa59bcfb7ed9
Kevin Ng <kevin@everybase.co>
2022-07-28 19:22:43 +0800

    Update delete leads with users with no name script

--- scripts/delete_leads_with_users_with_no_name.py
+++ scripts/delete_leads_with_users_with_no_name.py
@@ -4,3 +4,4 @@ def run():
     print(models.Lead.objects.filter(author__first_name__isnull=True).count())
     print(models.Lead.objects.filter(author__last_name__isnull=True).count())
     print(models.Lead.objects.filter(author__first_name__isnull=True).filter(author__last_name__isnull=True).count())
+    models.Lead.objects.filter(author__first_name__isnull=True).filter(author__last_name__isnull=True).delete()
