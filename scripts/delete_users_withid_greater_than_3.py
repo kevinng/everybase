bb92727517d7a58d656a1c67cfb0175933e46ddb
Kevin Ng <kevin@everybase.co>
2022-10-27 01:22:06 +0800

    Update delete user script to delete dependencies

--- scripts/delete_users_withid_greater_than_3.py
+++ scripts/delete_users_withid_greater_than_3.py
@@ -1,4 +1,24 @@
 from relationships import models
+from chat import models as cmods
+from files import models as fimods
 
 def run():
-    models.User.objects.filter(id__gt=3).delete()
+    users = models.User.objects.filter(id__gt=3)
+
+    for u in users:
+        for tim in cmods.TwilioInboundMessage.objects.filter(from_user=u):
+            tim.delete()
+
+        for tim in cmods.TwilioInboundMessage.objects.filter(to_user=u):
+            tim.delete()
+
+        for f in fimods.File.objects.filter(uploader=u):
+            f.delete()
+
+        for tom in cmods.TwilioOutboundMessage.objects.filter(from_user=u):
+            tom.delete()
+
+        for tom in cmods.TwilioOutboundMessage.objects.filter(to_user=u):
+            tom.delete()
+
+        u.delete()
